88b81e644eaf420ae5051a61d7dcf18d7dc044df
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-01-23 00:01:22 +0000

    4.0pre4 ready for release
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@3992 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- installer/mk_filelist.sh
+++ installer/mk_filelist.sh
@@ -15,36 +15,37 @@ find doc \( -name \*.txt -o -name \*.html -o -name \*.gif \) >> installer/jedit-
 find doc -name toc.xml >> installer/jedit-program
 
 echo -n "jedit-program: "
-ls -l `cat installer/jedit-program` | awk 'BEGIN { size=0 } { disk_size+=(int($5/8192+1)*8); size+=$5/1024 } END { print disk_size " " size }'
+ls -l `cat installer/jedit-program` | awk 'BEGIN { size=0 } { disk_size+=(int($5/4096+1)*4); size+=$5/1024 } END { print disk_size " " size }'
 
 # jedit-macros fileset
 find macros -name \*.bsh > installer/jedit-macros
 
 echo -n "jedit-macros: "
-ls -l `cat installer/jedit-macros` | awk 'BEGIN { size=0 } { disk_size+=(int($5/8192+1)*8); size+=$5/1024 } END { print disk_size " " size }'
+ls -l `cat installer/jedit-macros` | awk 'BEGIN { size=0 } { disk_size+=(int($5/4096+1)*4); size+=$5/1024 } END { print disk_size " " size }'
 
 # jedit-windows fileset
-echo jedinit.exe > installer/jedit-windows
-echo jedinstl.dll >> installer/jedit-windows
-echo jedit.exe >> installer/jedit-windows
+echo jeshlstb.dl_ > installer/jedit-windows
+echo ltslog.dll >> installer/jedit-windows
 echo jeditsrv.exe >> installer/jedit-windows
-echo jeservps.dll >> installer/jedit-windows
-echo jeshlstb.dl_ >> installer/jedit-windows
+echo jedit.exe >> installer/jedit-windows
+echo jedinit.exe >> installer/jedit-windows
 echo unlaunch.exe >> installer/jedit-windows
+echo jedinstl.dll >> installer/jedit-windows
+echo jeservps.dll >> installer/jedit-windows
 
 echo -n "jedit-windows: "
-ls -l `cat installer/jedit-windows` | awk 'BEGIN { size=0 } { disk_size+=(int($5/8192+1)*8); size+=$5/1024 } END { print disk_size " " size }'
+ls -l `cat installer/jedit-windows` | awk 'BEGIN { size=0 } { disk_size+=(int($5/4096+1)*4); size+=$5/1024 } END { print disk_size " " size }'
 
 # jedit-mac fileset
 echo jars/MacOS.jar > installer/jedit-mac
 echo -n "jedit-mac: "
-ls -l `cat installer/jedit-mac` | awk 'BEGIN { size=0 } { disk_size+=(int($5/8192+1)*8); size+=$5/1024 } END { print disk_size " " size }'
+ls -l `cat installer/jedit-mac` | awk 'BEGIN { size=0 } { disk_size+=(int($5/4096+1)*4); size+=$5/1024 } END { print disk_size " " size }'
 
 # jedit-os2 fileset
 echo jedit.cmd > installer/jedit-os2
 
 echo -n "jedit-os2: "
-ls -l `cat installer/jedit-os2` | awk 'BEGIN { size=0 } { disk_size+=(int($5/8192+1)*8); size+=$5/1024 } END { print disk_size " " size }'
+ls -l `cat installer/jedit-os2` | awk 'BEGIN { size=0 } { disk_size+=(int($5/4096+1)*4); size+=$5/1024 } END { print disk_size " " size }'
 
 
 for file in installer/jedit-*
