6d7d5566f1a4f9266dc73cdc5d7aa3c227d42cbb
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-05-15 21:40:26 +0000

    added copyright notices to tom's code
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@4159 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/print/BufferPrinter1_3.java
+++ org/gjt/sp/jedit/print/BufferPrinter1_3.java
@@ -4,6 +4,7 @@
  * :folding=explicit:collapseFolds=1:
  *
  * Copyright (C) 2001 Slava Pestov
+ * Portions copyright (C) 2002 Thomas Dilts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
