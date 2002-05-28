4a4042a7ccbceaecb81e0ed56ea215dacdd723fd
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-05-28 01:58:02 +0000

    Removed MiscUtilities.listDirectory()
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@2412 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/MiscUtilities.java
+++ org/gjt/sp/jedit/MiscUtilities.java
@@ -353,42 +353,6 @@ public class MiscUtilities
 		return name.replace('.','/').concat(".class");
 	} //}}}
 
-	//{{{ listFiles() method
-	/**
-	 * Returns an array containing the full path names of all files
-	 * within the specified directory that match the specified file
-	 * name glob.
-	 * @param directory The directory path
-	 * @param glob The file name glob
-	 * @param recurse If true, subdirectories will be listed as well
-	 */
-	public static String[] listDirectory(String directory, String glob,
-		boolean recurse)
-	{
-		Log.log(Log.DEBUG,MiscUtilities.class,"Listing " + directory);
-		Vector files = new Vector(100);
-
-		RE filter;
-		try
-		{
-			filter = new RE(globToRE(glob));
-		}
-		catch(Exception e)
-		{
-			Log.log(Log.ERROR,MiscUtilities.class,e);
-			return null;
-		}
-
-		listDirectory(new Vector(),files,new File(directory),filter,recurse);
-
-		String[] retVal = new String[files.size()];
-		files.copyInto(retVal);
-
-		quicksort(retVal,new StringICaseCompare());
-
-		return retVal;
-	} //}}}
-
 	//}}}
 
 	//{{{ Text methods
@@ -1049,56 +1013,6 @@ loop:		for(int i = 0; i < str.length(); i++)
 
 	//{{{ Private members
 	private MiscUtilities() {}
-
-	//{{{ listDirectory() method
-	private static void listDirectory(Vector stack, Vector files,
-		File directory, RE filter, boolean recurse)
-	{
-		if(stack.contains(directory))
-		{
-			Log.log(Log.ERROR,MiscUtilities.class,
-				"Recursion in listDirectory(): "
-				+ directory.getPath());
-			return;
-		}
-		else
-			stack.addElement(directory);
-
-		File[] _files = directory.listFiles();
-		if(_files == null)
-			return;
-
-		for(int i = 0; i < _files.length; i++)
-		{
-			File file = _files[i];
-			if(file.isDirectory())
-			{
-				if(recurse)
-				{
-					// resolve symlinks to avoid loops
-					try
-					{
-						file = new File(file.getCanonicalPath());
-					}
-					catch(IOException io)
-					{
-					}
-
-					listDirectory(stack,files,file,filter,recurse);
-				}
-			}
-			else
-			{
-				if(!filter.isMatch(file.getName()))
-					continue;
-
-				String path = file.getPath();
-				Log.log(Log.DEBUG,MiscUtilities.class,path);
-
-				files.addElement(path);
-			}
-		}
-	} //}}}
 
 	//{{{ resolveSymlinks() method
 	private static String resolveSymlinks(String path)
