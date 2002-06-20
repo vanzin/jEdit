b59a2174cd32adc5ec4144263182b2c25ff99bdd
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-06-20 10:33:20 +0000

    patch from gerd, integer property methods trim whitespace'
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@2487 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/JEditTextArea.java
+++ org/gjt/sp/jedit/textarea/JEditTextArea.java
@@ -6143,10 +6143,12 @@ loop:			for(int i = lineNo + 1; i < getLineCount(); i++)
 			if(dragStartOffset == getLineLength(dragStartLine))
 				dragStartOffset--;
 
+			boolean joinNonWordChars =
+				jEdit.getBooleanProperty("view.joinNonWordChars");
 			int wordStart = TextUtilities.findWordStart(lineText,
-				dragStartOffset,noWordSep);
+				dragStartOffset,noWordSep,joinNonWordChars);
 			int wordEnd = TextUtilities.findWordEnd(lineText,
-				dragStartOffset+1,noWordSep);
+				dragStartOffset+1,noWordSep,joinNonWordChars);
 
 			int lineStart = getLineStartOffset(dragStartLine);
 			addToSelection(new Selection.Range(lineStart + wordStart,
