ad384dc9be7d3cd98b3db065191ef6ac940d1f06
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-08-21 19:54:39 +0000

    pre4 ready... for real
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@4324 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -72,7 +72,7 @@ public class jEdit
 	public static String getBuild()
 	{
 		// (major).(minor).(<99 = preX, 99 = final).(bug fix)
-		return "04.01.03.00";
+		return "04.01.04.00";
 	} //}}}
 
 	//{{{ main() method
