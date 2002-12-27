19cdf787b0a1512a3aeeaf8202ebf9fbee0d4679
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-12-27 02:13:34 +0000

    NEWS.txt updated
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@4400 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/io/FileVFS.java
+++ org/gjt/sp/jedit/io/FileVFS.java
@@ -418,39 +418,8 @@ public class FileVFS extends VFS
 				{
 					String s = output.substring(1, 10);
 
-					if(s.length() == 9)
-					{
-						if(s.charAt(0) == 'r')
-							permissions += 0400;
-						if(s.charAt(1) == 'w')
-							permissions += 0200;
-						if(s.charAt(2) == 'x')
-							permissions += 0100;
-						else if(s.charAt(2) == 's')
-							permissions += 04100;
-						else if(s.charAt(2) == 'S')
-							permissions += 04000;
-						if(s.charAt(3) == 'r')
-							permissions += 040;
-						if(s.charAt(4) == 'w')
-							permissions += 020;
-						if(s.charAt(5) == 'x')
-							permissions += 010;
-						else if(s.charAt(5) == 's')
-							permissions += 02010;
-						else if(s.charAt(5) == 'S')
-							permissions += 02000;
-						if(s.charAt(6) == 'r')
-							permissions += 04;
-						if(s.charAt(7) == 'w')
-							permissions += 02;
-						if(s.charAt(8) == 'x')
-							permissions += 01;
-						else if(s.charAt(8) == 't')
-							permissions += 01001;
-						else if(s.charAt(8) == 'T')
-							permissions += 01000;
-					}
+					permissions = MiscUtilities
+						.parsePermissions(s);
 				}
 			}
 
