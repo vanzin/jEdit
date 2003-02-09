346a270900a5067ead5852417cb7dfb2a7015a38
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-02-09 05:16:57 +0000

    removed API guide part of user's guide in favor of new javadocs
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@2679 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -449,8 +449,17 @@ public class jEdit
 
 	//{{{ getProperty() method
 	/**
-	 * Returns the property with the specified name, formatting it with
-	 * the <code>java.text.MessageFormat.format()</code> method.
+	 * Returns the property with the specified name.<p>
+	 *
+	 * The elements of the <code>args</code> array are substituted
+	 * into the value of the property in place of strings of the
+	 * form <code>{<i>n</i>}</code>, where <code><i>n</i></code> is an index
+	 * in the array.<p>
+	 *
+	 * You can find out more about this feature by reading the
+	 * documentation for the <code>format</code> method of the
+	 * <code>java.text.MessageFormat</code> class.
+	 *
 	 * @param name The property
 	 * @param args The positional parameters
 	 */
