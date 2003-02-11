9a2c4ee4789eeeb468d3b196c96e9142fe18c461
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-02-11 02:55:57 +0000

    started plugin-tips section
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@4476 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/EBComponent.java
+++ org/gjt/sp/jedit/EBComponent.java
@@ -40,7 +40,7 @@ public interface EBComponent
 	 * for various subclasses of the {@link EBMessage} class. Typically
 	 * this is done with one or more <code>if</code> blocks that test
 	 * whether the message is an instance of a derived message class in
-	 * which the plugin has an interest. For example:
+	 * which the component has an interest. For example:
 	 *
 	 * <pre> if(msg instanceof BufferUpdate) {
          *     // a buffer's state has changed!
