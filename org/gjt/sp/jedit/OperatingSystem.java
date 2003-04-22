260e561d99643a0f774f69b9973ecde11cc368c7
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-04-22 19:46:16 +0000

    detects if running on VMS
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@2840 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/OperatingSystem.java
+++ org/gjt/sp/jedit/OperatingSystem.java
@@ -1,6 +1,9 @@
 /*
  * OperatingSystem.java - OS detection
- * Copyright (C) 2002 Slava Pestov
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2002, 2003 Slava Pestov
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -224,14 +227,23 @@ L2:			for (int j=0; j < gc.length; j++)
 		return os == MAC_OS_X;
 	} //}}}
 
+	//{{{ isVMS() method
+	/**
+	 * Returns if we're running VMS.
+	 */
+	public static final boolean isVMS()
+	{
+		return os == VMS;
+	} //}}}
+
 	//{{{ isMacOSLF() method
-        /**
-         * Returns if we're running MacOS X and using the native look and feel.
-         */
-        public static final boolean isMacOSLF()
-        {
-                return (isMacOS() && UIManager.getLookAndFeel().isNativeLookAndFeel());
-        } //}}}
+	/**
+	* Returns if we're running MacOS X and using the native look and feel.
+	*/
+	public static final boolean isMacOSLF()
+	{
+		return (isMacOS() && UIManager.getLookAndFeel().isNativeLookAndFeel());
+	} //}}}
 
 	//{{{ isJava14() method
 	/**
@@ -248,6 +260,7 @@ L2:			for (int j=0; j < gc.length; j++)
 	private static final int WINDOWS_NT = 0x666;
 	private static final int OS2 = 0xDEAD;
 	private static final int MAC_OS_X = 0xABC;
+	private static final int VMS = 0xDEAD2;
 	private static final int UNKNOWN = 0xBAD;
 
 	private static int os;
@@ -275,6 +288,10 @@ L2:			for (int j=0; j < gc.length; j++)
 			else if(osName.indexOf("OS/2") != -1)
 			{
 				os = OS2;
+			}
+			else if(osName.indexOf("VMS") != -1)
+			{
+				os = VMS;
 			}
 			else if(File.separatorChar == '/')
 			{
