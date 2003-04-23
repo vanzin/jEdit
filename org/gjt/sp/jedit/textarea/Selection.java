1edb2eab692ce10a4cff9cb591a60beb232e29ee
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-04-23 02:58:21 +0000

    more rectangular selection work
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@2842 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/Selection.java
+++ org/gjt/sp/jedit/textarea/Selection.java
@@ -582,17 +582,21 @@ public abstract class Selection implements Cloneable
 		boolean contentInserted(Buffer buffer, int startLine, int start,
 			int numLines, int length)
 		{
+			if(this.end < start)
+				return false;
+
 			boolean changed = false;
+
+			int startVirtualColumn = buffer.getVirtualWidth(
+				this.startLine,start - buffer.getLineStartOffset(this.startLine));
+			int endVirtualColumn = buffer.getVirtualWidth(
+				this.endLine,end - buffer.getLineStartOffset(this.endLine));
 
 			if(this.start == start)
 			{
-				int startVirtualColumn = buffer.getVirtualWidth(
-					this.startLine,start - buffer.getLineStartOffset(this.startLine));
 				this.start += length;
 				int newStartVirtualColumn = buffer.getVirtualWidth(
 					startLine,start - buffer.getLineStartOffset(this.startLine));
-				int endVirtualColumn = buffer.getVirtualWidth(
-					this.endLine,end - buffer.getLineStartOffset(this.endLine));
 				int[] totalVirtualWidth = new int[1];
 				int newEnd = buffer.getOffsetOfVirtualColumn(
 					this.endLine,endVirtualColumn +
@@ -601,6 +605,11 @@ public abstract class Selection implements Cloneable
 					totalVirtualWidth);
 				if(newEnd != -1)
 					end = buffer.getLineStartOffset(this.endLine) + newEnd;
+				else
+				{
+					end = buffer.getLineEndOffset(this.endLine) - 1;
+					extraEndVirt = totalVirtualWidth[0] - endVirtualColumn;
+				}
 			}
 			else if(this.start > start)
 			{
@@ -610,15 +619,21 @@ public abstract class Selection implements Cloneable
 				changed = true;
 			}
 
-			if(this.end >= start)
+			this.end += length;
+			if(numLines != 0)
+				this.endLine = buffer.getLineOfOffset(this.end);
+			int newEndVirtualColumn = buffer.getVirtualWidth(
+				endLine,end - buffer.getLineStartOffset(this.endLine));
+			if(startLine == this.endLine && extraEndVirt != 0)
 			{
-				this.end += length;
-				if(numLines != 0)
-					this.endLine = buffer.getLineOfOffset(this.end);
-				changed = true;
+				extraEndVirt += (endVirtualColumn - newEndVirtualColumn);
+			}
+			else if(startLine == this.startLine && extraStartVirt != 0)
+			{
+				extraStartVirt += (endVirtualColumn - newEndVirtualColumn);
 			}
 
-			return changed;
+			return true;
 		} //}}}
 
 		//{{{ contentRemoved() method
@@ -676,6 +691,5 @@ public abstract class Selection implements Cloneable
 		} //}}}
 
 		//}}}
-		
 	} //}}}
 }
