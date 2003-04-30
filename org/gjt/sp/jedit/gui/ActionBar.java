06925d7193bab76d5d000afa5b4ee19e146b0fb1
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-04-30 05:47:11 +0000

    don't bomb if plugin.stop() fails
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@2867 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/ActionBar.java
+++ org/gjt/sp/jedit/gui/ActionBar.java
@@ -513,6 +513,26 @@ public class ActionBar extends JPanel
 					action.processKeyEvent(evt);
 				else if(keyCode == KeyEvent.VK_ENTER)
 					invoke();
+				else if(keyCode == KeyEvent.VK_UP)
+				{
+					int selected = list.getSelectedIndex();
+					if(selected == 0)
+					{
+						list.setSelectedIndex(
+							list.getModel().getSize()
+							- 1);
+						evt.consume();
+					}
+				}
+				else if(keyCode == KeyEvent.VK_DOWN)
+				{
+					int selected = list.getSelectedIndex();
+					if(selected == list.getModel().getSize() - 1)
+					{
+						list.setSelectedIndex(0);
+						evt.consume();
+					}
+				}
 			}
 		} //}}}
 	} //}}}
