0a33c592d507a95567a03360fe445930cd3b16ff
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-05-16 21:16:27 +0000

    better handling of vfs updates in browser
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@4714 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/browser/VFSFileNameField.java
+++ org/gjt/sp/jedit/browser/VFSFileNameField.java
@@ -209,23 +209,13 @@ class VFSFileNameField extends HistoryTextField
 		}
 		else
 		{
-			if(pathsEqual(parent,browser.getDirectory()))
+			if(VFSBrowser.pathsEqual(parent,browser.getDirectory()))
 				newText = name;
 			else
 				newText = path;
 		}
 
 		setText(newText);
-	} //}}}
-
-	//{{{ pathsEqual() method
-	private boolean pathsEqual(String p1, String p2)
-	{
-		if(p1.endsWith("/") || p1.endsWith(File.separator))
-			p1 = p1.substring(0,p1.length() - 1);
-		if(p2.endsWith("/") || p2.endsWith(File.separator))
-			p2 = p2.substring(0,p2.length() - 2);
-		return p1.equals(p2);
 	} //}}}
 
 	//{{{ goToParent() method
