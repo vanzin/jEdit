7974d4119e8046c2816be8464aa022365e9579d4
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-05-27 21:39:16 +0000

    added MouseActions.java
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@2931 6b1eeb88-9816-0410-afa2-b43733a0f04e

---
+++ org/gjt/sp/jedit/textarea/MouseActions.java
@@ -0,0 +1,46 @@
+/*
+ * MouseActions.java - Simplifies mouse handling
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2003 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.gjt.sp.jedit.textarea;
+
+import java.awt.event.MouseEvent;
+import org.gjt.sp.jedit.gui.DefaultInputHandler;
+import org.gjt.sp.jedit.jEdit;
+
+public class MouseActions
+{
+	//{{{ MouseActions constructor
+	MouseActions(String name)
+	{
+		this.name = name;
+	} //}}}
+
+	//{{{ getActionForEvent() method
+	String getActionForEvent(MouseEvent evt, String variant)
+	{
+		return jEdit.getProperty("view." + name + "."
+			+ DefaultInputHandler.getModifierString(evt)
+			+ variant + "Click");
+	} //}}}
+
+	private String name;
+}
