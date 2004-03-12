faaf86f1f39ea4f342b148898a7b139202761808
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2004-03-12 19:27:00 +0000

    API for enderak
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@3181 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -2521,6 +2521,17 @@ public class jEdit
 		}
 	} //}}}
 
+	//{{{ getEditServer() method
+	/**
+	 * Returns the edit server instance. You can use this to find out the
+	 * port number jEdit is listening on.
+	 * @since jEdit 4.2pre10
+	 */
+	public static EditServer getEditServer()
+	{
+		return server;
+	} //}}}
+
 	//}}}
 
 	//{{{ Package-private members
