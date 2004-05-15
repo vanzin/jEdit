e44ccfb82a3f50aaa443fd9d2d1d03434a8ed6cf
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2004-05-15 21:57:22 +0000

    PL/I mode
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@5044 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -68,7 +68,7 @@ public class jEdit
 	public static String getBuild()
 	{
 		// (major).(minor).(<99 = preX, 99 = final).(bug fix)
-		return "04.02.13.00";
+		return "04.02.14.00";
 	} //}}}
 
 	//{{{ main() method
