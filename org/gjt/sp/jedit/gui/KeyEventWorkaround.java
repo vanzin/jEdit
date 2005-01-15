69bfc7f16324694241136e6b315ddde5ef023226
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-01-15 20:48:27 +0000

    KeyEventWorkaround changes; asp.xml updated; drag and drop highlights selection when dropped
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@5169 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/KeyEventWorkaround.java
+++ org/gjt/sp/jedit/gui/KeyEventWorkaround.java
@@ -29,15 +29,16 @@ import org.gjt.sp.jedit.Debug;
 
 /**
  * Various hacks to get keyboard event handling to behave in a consistent manner
- * across Java implementations.
+ * across Java implementations. This type of stuff should not be necessary, but
+ * Java's keyboard handling is crap, to put it mildly.
  *
  * @author Slava Pestov
  * @version $Id$
  */
 public class KeyEventWorkaround
 {
-	//{{{ isModifier() method
-	public static boolean isModifier(int keyCode)
+	//{{{ isBindable() method
+	public static boolean isBindable(int keyCode)
 	{
 		switch(keyCode)
 		{
@@ -46,6 +47,139 @@ public class KeyEventWorkaround
 		case KeyEvent.VK_CONTROL:
 		case KeyEvent.VK_SHIFT:
 		case KeyEvent.VK_META:
+		case KeyEvent.VK_DEAD_GRAVE:
+		case KeyEvent.VK_DEAD_ACUTE:
+		case KeyEvent.VK_DEAD_CIRCUMFLEX:
+		case KeyEvent.VK_DEAD_TILDE:
+		case KeyEvent.VK_DEAD_MACRON:
+		case KeyEvent.VK_DEAD_BREVE:
+		case KeyEvent.VK_DEAD_ABOVEDOT:
+		case KeyEvent.VK_DEAD_DIAERESIS:
+		case KeyEvent.VK_DEAD_ABOVERING:
+		case KeyEvent.VK_DEAD_DOUBLEACUTE:
+		case KeyEvent.VK_DEAD_CARON:
+		case KeyEvent.VK_DEAD_CEDILLA:
+		case KeyEvent.VK_DEAD_OGONEK:
+		case KeyEvent.VK_DEAD_IOTA:
+		case KeyEvent.VK_DEAD_VOICED_SOUND:
+		case KeyEvent.VK_DEAD_SEMIVOICED_SOUND:
+			return false;
+		default:
+			return true;
+		}
+	} //}}}
+
+	//{{{ isPrintable() method
+	/**
+	 * We need to know if a keycode can potentially result in a
+	 * keytyped.
+	 * @since jEdit 4.3pre2
+	 */
+	public static boolean isPrintable(int keyCode)
+	{
+		switch(keyCode)
+		{
+		/* case KeyEvent.VK_ENTER:
+		case KeyEvent.VK_TAB: */
+		case KeyEvent.VK_SPACE:
+		case KeyEvent.VK_COMMA:
+		case KeyEvent.VK_MINUS:
+		case KeyEvent.VK_PERIOD:
+		case KeyEvent.VK_SLASH:
+		case KeyEvent.VK_0:
+		case KeyEvent.VK_1:
+		case KeyEvent.VK_2:
+		case KeyEvent.VK_3:
+		case KeyEvent.VK_4:
+		case KeyEvent.VK_5:
+		case KeyEvent.VK_6:
+		case KeyEvent.VK_7:
+		case KeyEvent.VK_8:
+		case KeyEvent.VK_9:
+		case KeyEvent.VK_SEMICOLON:
+		case KeyEvent.VK_EQUALS   :
+		case KeyEvent.VK_A:
+		case KeyEvent.VK_B:
+		case KeyEvent.VK_C:
+		case KeyEvent.VK_D:
+		case KeyEvent.VK_E:
+		case KeyEvent.VK_F:
+		case KeyEvent.VK_G:
+		case KeyEvent.VK_H:
+		case KeyEvent.VK_I:
+		case KeyEvent.VK_J:
+		case KeyEvent.VK_K:
+		case KeyEvent.VK_L:
+		case KeyEvent.VK_M:
+		case KeyEvent.VK_N:
+		case KeyEvent.VK_O:
+		case KeyEvent.VK_P:
+		case KeyEvent.VK_Q:
+		case KeyEvent.VK_R:
+		case KeyEvent.VK_S:
+		case KeyEvent.VK_T:
+		case KeyEvent.VK_U:
+		case KeyEvent.VK_V:
+		case KeyEvent.VK_W:
+		case KeyEvent.VK_X:
+		case KeyEvent.VK_Y:
+		case KeyEvent.VK_Z:
+		case KeyEvent.VK_OPEN_BRACKET :
+		case KeyEvent.VK_BACK_SLASH   :
+		case KeyEvent.VK_CLOSE_BRACKET:
+		case KeyEvent.VK_NUMPAD0 :
+		case KeyEvent.VK_NUMPAD1 :
+		case KeyEvent.VK_NUMPAD2 :
+		case KeyEvent.VK_NUMPAD3 :
+		case KeyEvent.VK_NUMPAD4 :
+		case KeyEvent.VK_NUMPAD5 :
+		case KeyEvent.VK_NUMPAD6 :
+		case KeyEvent.VK_NUMPAD7 :
+		case KeyEvent.VK_NUMPAD8 :
+		case KeyEvent.VK_NUMPAD9 :
+		case KeyEvent.VK_MULTIPLY:
+		case KeyEvent.VK_ADD     :
+		case KeyEvent.VK_SEPARATOR:
+		case KeyEvent.VK_SUBTRACT   :
+		case KeyEvent.VK_DECIMAL    :
+		case KeyEvent.VK_DIVIDE     :
+		case KeyEvent.VK_BACK_QUOTE:
+		case KeyEvent.VK_QUOTE:
+		case KeyEvent.VK_DEAD_GRAVE:
+		case KeyEvent.VK_DEAD_ACUTE:
+		case KeyEvent.VK_DEAD_CIRCUMFLEX:
+		case KeyEvent.VK_DEAD_TILDE:
+		case KeyEvent.VK_DEAD_MACRON:
+		case KeyEvent.VK_DEAD_BREVE:
+		case KeyEvent.VK_DEAD_ABOVEDOT:
+		case KeyEvent.VK_DEAD_DIAERESIS:
+		case KeyEvent.VK_DEAD_ABOVERING:
+		case KeyEvent.VK_DEAD_DOUBLEACUTE:
+		case KeyEvent.VK_DEAD_CARON:
+		case KeyEvent.VK_DEAD_CEDILLA:
+		case KeyEvent.VK_DEAD_OGONEK:
+		case KeyEvent.VK_DEAD_IOTA:
+		case KeyEvent.VK_DEAD_VOICED_SOUND:
+		case KeyEvent.VK_DEAD_SEMIVOICED_SOUND:
+		case KeyEvent.VK_AMPERSAND:
+		case KeyEvent.VK_ASTERISK:
+		case KeyEvent.VK_QUOTEDBL:
+		case KeyEvent.VK_LESS:
+		case KeyEvent.VK_GREATER:
+		case KeyEvent.VK_BRACELEFT:
+		case KeyEvent.VK_BRACERIGHT:
+		case KeyEvent.VK_AT:
+		case KeyEvent.VK_COLON:
+		case KeyEvent.VK_CIRCUMFLEX:
+		case KeyEvent.VK_DOLLAR:
+		case KeyEvent.VK_EURO_SIGN:
+		case KeyEvent.VK_EXCLAMATION_MARK:
+		case KeyEvent.VK_INVERTED_EXCLAMATION_MARK:
+		case KeyEvent.VK_LEFT_PARENTHESIS:
+		case KeyEvent.VK_NUMBER_SIGN:
+		case KeyEvent.VK_PLUS:
+		case KeyEvent.VK_RIGHT_PARENTHESIS:
+		case KeyEvent.VK_UNDERSCORE:
 			return true;
 		default:
 			return false;
@@ -93,39 +227,23 @@ public class KeyEventWorkaround
 			// get rid of keys we never need to handle
 			switch(keyCode)
 			{
-			case KeyEvent.VK_DEAD_GRAVE:
-			case KeyEvent.VK_DEAD_ACUTE:
-			case KeyEvent.VK_DEAD_CIRCUMFLEX:
-			case KeyEvent.VK_DEAD_TILDE:
-			case KeyEvent.VK_DEAD_MACRON:
-			case KeyEvent.VK_DEAD_BREVE:
-			case KeyEvent.VK_DEAD_ABOVEDOT:
-			case KeyEvent.VK_DEAD_DIAERESIS:
-			case KeyEvent.VK_DEAD_ABOVERING:
-			case KeyEvent.VK_DEAD_DOUBLEACUTE:
-			case KeyEvent.VK_DEAD_CARON:
-			case KeyEvent.VK_DEAD_CEDILLA:
-			case KeyEvent.VK_DEAD_OGONEK:
-			case KeyEvent.VK_DEAD_IOTA:
-			case KeyEvent.VK_DEAD_VOICED_SOUND:
-			case KeyEvent.VK_DEAD_SEMIVOICED_SOUND:
 			case '\0':
 				return null;
 			case KeyEvent.VK_ALT:
 				modifiers |= InputEvent.ALT_MASK;
-				return evt;
+				break;
 			case KeyEvent.VK_ALT_GRAPH:
 				modifiers |= InputEvent.ALT_GRAPH_MASK;
-				return evt;
+				break;
 			case KeyEvent.VK_CONTROL:
 				modifiers |= InputEvent.CTRL_MASK;
-				return evt;
+				break;
 			case KeyEvent.VK_SHIFT:
 				modifiers |= InputEvent.SHIFT_MASK;
-				return evt;
+				break;
 			case KeyEvent.VK_META:
 				modifiers |= InputEvent.META_MASK;
-				return evt;
+				break;
 			default:
 				if(!evt.isMetaDown())
 				{
@@ -137,15 +255,7 @@ public class KeyEventWorkaround
 					else if(!evt.isControlDown()
 						&& !evt.isAltDown())
 					{
-						if(keyCode >= KeyEvent.VK_0
-							&& keyCode <= KeyEvent.VK_9)
-						{
-							lastKeyTime = 0L;
-							return null;
-						}
-
-						if(keyCode >= KeyEvent.VK_A
-							&& keyCode <= KeyEvent.VK_Z)
+						if(isPrintable(keyCode))
 						{
 							lastKeyTime = 0L;
 							return null;
@@ -166,8 +276,9 @@ public class KeyEventWorkaround
 				else
 					last = LAST_NOTHING;
 
-				return evt;
+				break;
 			}
+			break;
 		//}}}
 		//{{{ KEY_TYPED...
 		case KeyEvent.KEY_TYPED:
@@ -233,8 +344,7 @@ public class KeyEventWorkaround
 				}
 				modifiers = 0;
 			}
-
-			return evt;
+			break;
 		//}}}
 		//{{{ KEY_RELEASED...
 		case KeyEvent.KEY_RELEASED:
@@ -247,19 +357,19 @@ public class KeyEventWorkaround
 				// where A+TAB window switching activates
 				// the menu bar on Windows.
 				evt.consume();
-				return null;
+				break;
 			case KeyEvent.VK_ALT_GRAPH:
 				modifiers &= ~InputEvent.ALT_GRAPH_MASK;
-				return null;
+				break;
 			case KeyEvent.VK_CONTROL:
 				modifiers &= ~InputEvent.CTRL_MASK;
-				return null;
+				break;
 			case KeyEvent.VK_SHIFT:
 				modifiers &= ~InputEvent.SHIFT_MASK;
-				return null;
+				break;
 			case KeyEvent.VK_META:
 				modifiers &= ~InputEvent.META_MASK;
-				return null;
+				break;
 			case KeyEvent.VK_LEFT:
 			case KeyEvent.VK_RIGHT:
 			case KeyEvent.VK_UP:
@@ -274,11 +384,10 @@ public class KeyEventWorkaround
 					last = LAST_ALT;
 				break;
 			}
-			return evt;
+			break;
 		//}}}
-		default:
-			return evt;
 		}
+		return evt;
 	} //}}}
 
 	//{{{ numericKeypadKey() method
