206de4f30442ee3b86f144eaa002c940223d61b1
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-09-10 19:40:17 +0000

    Reformatted sourcecode, organized imports. Mostly cosmetic changes to make it look nicer.
    added an extra <property file="../build.properties" /> to the build.xml
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@5275 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- jars/QuickNotepad/QuickNotepadPlugin.java
+++ jars/QuickNotepad/QuickNotepadPlugin.java
@@ -25,11 +25,10 @@ import org.gjt.sp.jedit.EditPlugin;
 
 /**
  * The QuickNotepad plugin
- *
+ * 
  * @author John Gellene
  */
-public class QuickNotepadPlugin extends EditPlugin
-{
+public class QuickNotepadPlugin extends EditPlugin {
 	public static final String NAME = "quicknotepad";
 	public static final String OPTION_PREFIX = "options.quicknotepad.";
 }
