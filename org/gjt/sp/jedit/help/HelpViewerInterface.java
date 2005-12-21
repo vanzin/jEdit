f39be75888143d5d524745d5efcba331a800c847
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-12-21 07:44:17 +0000

    Bug # 1386702 (dockablewindowmanager) api docs in HelpViewerInterface cosmetic changes to IncludeGuard
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@3492 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/help/HelpViewerInterface.java
+++ org/gjt/sp/jedit/help/HelpViewerInterface.java
@@ -3,23 +3,21 @@ package org.gjt.sp.jedit.help;
 import java.awt.Component;
 import java.beans.PropertyChangeListener;
 
-import javax.swing.event.ChangeListener;
-import java.beans.PropertyChangeSupport;
-
 /**
- * Interface supported by all HelpViewer classes.  
+ * Interface supported by all HelpViewer classes.
+ * Currently used by @ref infoviewer.InfoViewerPlugin and @ref HelpViewer 
  * 
  * @author ezust
- * @version $id
+ * @version $id$
  */
 public interface HelpViewerInterface 
 {
-	public void gotoURL(String url, boolean addToHistory);
-	public String getShortURL();
-	public String getBaseURL();
-	public void setTitle(String newTitle);
-	public Component getComponent();
-	public void queueTOCReload();
-	public void dispose();
 	public void addPropertyChangeListener(PropertyChangeListener l);
+	public void dispose();
+	public String getBaseURL();
+	public Component getComponent();
+	public String getShortURL();
+	public void gotoURL(String url, boolean addToHistory);
+	public void queueTOCReload();
+	public void setTitle(String newTitle);
 }
