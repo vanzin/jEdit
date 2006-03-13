e2c5f05c46869d91c90bc4f6aaa0eb7e07289048
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-03-13 17:58:13 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@5356 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/JEditTextArea.java
+++ org/gjt/sp/jedit/textarea/JEditTextArea.java
@@ -306,6 +306,12 @@ public class JEditTextArea extends JComponent
 	//{{{ getBuffer() method
 	/**
 	 * Returns the buffer this text area is editing.
+	 * @since jedit 4.3pre3
+	 * 
+	 *  Prior to 4.3pre3, this function returned a "Buffer" type.
+	 *  If this causes your code to break, try calling view.getBuffer() instead of 
+	 *  view.getTextArea().getBuffer().
+	 *  
 	 */
 	public final JEditBuffer getBuffer()
 	{
