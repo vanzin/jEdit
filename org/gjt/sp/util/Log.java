819de06da60e080b33ec6bcbd63012fbd0ba8c74
mediumnet <mediumnet@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-06-21 23:26:31 +0000

    More structured logging
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@3643 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/util/Log.java
+++ org/gjt/sp/util/Log.java
@@ -216,6 +216,19 @@ public class Log
 		return listModel;
 	} //}}}
 
+	/**
+		If an exception is the cause of a call to {@link #log}, then
+		the exception should be explicitly provided so that it can be presented
+		to the (debugging) user in a useful manner
+		(not just the exception message, but also the exception stack trace)
+	*/
+	public static void log(int urgency, Object source, Object message,Throwable exception)
+	{
+		// We can do nicer here, but this is a start...
+		log(urgency,source,message); 
+		log(urgency,source,exception);
+	}
+	
 	//{{{ log() method
 	/**
 	 * Logs a message. This method is thread-safe.<p>
