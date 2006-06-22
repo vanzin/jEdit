5c544acc3d0b559603dca1242298dbd3fce08f36
orutherfurd <orutherfurd@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-06-22 16:21:43 +0000

    * coding convention fixes to org/gjt/sp/util/Log.java * fussy formatting fixes for doc/CHANGES.txt
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@5472 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/util/Log.java
+++ org/gjt/sp/util/Log.java
@@ -216,19 +216,25 @@ public class Log
 		return listModel;
 	} //}}}
 
+	//{{{ log() method
 	/**
-		If an exception is the cause of a call to {@link #log}, then
-		the exception should be explicitly provided so that it can be presented
-		to the (debugging) user in a useful manner
-		(not just the exception message, but also the exception stack trace)
-	*/
-	public static void log(int urgency, Object source, Object message,Throwable exception)
+	 * Logs an exception with a message.
+	 *
+	 * If an exception is the cause of a call to {@link #log}, then
+	 * the exception should be explicitly provided so that it can 
+	 * be presented to the (debugging) user in a useful manner
+	 * (not just the exception message, but also the exception stack trace)
+	 *
+	 * @since jEdit 4.3pre5
+	 */
+	public static void log(int urgency, Object source, Object message, 
+		Throwable exception)
 	{
 		// We can do nicer here, but this is a start...
-		log(urgency,source,message); 
+		log(urgency,source,message);
 		log(urgency,source,exception);
-	}
-	
+	} //}}}
+
 	//{{{ log() method
 	/**
 	 * Logs a message. This method is thread-safe.<p>
