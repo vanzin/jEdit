cb3adc26cd3f2b018a87d362fc09089e3a488778
mediumnet <mediumnet@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-08 14:31:34 +0000

    Some comments on incompatibilities between xselection and jEdit.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@5560 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/Registers.java
+++ org/gjt/sp/jedit/Registers.java
@@ -610,6 +610,22 @@ public class Registers
 		{
 			try
 			{
+
+				if (false) {
+					/*
+						This is to debug clipboard problems.
+						
+						Apparently, jEdit is unable to copy text from clipbard into the current
+						text buffer if the clipboard was filles using the command
+							echo test | xselection CLIPBOARD -
+						under Linux. However, it seems that Java does not offer any
+						data flavor for this clipboard content (under J2RE 1.5.0_06-b05)
+						Thus, copying from clipboard seems to be plainly impossible.
+					*/
+					Log.log(Log.DEBUG,this,"clipboard.getContents(this)="+clipboard.getContents(this)+".");
+					debugListDataFlavors(clipboard.getContents(this));
+				}
+				
 				String selection = (String)(clipboard
 					.getContents(this).getTransferData(
 					DataFlavor.stringFlavor));
