c5afdb40e43c418421156fc48a9a8b1b6b7f9a07
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-25 01:47:40 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@5608 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/proto/jeditresource/Handler.java
+++ org/gjt/sp/jedit/proto/jeditresource/Handler.java
@@ -21,9 +21,24 @@ package org.gjt.sp.jedit.proto.jeditresource;
 
 import java.io.IOException;
 import java.net.*;
-
+/**
+ *  
+ *
+ * One somewhat unconventional requirement of URLStreamHandler classes 
+ * is that the class name and even the package name have certain restrictions. 
+ * You must name the handler class Handler, as in the previous example. 
+ * The package name must include the protocol name as the last dot-separated token.
+ * This way, the Handler is automatically created in a lazy-fashion by the default
+ * URLStreamHandlerFactory.
+ *
+ * see http://java.sun.com/developer/onlineTraining/protocolhandlers/
+ * 
+ * You should never need to create an instance of this class directly. 
+ *
+ */
 public class Handler extends URLStreamHandler
 {
+	
 	public URLConnection openConnection(URL url)
 		throws IOException
 	{
