36ac265fce50ba62a653f752a6f0fbe35e9d5e69
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-10-04 03:02:45 +0000

    Updated api docs...
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@7168 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/msg/CaretChanging.java
+++ org/gjt/sp/jedit/msg/CaretChanging.java
@@ -1,15 +1,53 @@
+/*
+ * CaretChanging.java - caret changing (specialized text area update message)
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2006 Alan Ezust
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package org.gjt.sp.jedit.msg;
 
 import org.gjt.sp.jedit.textarea.JEditTextArea;
+
+/**
+   a message emitted by various plugins, including
+   CodeBrowser, Tags, CscopeFinder, and SideKick, whenever the plugin
+   takes an action to change the caret to another location. 
+   It is not necessary to emit this signal when changing buffers,
+   since the BufferChanging is automatically sent by jEdit.
+   However, if you wish to navigate from one point in a buffer to 
+   another, without changing buffers, it is appropriate to 
+   EditBus.send(new CaretChanging(view.getTextArea()));
+   
+   @author ezust
+   @since jEdit 4.3pre3
+*/
 
 public class CaretChanging extends TextAreaUpdate
 {
 	JEditTextArea jta;
 	int caret;
-	public CaretChanging(JEditTextArea jta) {
+	public CaretChanging(JEditTextArea jta) 
+    {
 		super(jta, TextAreaUpdate.CARET_CHANGING);
 		this.jta = jta;
 		caret = jta.getCaretPosition();
 	}
 
 }
+
