5ff5523329d22707c737bd1aaec38ba4d9a20c1c
vampire0 <vampire0@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-10-20 12:01:46 +0000

    Added JavaDoc relationship about addSizeSaver() and saveGeometry()
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@7625 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/GUIUtilities.java
+++ org/gjt/sp/jedit/GUIUtilities.java
@@ -419,7 +419,6 @@ public class GUIUtilities
 		return mi;
 	} //}}}
 
-	
 	//{{{ loadToolBar() method
 	/**
 	 * Creates a toolbar.
@@ -1212,9 +1211,12 @@ public class GUIUtilities
 	 * Saves a window's geometry to the properties.
 	 * The geometry is saved to the <code><i>name</i>.x</code>,
 	 * <code><i>name</i>.y</code>, <code><i>name</i>.width</code> and
-	 * <code><i>name</i>.height</code> properties.
+	 * <code><i>name</i>.height</code> properties.<br />
+	 * For Frame's and descendents use {@link #addSizeSaver(Frame,String)} to save the sizes
+	 * correct even if the Frame is in maximized or iconified state.
 	 * @param win The window to load geometry from
 	 * @param name The name of the window 
+	 * @see #addSizeSaver(Frame,String)
 	 */
 	public static void saveGeometry(Window win, String name) {
 		saveGeometry (win, win.getParent(), name);
@@ -1225,10 +1227,13 @@ public class GUIUtilities
 	 * Saves a window's geometry to the properties.
 	 * The geometry is saved to the <code><i>name</i>.x</code>,
 	 * <code><i>name</i>.y</code>, <code><i>name</i>.width</code> and
-	 * <code><i>name</i>.height</code> properties.
+	 * <code><i>name</i>.height</code> properties.<br />
+	 * For Frame's and descendents use {@link #addSizeSaver(Frame,Container,String)} to save the sizes
+	 * correct even if the Frame is in maximized or iconified state.
 	 * @param win The window to load geometry from
 	 * @param parent The parent frame to be relative to.
 	 * @param name The name of the window 
+	 * @see #addSizeSaver(Frame,Container,String)
 	 */
 	public static void saveGeometry(Window win, Container parent, String name)
 	{
@@ -1568,11 +1573,12 @@ public class GUIUtilities
 
 	//{{{ addSizeSaver() method
 	/**
-	 * Adds a SizeSaver to the specified Frame.
+	* Adds a SizeSaver to the specified Frame. For non-Frame's use {@link #saveGeometry(Window,String)}
 	 *
 	 * @param frame The Frame for which to save the size
 	 * @param name The prefix for the settings
 	 * @since jEdit 4.3pre6
+	 * @see #saveGeometry(Window,String)
 	 */
 	public static void addSizeSaver(Frame frame, String name) 
 	{
@@ -1581,12 +1587,13 @@ public class GUIUtilities
 
 	//{{{ addSizeSaver() method
 	/**
-	 * Adds a SizeSaver to the specified Frame.
+	 * Adds a SizeSaver to the specified Frame. For non-Frame's use {@link #saveGeometry(Window,Container,String)}
 	 *
 	 * @param frame The Frame for which to save the size
 	 * @param parent The parent to be relative to
 	 * @param name The prefix for the settings
 	 * @since jEdit 4.3pre7
+	 * @see #saveGeometry(Window,Container,String)
 	 */
 	public static void addSizeSaver(Frame frame, Container parent, String name)
 	{
@@ -1637,11 +1644,13 @@ public class GUIUtilities
 
 	//{{{ SizeSaver class
 	/**
-	 * A combined ComponentListener and WindowStateListener to continually save a Frames size.<p>
+	 * A combined ComponentListener and WindowStateListener to continually save a Frames size.<br />
+	 * For non-Frame's use {@link #saveGeometry(Window,String)}
 	 *
 	 * @author Björn Kautler
 	 * @version $Id$
 	 * @since jEdit 4.3pre6
+	 * @see #saveGeometry(Window,Container,String)
 	 */
 	static class SizeSaver extends ComponentAdapter implements WindowStateListener
 	{
