548eab58a7cc492909ed81381ee812a91f9cd969
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-06-05 22:01:21 +0000

    some javadoc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@9688 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/Selection.java
+++ org/gjt/sp/jedit/textarea/Selection.java
@@ -179,8 +179,26 @@ public abstract class Selection implements Cloneable
 	 */
 	abstract int setText(JEditBuffer buffer, String text);
 
+	/**
+	 * Called when text is inserted into the buffer.
+	 * @param buffer The buffer in question
+	 * @param startLine The first line
+	 * @param start The start offset, from the beginning of the buffer
+	 * @param numLines The number of lines inserted
+	 * @param length The number of characters inserted
+	 * @return <code>true</code> if the selection was changed
+	 */
 	abstract boolean contentInserted(JEditBuffer buffer, int startLine, int start,
 		int numLines, int length);
+
+	/**
+	 * Called when text is removed from the buffer.
+	 * @param buffer The buffer in question
+	 * @param startLine The first line
+	 * @param start The start offset, from the beginning of the buffer
+	 * @param numLines The number of lines removed
+	 * @param length The number of characters removed
+	 */
 	abstract boolean contentRemoved(JEditBuffer buffer, int startLine, int start,
 		int numLines, int length);
 	//}}}
