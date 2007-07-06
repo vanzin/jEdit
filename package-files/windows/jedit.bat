79912d6964a49346c66b75f5c016fb24e9aec0c0
Vampire0 <Vampire0@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-07-06 15:44:46 +0000

    improved the startup scripts and added the higher max memory setting to the Mac OS X bundle
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@9932 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- package-files/windows/jedit.bat
+++ package-files/windows/jedit.bat
@@ -1,9 +1,2 @@
 @echo off
-:LOOP
-	"{app}\launchstub.exe" "{javaw.exe}" -Xms64M -Xmx192M -jar "{jedit.jar}" -reuseview "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
-	set i=
-	:COUNT_LOOP
-		set i=%i%-
-		shift
-	if not "%i%" == "---------" goto COUNT_LOOP
-if not "%1" == "" goto LOOP
+start "jEdit startup" "C:\Programme\Java\jdk1.5.0\bin\javaw.exe" -Xms64M -Xmx192M -jar "C:\Programme\jEdit\jedit.jar" -reuseview %*
