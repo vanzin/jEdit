4b3a7e82f08c4ff4bd61bfef67f119ca9244b010
Vampire0 <Vampire0@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-07-08 13:01:53 +0000

    Accidently used hardcoded paths
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@9959 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- package-files/windows/jedit.bat
+++ package-files/windows/jedit.bat
@@ -1,2 +1,2 @@
 @echo off
-start "jEdit startup" "C:\Programme\Java\jdk1.5.0\bin\javaw.exe" -Xms64M -Xmx192M -jar "C:\Programme\jEdit\jedit.jar" -reuseview %*
+start "jEdit startup" "{javaw.exe}" -Xms64M -Xmx192M -jar "{jedit.jar}" -reuseview %*
