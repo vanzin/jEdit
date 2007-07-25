03903cd92765cab5c63f24da91f812f6b9abf4e1
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-07-25 19:39:35 +0000

    Resurrected a workaround for a focus bug when a completion popup is closed.
    It was once dropped at r10021 because I can't see any sense of the code
    and any problems in JDK 6 on Windows XP. But the problem was reported on
    jedit-devel.
    http://sourceforge.net/mailarchive/forum.php?thread_name=fdd462d30707161746i216ddf7et47b06695c920a275%40mail.gmail.com&forum_name=jedit-devel
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@10119 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/CompletionPopup.java
+++ org/gjt/sp/jedit/gui/CompletionPopup.java
@@ -44,6 +44,7 @@ import javax.swing.JWindow;
 import javax.swing.ListSelectionModel;
 import javax.swing.ListCellRenderer;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
 
 import org.gjt.sp.jedit.GUIUtilities;
 import org.gjt.sp.jedit.View;
@@ -137,6 +138,23 @@ public class CompletionPopup extends JWindow
 		{
 			view.setKeyEventInterceptor(null);
 			super.dispose();
+
+			// This is a workaround to ensure setting the
+			// focus back to the textArea. Without this, the
+			// focus gets lost after closing the popup in
+			// some environments. It seems to be a bug in
+			// J2SE 1.4 or 5.0. Probably it relates to the
+			// following one.
+			// "Frame does not receives focus after closing
+			// of the owned window"
+			// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4810575
+			SwingUtilities.invokeLater(new Runnable()
+			{
+				public void run()
+				{
+					view.getTextArea().requestFocus();
+				}
+			});
 		}
 	} //}}}
 
