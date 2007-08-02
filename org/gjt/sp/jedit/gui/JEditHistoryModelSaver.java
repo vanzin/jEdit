4e5968eb5829bad0be2349954c55dc87099390e7
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-08-02 00:28:59 +0000

    Changed the encoding of "history" file to UTF-8 to allow characters which are not covered by the system default encoding. (SF.net Bug #1691638)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@10282 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/JEditHistoryModelSaver.java
+++ org/gjt/sp/jedit/gui/JEditHistoryModelSaver.java
@@ -27,6 +27,8 @@ import org.gjt.sp.jedit.MiscUtilities;
 import org.gjt.sp.jedit.jEdit;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.CharacterCodingException;
 import java.util.*;
 
 /**
@@ -55,45 +57,30 @@ public class JEditHistoryModelSaver implements HistoryModelSaver
 			models = Collections.synchronizedMap(new HashMap<String, HistoryModel>());
 
 		BufferedReader in = null;
-
 		try
 		{
-			in = new BufferedReader(new FileReader(history));
-
-			HistoryModel currentModel = null;
-			String line;
-
-			while((line = in.readLine()) != null)
+			// Try loading with UTF-8 and fallback to the system
+			// default encoding to load a history which was made by
+			// an old version as well.
+			try
 			{
-				if(line.length() > 0 && line.charAt(0) == '[' && line.charAt(line.length() - 1) == ']')
-				{
-					if(currentModel != null)
-					{
-						models.put(currentModel.getName(),
-							currentModel);
-					}
-
-					String modelName = MiscUtilities
-						.escapesToChars(line.substring(
-						1,line.length() - 1));
-					currentModel = new HistoryModel(
-						modelName);
-				}
-				else if(currentModel == null)
-				{
-					throw new IOException("History data starts"
-						+ " before model name");
-				}
-				else
-				{
-					currentModel.addElement(MiscUtilities
-						.escapesToChars(line));
-				}
+				// Pass the decoder explicitly to report a decode error
+				// as an exception instead of replacing with \xFFFD.
+				in = new BufferedReader(new InputStreamReader(
+					new FileInputStream(history),
+					Charset.forName("UTF-8").newDecoder()));
+				models.putAll(loadFromReader(in));
 			}
-
-			if(currentModel != null)
+			catch(CharacterCodingException e)
 			{
-				models.put(currentModel.getName(),currentModel);
+				// It seems to be made by an old version of jEdit.
+				in.close();
+				Log.log(Log.MESSAGE,HistoryModel.class,
+					"Failed to load history with UTF-8." +
+					" Fallbacking to the system default encoding.");
+
+				in = new BufferedReader(new FileReader(history));
+				models.putAll(loadFromReader(in));
 			}
 		}
 		catch(FileNotFoundException fnf)
@@ -134,7 +121,8 @@ public class JEditHistoryModelSaver implements HistoryModelSaver
 
 		try
 		{
-			out = new BufferedWriter(new FileWriter(file1));
+			out = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(file1), "UTF-8"));
 
 			if(models != null)
 			{
@@ -184,6 +172,52 @@ public class JEditHistoryModelSaver implements HistoryModelSaver
 	private static final String TO_ESCAPE = "\r\n\t\\\"'[]";
 	private static File history;
 	private static long historyModTime;
+
+	//{{{ loadFromReader() method
+	private static Map<String, HistoryModel> loadFromReader(BufferedReader in)
+		throws IOException
+	{
+		Map<String, HistoryModel> result = new HashMap<String, HistoryModel>();
+
+		HistoryModel currentModel = null;
+		String line;
+
+		while((line = in.readLine()) != null)
+		{
+			if(line.length() > 0 && line.charAt(0) == '[' && line.charAt(line.length() - 1) == ']')
+			{
+				if(currentModel != null)
+				{
+					result.put(currentModel.getName(),
+						currentModel);
+				}
+
+				String modelName = MiscUtilities
+					.escapesToChars(line.substring(
+					1,line.length() - 1));
+				currentModel = new HistoryModel(
+					modelName);
+			}
+			else if(currentModel == null)
+			{
+				throw new IOException("History data starts"
+					+ " before model name");
+			}
+			else
+			{
+				currentModel.addElement(MiscUtilities
+					.escapesToChars(line));
+			}
+		}
+
+		if(currentModel != null)
+		{
+			result.put(currentModel.getName(),currentModel);
+		}
+
+		return result;
+	} //}}}
+
 	//}}}
 
 }
