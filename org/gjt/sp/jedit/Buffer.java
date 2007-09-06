c8b111939c198f1350c7bcacec3004c8818bcafa
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-09-06 18:03:18 +0000

    Applied patch 1788847 (edit syntax highlight under caret)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@10456 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/Buffer.java
+++ org/gjt/sp/jedit/Buffer.java
@@ -32,6 +32,7 @@ import java.net.Socket;
 import java.util.*;
 import org.gjt.sp.jedit.browser.VFSBrowser;
 import org.gjt.sp.jedit.buffer.*;
+import org.gjt.sp.jedit.gui.StyleEditor;
 import org.gjt.sp.jedit.io.*;
 import org.gjt.sp.jedit.msg.*;
 import org.gjt.sp.jedit.syntax.*;
@@ -2008,6 +2009,52 @@ public class Buffer extends JEditBuffer
 			}
 		} //}}}
 	} //}}}
+
+	//{{{ editSyntaxStyle() method
+	/**
+	 * Edit the syntax style of the token under the caret.
+	 * 
+	 * @param textArea the textarea where your caret is
+	 * @since jEdit 4.3pre11
+	 */
+	void editSyntaxStyle(JEditTextArea textArea) {
+		int lineNum = textArea.getCaretLine();
+		int start = getLineStartOffset(lineNum);
+		int position = textArea.getCaretPosition();
+		
+		DefaultTokenHandler tokenHandler = new DefaultTokenHandler();
+		markTokens(lineNum,tokenHandler);
+		Token token = tokenHandler.getTokens();
+	
+		while(token.id != Token.END)
+		{
+			int next = start + token.length;
+			if (start <= position && next > position)
+				break;
+			start = next;
+			token = token.next;
+		}
+		if (token.id == Token.END || token.id == Token.NULL)
+		{
+			JOptionPane.showMessageDialog(textArea,
+				jEdit.getProperty("syntax-style-no-token.message"),
+				jEdit.getProperty("syntax-style-no-token.title"),
+				JOptionPane.PLAIN_MESSAGE);
+			return;
+		}
+		String typeName = Token.tokenToString(token.id);
+		String property = "view.style." + typeName.toLowerCase();
+		SyntaxStyle currentStyle = GUIUtilities.parseStyle(
+				jEdit.getProperty(property), "Dialog",12);
+		SyntaxStyle style = new StyleEditor(
+				(JFrame)SwingUtilities.getAncestorOfClass(
+						JFrame.class, textArea),
+				currentStyle, typeName).getStyle();
+		if(style != null)
+			jEdit.setProperty(property, GUIUtilities.getStyleString(style));
+		jEdit.propertiesChanged();
+	} //}}}
+	//}}}
 
 	//}}}
 }
--- org/gjt/sp/jedit/options/SyntaxHiliteOptionPane.java
+++ org/gjt/sp/jedit/options/SyntaxHiliteOptionPane.java
@@ -35,6 +35,7 @@ import java.util.Collections;
 import org.gjt.sp.jedit.syntax.*;
 import org.gjt.sp.jedit.gui.ColorWellButton;
 import org.gjt.sp.jedit.gui.EnhancedDialog;
+import org.gjt.sp.jedit.gui.StyleEditor;
 import org.gjt.sp.jedit.*;
 //}}}
 
@@ -115,9 +116,9 @@ public class SyntaxHiliteOptionPane extends AbstractOptionPane
 				return;
 
 			SyntaxStyle style = new StyleEditor(
-				SyntaxHiliteOptionPane.this,
+					GUIUtilities.getParentDialog(SyntaxHiliteOptionPane.this),
 				(SyntaxStyle)styleModel.getValueAt(
-				row,1)).getStyle();
+				row,1), (String) styleModel.getValueAt(row, 0)).getStyle();
 			if(style != null)
 				styleModel.setValueAt(style,row,1);
 		}
@@ -288,130 +289,3 @@ class StyleTableModel extends AbstractTableModel
 	} //}}}
 } //}}}
 
-//{{{ StyleEditor class
-class StyleEditor extends EnhancedDialog implements ActionListener
-{
-	//{{{ StyleEditor constructor
-	StyleEditor(Component comp, SyntaxStyle style)
-	{
-		super(GUIUtilities.getParentDialog(comp),
-			jEdit.getProperty("style-editor.title"),true);
-
-		JPanel content = new JPanel(new BorderLayout(12,12));
-		content.setBorder(new EmptyBorder(12,12,12,12));
-		setContentPane(content);
-
-		JPanel panel = new JPanel(new GridLayout(4,2,12,12));
-
-		italics = new JCheckBox(jEdit.getProperty("style-editor.italics"));
-		italics.setSelected(style.getFont().isItalic());
-		panel.add(italics);
-		panel.add(new JLabel());
-
-		bold = new JCheckBox(jEdit.getProperty("style-editor.bold"));
-		bold.setSelected(style.getFont().isBold());
-		panel.add(bold);
-		panel.add(new JLabel());
-
-		Color fg = style.getForegroundColor();
-
-		fgColorCheckBox = new JCheckBox(jEdit.getProperty("style-editor.fgColor"));
-		fgColorCheckBox.setSelected(fg != null);
-		fgColorCheckBox.addActionListener(this);
-		panel.add(fgColorCheckBox);
-
-		fgColor = new ColorWellButton(fg);
-		fgColor.setEnabled(fg != null);
-		panel.add(fgColor);
-
-		Color bg = style.getBackgroundColor();
-		bgColorCheckBox = new JCheckBox(jEdit.getProperty("style-editor.bgColor"));
-		bgColorCheckBox.setSelected(bg != null);
-		bgColorCheckBox.addActionListener(this);
-		panel.add(bgColorCheckBox);
-
-		bgColor = new ColorWellButton(bg);
-		bgColor.setEnabled(bg != null);
-		panel.add(bgColor);
-
-		content.add(BorderLayout.CENTER,panel);
-
-		Box box = new Box(BoxLayout.X_AXIS);
-		box.add(Box.createGlue());
-		box.add(ok = new JButton(jEdit.getProperty("common.ok")));
-		getRootPane().setDefaultButton(ok);
-		ok.addActionListener(this);
-		box.add(Box.createHorizontalStrut(6));
-		box.add(cancel = new JButton(jEdit.getProperty("common.cancel")));
-		cancel.addActionListener(this);
-		box.add(Box.createGlue());
-
-		content.add(BorderLayout.SOUTH,box);
-
-		pack();
-		setLocationRelativeTo(GUIUtilities.getParentDialog(comp));
-
-		setResizable(false);
-		setVisible(true);
-	} //}}}
-
-	//{{{ actionPerformed() method
-	public void actionPerformed(ActionEvent evt)
-	{
-		Object source = evt.getSource();
-		if(source == ok)
-			ok();
-		else if(source == cancel)
-			cancel();
-		else if(source == fgColorCheckBox)
-			fgColor.setEnabled(fgColorCheckBox.isSelected());
-		else if(source == bgColorCheckBox)
-			bgColor.setEnabled(bgColorCheckBox.isSelected());
-	} //}}}
-
-	//{{{ ok() method
-	public void ok()
-	{
-		okClicked = true;
-		dispose();
-	} //}}}
-
-	//{{{ cancel() method
-	public void cancel()
-	{
-		dispose();
-	} //}}}
-
-	//{{{ getStyle() method
-	public SyntaxStyle getStyle()
-	{
-		if(!okClicked)
-			return null;
-
-		Color foreground = (fgColorCheckBox.isSelected()
-			? fgColor.getSelectedColor()
-			: null);
-
-		Color background = (bgColorCheckBox.isSelected()
-			? bgColor.getSelectedColor()
-			: null);
-
-		return new SyntaxStyle(foreground,background,
-				new Font("Dialog",
-				(italics.isSelected() ? Font.ITALIC : 0)
-				| (bold.isSelected() ? Font.BOLD : 0),
-				12));
-	} //}}}
-
-	//{{{ Private members
-	private JCheckBox italics;
-	private JCheckBox bold;
-	private JCheckBox fgColorCheckBox;
-	private ColorWellButton fgColor;
-	private JCheckBox bgColorCheckBox;
-	private ColorWellButton bgColor;
-	private JButton ok;
-	private JButton cancel;
-	private boolean okClicked;
-	//}}}
-} //}}}
