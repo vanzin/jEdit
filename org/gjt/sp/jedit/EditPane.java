70699b8e01a1783499b263c0fd463ec2952d0581
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-11-07 21:13:44 +0000

    Added a new method EditPane.initPainter()
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@10993 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/EditPane.java
+++ org/gjt/sp/jedit/EditPane.java
@@ -704,51 +704,7 @@ public class EditPane extends JPanel implements EBComponent
 	{
 		TextAreaPainter painter = textArea.getPainter();
 
-		painter.setFont(jEdit.getFontProperty("view.font"));
-		painter.setStructureHighlightEnabled(jEdit.getBooleanProperty(
-			"view.structureHighlight"));
-		painter.setStructureHighlightColor(
-			jEdit.getColorProperty("view.structureHighlightColor"));
-		painter.setEOLMarkersPainted(jEdit.getBooleanProperty(
-			"view.eolMarkers"));
-		painter.setEOLMarkerColor(
-			jEdit.getColorProperty("view.eolMarkerColor"));
-		painter.setWrapGuidePainted(jEdit.getBooleanProperty(
-			"view.wrapGuide"));
-		painter.setWrapGuideColor(
-			jEdit.getColorProperty("view.wrapGuideColor"));
-		painter.setCaretColor(
-			jEdit.getColorProperty("view.caretColor"));
-		painter.setSelectionColor(
-			jEdit.getColorProperty("view.selectionColor"));
-		painter.setMultipleSelectionColor(
-			jEdit.getColorProperty("view.multipleSelectionColor"));
-		painter.setBackground(
-			jEdit.getColorProperty("view.bgColor"));
-		painter.setForeground(
-			jEdit.getColorProperty("view.fgColor"));
-		painter.setBlockCaretEnabled(jEdit.getBooleanProperty(
-			"view.blockCaret"));
-		painter.setLineHighlightEnabled(jEdit.getBooleanProperty(
-			"view.lineHighlight"));
-		painter.setLineHighlightColor(
-			jEdit.getColorProperty("view.lineHighlightColor"));
-		painter.setAntiAlias(new AntiAlias(jEdit.getProperty("view.antiAlias")));
-		painter.setFractionalFontMetricsEnabled(jEdit.getBooleanProperty(
-			"view.fracFontMetrics"));
-
-		String defaultFont = jEdit.getProperty("view.font");
-		int defaultFontSize = jEdit.getIntegerProperty("view.fontsize",12);
-		painter.setStyles(GUIUtilities.loadStyles(defaultFont,defaultFontSize));
-
-		SyntaxStyle[] foldLineStyle = new SyntaxStyle[4];
-		for(int i = 0; i <= 3; i++)
-		{
-			foldLineStyle[i] = GUIUtilities.parseStyle(
-				jEdit.getProperty("view.style.foldLine." + i),
-				defaultFont,defaultFontSize);
-		}
-		painter.setFoldLineStyle(foldLineStyle);
+		initPainter(painter);
 		Gutter gutter = textArea.getGutter();
 		gutter.setExpanded(jEdit.getBooleanProperty(
 			"view.gutter.lineNumbers"));
@@ -839,6 +795,62 @@ public class EditPane extends JPanel implements EBComponent
 			bufferSwitcher.setMaximumRowCount(jEdit.getIntegerProperty(
 				"bufferSwitcher.maxRowCount",10));
 		}
+	} //}}}
+
+	//{{{ propertiesChanged() method
+	/**
+	 * Init the painter of a textarea.
+	 *
+	 * @param painter the painter of a textarea
+	 * @since jEdit 4.3pre12
+	 */
+	public static void initPainter(TextAreaPainter painter)
+	{
+		painter.setFont(jEdit.getFontProperty("view.font"));
+		painter.setStructureHighlightEnabled(jEdit.getBooleanProperty(
+			"view.structureHighlight"));
+		painter.setStructureHighlightColor(
+			jEdit.getColorProperty("view.structureHighlightColor"));
+		painter.setEOLMarkersPainted(jEdit.getBooleanProperty(
+			"view.eolMarkers"));
+		painter.setEOLMarkerColor(
+			jEdit.getColorProperty("view.eolMarkerColor"));
+		painter.setWrapGuidePainted(jEdit.getBooleanProperty(
+			"view.wrapGuide"));
+		painter.setWrapGuideColor(
+			jEdit.getColorProperty("view.wrapGuideColor"));
+		painter.setCaretColor(
+			jEdit.getColorProperty("view.caretColor"));
+		painter.setSelectionColor(
+			jEdit.getColorProperty("view.selectionColor"));
+		painter.setMultipleSelectionColor(
+			jEdit.getColorProperty("view.multipleSelectionColor"));
+		painter.setBackground(
+			jEdit.getColorProperty("view.bgColor"));
+		painter.setForeground(
+			jEdit.getColorProperty("view.fgColor"));
+		painter.setBlockCaretEnabled(jEdit.getBooleanProperty(
+			"view.blockCaret"));
+		painter.setLineHighlightEnabled(jEdit.getBooleanProperty(
+			"view.lineHighlight"));
+		painter.setLineHighlightColor(
+			jEdit.getColorProperty("view.lineHighlightColor"));
+		painter.setAntiAlias(new AntiAlias(jEdit.getProperty("view.antiAlias")));
+		painter.setFractionalFontMetricsEnabled(jEdit.getBooleanProperty(
+			"view.fracFontMetrics"));
+
+		String defaultFont = jEdit.getProperty("view.font");
+		int defaultFontSize = jEdit.getIntegerProperty("view.fontsize",12);
+		painter.setStyles(GUIUtilities.loadStyles(defaultFont,defaultFontSize));
+
+		SyntaxStyle[] foldLineStyle = new SyntaxStyle[4];
+		for(int i = 0; i <= 3; i++)
+		{
+			foldLineStyle[i] = GUIUtilities.parseStyle(
+				jEdit.getProperty("view.style.foldLine." + i),
+				defaultFont,defaultFontSize);
+		}
+		painter.setFoldLineStyle(foldLineStyle);
 	} //}}}
 
 	//{{{ loadBufferSwitcher() method
