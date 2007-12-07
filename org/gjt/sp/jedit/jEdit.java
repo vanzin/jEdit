44307d68c932e3e88345e4d23477069061511160
Vampire0 <Vampire0@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-12-07 23:29:21 +0000

    Updated the version number to 4.3pre13.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@11208 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -78,7 +78,7 @@ public class jEdit
 	public static String getBuild()
 	{
 		// (major).(minor).(<99 = preX, 99 = final).(bug fix)
-		return "04.03.12.00";
+		return "04.03.13.00";
 	} //}}}
 
 	//{{{ main() method
