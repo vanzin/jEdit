310584f422226a5a6a5a1972cb565cec1a35785f
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-02-11 12:36:10 +0000

    addFoldHandler method added
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@11776 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/buffer/DefaultFoldHandlerProvider.java
+++ org/gjt/sp/jedit/buffer/DefaultFoldHandlerProvider.java
@@ -55,4 +55,15 @@ public class DefaultFoldHandlerProvider implements FoldHandlerProvider
 	{
 		return folds.keySet().toArray(new String[folds.size()]); 
 	}
+	
+	/**
+	 * Add a new FoldHander.
+	 *
+	 * @param foldHandler the new foldHandler
+	 * @since jEdit 4.3pre13
+	 */
+	public void addFoldHandler(FoldHandler foldHandler)
+	{
+		folds.put(foldHandler.getName(), foldHandler);
+	}
 }
