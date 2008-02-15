d8bc7cbbdd531c74832395ea6f13862afa7d9d27
Vampire0 <Vampire0@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-02-15 00:07:23 +0000

    Fixed a bug where screens were shown off-screen because of a typo in GUIUtilities.adjustForScreenBounds()
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@11797 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/GUIUtilities.java
+++ org/gjt/sp/jedit/GUIUtilities.java
@@ -1033,19 +1033,29 @@ public class GUIUtilities
 		// Make sure the window is displayed in visible region
 		Rectangle osbounds = OperatingSystem.getScreenBounds(desired);
 
-		if(desired.x < osbounds.x || desired.x+desired.width
-			> desired.x + osbounds.width)
+		if (desired.width > osbounds.width)
 		{
-			if (desired.width > osbounds.width)
-				desired.width = osbounds.width;
-			desired.x = (osbounds.width - desired.width) / 2;
+			desired.width = osbounds.width;
 		}
-		if(desired.y < osbounds.y || desired.y+desired.height
-			> osbounds.y + osbounds.height)
+		if (desired.x < osbounds.x)
 		{
-			if (desired.height >= osbounds.height)
-				desired.height = osbounds.height;
-			desired.y = (osbounds.height - desired.height) / 2;
+			desired.x = osbounds.x;
+		}
+		if (desired.x + desired.width > osbounds.x + osbounds.width)
+		{
+			desired.x = osbounds.x + osbounds.width - desired.width;
+		}
+		if (desired.height > osbounds.height)
+		{
+			desired.height = osbounds.height;
+		}
+		if (desired.y < osbounds.y)
+		{
+			desired.y = osbounds.y;
+		}
+		if (desired.y + desired.height > osbounds.y + osbounds.height)
+		{
+			desired.y = osbounds.y + osbounds.height - desired.height;
 		}
 	} //}}}
 
