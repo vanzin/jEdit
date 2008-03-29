c9bfb6ff9deba673cec88bd53d00d1480fed2147
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-03-29 18:16:21 +0000

    Comments and literals are now filtered in RegexpIndentRule to retire complications in regexps to skip them.
    This change was first submitted as SF.net patch #1868690.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@12335 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/indent/RegexpIndentRule.java
+++ org/gjt/sp/jedit/indent/RegexpIndentRule.java
@@ -27,7 +27,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+import javax.swing.text.Segment;
+
 import org.gjt.sp.jedit.buffer.JEditBuffer;
+import org.gjt.sp.jedit.syntax.Token;
+import org.gjt.sp.jedit.syntax.TokenHandler;
+import org.gjt.sp.jedit.syntax.TokenMarker;
 
 /**
  * @author Slava Pestov
@@ -57,13 +62,13 @@ public class RegexpIndentRule implements IndentRule
 		List<IndentAction> indentActions)
 	{
 		if(thisAction != null
-			&& isMatch(buffer.getLineText(thisLineIndex)))
+			&& lineMatches(buffer, thisLineIndex))
 		{
 			indentActions.add(thisAction);
 		}
 		if(prevAction != null
 			&& prevLineIndex != -1
-			&& isMatch(buffer.getLineText(prevLineIndex)))
+			&& lineMatches(buffer, prevLineIndex))
 		{
 			indentActions.add(prevAction);
 			if (collapse)
@@ -71,7 +76,7 @@ public class RegexpIndentRule implements IndentRule
 		}
 		if(prevPrevAction != null
 			&& prevPrevLineIndex != -1
-			&& isMatch(buffer.getLineText(prevPrevLineIndex)))
+			&& lineMatches(buffer, prevPrevLineIndex))
 		{
 			indentActions.add(prevPrevAction);
 			if (collapse)
@@ -80,6 +85,11 @@ public class RegexpIndentRule implements IndentRule
 	} //}}}
 
 	//{{{ isMatch() method
+	/**
+	 * @deprecated
+	 *   Do not use this. This is here only for compatibility.
+	 */
+	@Deprecated
 	public boolean isMatch(String line)
 	{
 		Matcher m = regexp.matcher(line);
@@ -96,4 +106,70 @@ public class RegexpIndentRule implements IndentRule
 	private IndentAction prevPrevAction, prevAction, thisAction;
 	private Pattern regexp;
 	private boolean collapse;
+
+	//{{{ class TokenFilter
+	/**
+	 * A filter which removes non syntactic characters in comments
+	 * or literals which might confuse regexp matchings for indent.
+	 */
+	private static class TokenFilter implements TokenHandler
+	{
+		public StringBuilder result;
+
+		public TokenFilter(int originalLength)
+		{
+			result = new StringBuilder(originalLength);
+		}
+
+		public void handleToken(Segment seg
+			, byte id, int offset, int length
+			, TokenMarker.LineContext context)
+		{
+			// Avoid replacing an empty token into a non empty
+			// string.
+			if (length <= 0)
+			{
+				return;
+			}
+			
+			switch (id)
+			{
+			case Token.COMMENT1:
+			case Token.COMMENT2:
+			case Token.COMMENT3:
+			case Token.COMMENT4:
+				// Replace any comments to a white space
+				// so that they are simply ignored.
+				result.append(' ');
+				break;
+			case Token.LITERAL1:
+			case Token.LITERAL2:
+			case Token.LITERAL3:
+			case Token.LITERAL4:
+				// Replace any literals to a '0' which means
+				// a simple integer literal in most programming
+				// languages.
+				result.append('0');
+				break;
+			default:
+				result.append(seg.array
+					, seg.offset + offset
+					, length);
+				break;
+			}
+		}
+
+		public void setLineContext(TokenMarker.LineContext lineContext)
+		{
+		}
+	} //}}}
+
+	//{{{ lineMatches() method
+	private boolean lineMatches(JEditBuffer buffer, int lineIndex)
+	{
+		TokenFilter filter
+			= new TokenFilter(buffer.getLineLength(lineIndex));
+		buffer.markTokens(lineIndex, filter);
+		return regexp.matcher(filter.result).matches();
+	} //}}}
 }
