94b69e76410c78dd65e4c316d38207949831485d
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-05-27 03:29:27 +0000

    [ 1910061 ] 4.3pre13, r11659 - Center Caret On Screen action
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@12710 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/TextArea.java
+++ org/gjt/sp/jedit/textarea/TextArea.java
@@ -2283,12 +2283,31 @@ forward_scan:	do
 	 */
 	public void centerCaret()
 	{
+		int offset = getScreenLineStartOffset(visibleLines >> 1);
+		if(offset == -1)
+			getToolkit().beep();
+		else
+			setCaretPosition(offset);		
+	} //}}}
+
+	// {{{ scrollToCurrent() method
+	/**
+	 * Tries to scroll the textArea so that the caret is centered on the screen. 
+	 * Sometimes gets confused by folds but at least makes the caret visible and 
+	 * guesses better on subsequent attempts.
+	 * 
+	 * @since jEdit 4.3pre15
+	 */
+	public void scrollToCurrent() 
+	{
+		if (getDisplayManager().isLineVisible(getCaretLine()))
+			getDisplayManager().expandFold(getCaretLine(),true);
 		int physicalLine = getCaretLine();
 		int midPhysicalLine = getPhysicalLineOfScreenLine(visibleLines >> 1);
 		int diff = physicalLine -  midPhysicalLine;
 		setFirstLine(getFirstLine() + diff);
-	} //}}}
-
+	} // }}}
+	
 	//{{{ setCaretPosition() method
 	/**
 	 * Sets the caret position and deactivates the selection.
