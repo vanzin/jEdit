6be60545838737f5daaeea49b5952ea378db5fe4
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-06-28 22:36:37 +0000

    Clarified API docs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@12961 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/DynamicContextMenuService.java
+++ org/gjt/sp/jedit/gui/DynamicContextMenuService.java
@@ -5,17 +5,17 @@ import javax.swing.JMenuItem;
 import org.gjt.sp.jedit.textarea.JEditTextArea;
 
 /**
- * A service that can be offered by plugins when a context menu item needs to be offered
- * that is sensitive to the state of the editpane it was requested from.
+ * A service that can be offered by plugins when a text area context menu item 
+ * needs to be offered  that is sensitive to the state of the TextArea it was requested from.
  * @author ezust
  * @since jEdit 4.3pre15
  */
 abstract public class DynamicContextMenuService {
 	/**
 	 * 
-	 * @param pane the TextArea where the context menu was requested.
+	 * @param ta the TextArea where the context menu was requested.
 	 * 	   Use this to determine the location of the caret, or the edit mode of the buffer, etc.
-	 * @return a dynamic menu item (or JMenu) dependent on state of pane
+	 * @return a dynamic menu item (or JMenu) dependent on state of ta
 	 *         or null if there is no appropriate action to be added at this time
 	 */
 	public abstract JMenuItem createMenu(JEditTextArea ta); 
