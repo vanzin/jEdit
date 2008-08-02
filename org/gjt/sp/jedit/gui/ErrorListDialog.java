29477ffbcc89110e1c8fbf91854ba53ad2d99f13
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-08-02 10:58:24 +0000

    Made error messages in ErrorListDialog copyable to clipboard.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@13141 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/ErrorListDialog.java
+++ org/gjt/sp/jedit/gui/ErrorListDialog.java
@@ -84,6 +84,15 @@ public class ErrorListDialog extends EnhancedDialog
 			else
 				return false;
 		}
+
+		// This enables users to copy the error messages to
+		// clipboard with Ctrl+C on Windows. But it works only
+		// if the entry is selected by a mouse click.
+		public String toString()
+		{
+			return path + ":\n" +
+				TextUtilities.join(java.util.Arrays.asList(messages), "\n");
+		}
 	} //}}}
 
 	//{{{ ErrorListDialog constructor
