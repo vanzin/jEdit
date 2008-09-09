4f955bc3f78f302c6196e7cac9bb0945ba7d67d6
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-09-09 18:25:38 +0000

    Extended the scope of the workaround in r13622 to cover Mac OS X because the same problem was reported on it.
    http://www.nabble.com/--jedit-Patches-2096752---Embeddable-text-area-to19369320.html#a19386108
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@13633 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/syntax/Chunk.java
+++ org/gjt/sp/jedit/syntax/Chunk.java
@@ -346,7 +346,10 @@ public class Chunk extends Token
 	{
 		boolean sun_java_5 = false;
 		String vendor = System.getProperty("java.vendor");
-		if (vendor != null && vendor.startsWith("Sun"))
+		// Enable the workaround on Apple JVM, too, because the
+		// same problem was reported on Mac OS X.
+		if (vendor != null && (vendor.startsWith("Sun") ||
+					vendor.startsWith("Apple")))
 		{
 			String version = System.getProperty("java.version");
 			if (version != null && version.startsWith("1.5"))
