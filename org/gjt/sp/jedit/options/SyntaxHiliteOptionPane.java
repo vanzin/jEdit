8fde5280bb8449d08829cf14c2f94b76c2c52799
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-12-01 10:10:57 +0000

    moved top level StyleTableModel to inner class
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@14127 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/options/SyntaxHiliteOptionPane.java
+++ org/gjt/sp/jedit/options/SyntaxHiliteOptionPane.java
@@ -133,172 +133,173 @@ public class SyntaxHiliteOptionPane extends AbstractOptionPane
 				styleModel.setValueAt(style,row,1);
 		}
 	} //}}}
-} //}}}
 
-//{{{ StyleTableModel class
-class StyleTableModel extends AbstractTableModel
-{
-	private final java.util.List<StyleChoice> styleChoices;
-
-	//{{{ StyleTableModel constructor
-	StyleTableModel()
+	//{{{ StyleTableModel class
+	private static class StyleTableModel extends AbstractTableModel
 	{
-		styleChoices = new Vector<StyleChoice>(Token.ID_COUNT + 4);
-		// start at 1 not 0 to skip Token.NULL
-		for(int i = 1; i < Token.ID_COUNT; i++)
+		private final java.util.List<StyleChoice> styleChoices;
+
+		//{{{ StyleTableModel constructor
+		StyleTableModel()
 		{
-			String tokenName = Token.tokenToString((byte)i);
-			addStyleChoice(tokenName,"view.style." + tokenName.toLowerCase());
-		}
-
-		addStyleChoice(jEdit.getProperty("options.syntax.foldLine.1"),
-			"view.style.foldLine.1");
-		addStyleChoice(jEdit.getProperty("options.syntax.foldLine.2"),
-			"view.style.foldLine.2");
-		addStyleChoice(jEdit.getProperty("options.syntax.foldLine.3"),
-			"view.style.foldLine.3");
-		addStyleChoice(jEdit.getProperty("options.syntax.foldLine.0"),
-			"view.style.foldLine.0");
-
-		Collections.sort(styleChoices,new StandardUtilities.StringCompare<StyleChoice>(true));
-	} //}}}
-
-	//{{{ getColumnCount() method
-	public int getColumnCount()
-	{
-		return 2;
-	} //}}}
-
-	//{{{ getRowCount() method
-	public int getRowCount()
-	{
-		return styleChoices.size();
-	} //}}}
-
-	//{{{ getValueAt() method
-	public Object getValueAt(int row, int col)
-	{
-		StyleChoice ch = styleChoices.get(row);
-		switch(col)
-		{
-		case 0:
-			return ch.label;
-		case 1:
-			return ch.style;
-		default:
-			return null;
-		}
-	} //}}}
-
-	//{{{ setValueAt() method
-	@Override
-	public void setValueAt(Object value, int row, int col)
-	{
-		StyleChoice ch = styleChoices.get(row);
-		if(col == 1)
-			ch.style = (SyntaxStyle)value;
-		fireTableRowsUpdated(row,row);
-	} //}}}
-
-	//{{{ getColumnName() method
-	@Override
-	public String getColumnName(int index)
-	{
-		switch(index)
-		{
-		case 0:
-			return jEdit.getProperty("options.syntax.object");
-		case 1:
-			return jEdit.getProperty("options.syntax.style");
-		default:
-			return null;
-		}
-	} //}}}
-
-	//{{{ save() method
-	public void save()
-	{
-		for(int i = 0; i < styleChoices.size(); i++)
-		{
-			StyleChoice ch = styleChoices
-				.get(i);
-			jEdit.setProperty(ch.property,
-				GUIUtilities.getStyleString(ch.style));
-		}
-	} //}}}
-
-	//{{{ addStyleChoice() method
-	private void addStyleChoice(String label, String property)
-	{
-		styleChoices.add(new StyleChoice(label,
-			property,
-			GUIUtilities.parseStyle(jEdit.getProperty(property),
-			"Dialog",12)));
-	} //}}}
-
-	//{{{ StyleChoice class
-	private static class StyleChoice
-	{
-		private String label;
-		private String property;
-		private SyntaxStyle style;
-
-		StyleChoice(String label, String property, SyntaxStyle style)
-		{
-			this.label = label;
-			this.property = property;
-			this.style = style;
-		}
-
-		// for sorting
-		@Override
-		public String toString()
-		{
-			return label;
-		}
-	} //}}}
-
-	//{{{ StyleRenderer class
-	static class StyleRenderer extends JLabel
-		implements TableCellRenderer
-	{
-		//{{{ StyleRenderer constructor
-		StyleRenderer()
-		{
-			setOpaque(true);
-			setBorder(SyntaxHiliteOptionPane.noFocusBorder);
-			setText("Hello World");
-		} //}}}
-
-		//{{{ getTableCellRendererComponent() method
-		public Component getTableCellRendererComponent(
-			JTable table,
-			Object value,
-			boolean isSelected,
-			boolean cellHasFocus,
-			int row,
-			int col)
-		{
-			if (value != null)
+			styleChoices = new Vector<StyleChoice>(Token.ID_COUNT + 4);
+			// start at 1 not 0 to skip Token.NULL
+			for(int i = 1; i < Token.ID_COUNT; i++)
 			{
-				SyntaxStyle style = (SyntaxStyle)value;
-				setForeground(style.getForegroundColor());
-				if (style.getBackgroundColor() != null)
-					setBackground(style.getBackgroundColor());
-				else
-				{
-					// this part sucks
-					setBackground(jEdit.getColorProperty(
-						"view.bgColor"));
-				}
-				setFont(style.getFont());
+				String tokenName = Token.tokenToString((byte)i);
+				addStyleChoice(tokenName,"view.style." + tokenName.toLowerCase());
 			}
 
-			setBorder(cellHasFocus ? UIManager.getBorder(
-				"Table.focusCellHighlightBorder")
-				: SyntaxHiliteOptionPane.noFocusBorder);
-			return this;
+			addStyleChoice(jEdit.getProperty("options.syntax.foldLine.1"),
+			               "view.style.foldLine.1");
+			addStyleChoice(jEdit.getProperty("options.syntax.foldLine.2"),
+			               "view.style.foldLine.2");
+			addStyleChoice(jEdit.getProperty("options.syntax.foldLine.3"),
+			               "view.style.foldLine.3");
+			addStyleChoice(jEdit.getProperty("options.syntax.foldLine.0"),
+			               "view.style.foldLine.0");
+
+			Collections.sort(styleChoices,new StandardUtilities.StringCompare<StyleChoice>(true));
+		} //}}}
+
+		//{{{ getColumnCount() method
+		public int getColumnCount()
+		{
+			return 2;
+		} //}}}
+
+		//{{{ getRowCount() method
+		public int getRowCount()
+		{
+			return styleChoices.size();
+		} //}}}
+
+		//{{{ getValueAt() method
+		public Object getValueAt(int row, int col)
+		{
+			StyleChoice ch = styleChoices.get(row);
+			switch(col)
+			{
+				case 0:
+					return ch.label;
+				case 1:
+					return ch.style;
+				default:
+					return null;
+			}
+		} //}}}
+
+		//{{{ setValueAt() method
+		@Override
+		public void setValueAt(Object value, int row, int col)
+		{
+			StyleChoice ch = styleChoices.get(row);
+			if(col == 1)
+				ch.style = (SyntaxStyle)value;
+			fireTableRowsUpdated(row,row);
+		} //}}}
+
+		//{{{ getColumnName() method
+		@Override
+		public String getColumnName(int index)
+		{
+			switch(index)
+			{
+				case 0:
+					return jEdit.getProperty("options.syntax.object");
+				case 1:
+					return jEdit.getProperty("options.syntax.style");
+				default:
+					return null;
+			}
+		} //}}}
+
+		//{{{ save() method
+		public void save()
+		{
+			for(int i = 0; i < styleChoices.size(); i++)
+			{
+				StyleChoice ch = styleChoices
+					.get(i);
+				jEdit.setProperty(ch.property,
+				                  GUIUtilities.getStyleString(ch.style));
+			}
+		} //}}}
+
+		//{{{ addStyleChoice() method
+		private void addStyleChoice(String label, String property)
+		{
+			styleChoices.add(new StyleChoice(label,
+			                                 property,
+			                                 GUIUtilities.parseStyle(jEdit.getProperty(property),
+			                                                         "Dialog",12)));
+		} //}}}
+
+		//{{{ StyleChoice class
+		private static class StyleChoice
+		{
+			private String label;
+			private String property;
+			private SyntaxStyle style;
+
+			StyleChoice(String label, String property, SyntaxStyle style)
+			{
+				this.label = label;
+				this.property = property;
+				this.style = style;
+			}
+
+			// for sorting
+			@Override
+			public String toString()
+			{
+				return label;
+			}
+		} //}}}
+
+		//{{{ StyleRenderer class
+		static class StyleRenderer extends JLabel
+			implements TableCellRenderer
+		{
+			//{{{ StyleRenderer constructor
+			StyleRenderer()
+			{
+				setOpaque(true);
+				setBorder(SyntaxHiliteOptionPane.noFocusBorder);
+				setText("Hello World");
+			} //}}}
+
+			//{{{ getTableCellRendererComponent() method
+			public Component getTableCellRendererComponent(
+				JTable table,
+				Object value,
+				boolean isSelected,
+				boolean cellHasFocus,
+				int row,
+				int col)
+			{
+				if (value != null)
+				{
+					SyntaxStyle style = (SyntaxStyle)value;
+					setForeground(style.getForegroundColor());
+					if (style.getBackgroundColor() != null)
+						setBackground(style.getBackgroundColor());
+					else
+					{
+						// this part sucks
+						setBackground(jEdit.getColorProperty(
+							"view.bgColor"));
+					}
+					setFont(style.getFont());
+				}
+
+				setBorder(cellHasFocus ? UIManager.getBorder(
+					"Table.focusCellHighlightBorder")
+				                       : SyntaxHiliteOptionPane.noFocusBorder);
+				return this;
+			} //}}}
 		} //}}}
 	} //}}}
+
 } //}}}
 
