17668ab0b47f53d57c4a7f6bb9b30013c04501aa
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-12-18 21:47:53 +0000

    Shortened the try block where the readlock is active.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@14187 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/Buffer.java
+++ org/gjt/sp/jedit/Buffer.java
@@ -1899,26 +1899,32 @@ public class Buffer extends JEditBuffer
 		final byte[] dummy = new byte[1]; 
 		if (!jEdit.getBooleanProperty("useMD5forDirtyCalculation"))
 			return dummy;
+		ByteBuffer bb = null;
 		readLock();
-		try
+		try 
 		{
 			// Log.log(Log.NOTICE, this, "calculateHash()");
 			int length = getLength();
-			ByteBuffer bb = ByteBuffer.allocate(length * 2);	// Chars are 2 bytes
+			bb = ByteBuffer.allocate(length * 2);	// Chars are 2 bytes
 			CharBuffer cb = bb.asCharBuffer();
 			cb.append( getSegment(0, length) );
+		}
+		finally 
+		{
+			readUnlock();
+		} 
+		try 
+		{
 			MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
 			digest.update( bb );
 			return digest.digest();
 		}
-		catch (NoSuchAlgorithmException nsae)
+		catch (NoSuchAlgorithmException nsae) 
 		{
 			Log.log(Log.ERROR, this, "Can't Calculate MD5 hash!", nsae);
 			return dummy;
 		}
-		finally {
-			readUnlock();
-		}
+		
 	}
 	
 	/** Update the buffer's members with the current hash and length,
