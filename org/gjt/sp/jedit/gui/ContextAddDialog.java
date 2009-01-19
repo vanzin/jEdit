62949ab8db242627ebb074b2bbdbe1150f67cf95
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-01-19 10:56:38 +0000

    fixed indentation, added copyright and explicit folds
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@14391 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/ContextAddDialog.java
+++ org/gjt/sp/jedit/gui/ContextAddDialog.java
@@ -1,5 +1,28 @@
+/*
+ * ContextAddDialog.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2007 Marcelo Vancin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package org.gjt.sp.jedit.gui;
 
+//{{{ Imports
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridLayout;
@@ -26,6 +49,9 @@ import org.gjt.sp.jedit.ActionSet;
 import org.gjt.sp.jedit.EditAction;
 import org.gjt.sp.jedit.GUIUtilities;
 import org.gjt.sp.jedit.jEdit;
+//}}}
+
+
 /**
  * Dialog for showing ActionSets and adding actions to context menus
  * Was package private and located in AbstractContextOptionPane.java
@@ -34,159 +60,169 @@ import org.gjt.sp.jedit.jEdit;
  */
 public class ContextAddDialog extends EnhancedDialog
 {
-    public ContextAddDialog(Component comp)
-    {
-        super(GUIUtilities.getParentDialog(comp),
-            jEdit.getProperty("options.context.add.title"),
-            true);
+	//{{{ ContextAddDialog constructor
+	public ContextAddDialog(Component comp)
+	{
+		super(GUIUtilities.getParentDialog(comp),
+		      jEdit.getProperty("options.context.add.title"),
+		      true);
 
-        JPanel content = new JPanel(new BorderLayout());
-        content.setBorder(new EmptyBorder(12,12,12,12));
-        setContentPane(content);
+		JPanel content = new JPanel(new BorderLayout());
+		content.setBorder(new EmptyBorder(12,12,12,12));
+		setContentPane(content);
 
-        ActionHandler actionHandler = new ActionHandler();
-        ButtonGroup grp = new ButtonGroup();
+		ActionHandler actionHandler = new ActionHandler();
+		ButtonGroup grp = new ButtonGroup();
 
-        JPanel typePanel = new JPanel(new GridLayout(3,1,6,6));
-        typePanel.setBorder(new EmptyBorder(0,0,6,0));
-        typePanel.add(new JLabel(
-            jEdit.getProperty("options.context.add.caption")));
+		JPanel typePanel = new JPanel(new GridLayout(3,1,6,6));
+		typePanel.setBorder(new EmptyBorder(0,0,6,0));
+		typePanel.add(new JLabel(
+					 jEdit.getProperty("options.context.add.caption")));
 
-        separator = new JRadioButton(jEdit.getProperty("options.context"
-            + ".add.separator"));
-        separator.addActionListener(actionHandler);
-        grp.add(separator);
-        typePanel.add(separator);
+		separator = new JRadioButton(jEdit.getProperty("options.context"
+							       + ".add.separator"));
+		separator.addActionListener(actionHandler);
+		grp.add(separator);
+		typePanel.add(separator);
 
-        action = new JRadioButton(jEdit.getProperty("options.context"
-            + ".add.action"));
-        action.addActionListener(actionHandler);
-        grp.add(action);
-        action.setSelected(true);
-        typePanel.add(action);
+		action = new JRadioButton(jEdit.getProperty("options.context"
+							    + ".add.action"));
+		action.addActionListener(actionHandler);
+		grp.add(action);
+		action.setSelected(true);
+		typePanel.add(action);
 
-        content.add(BorderLayout.NORTH,typePanel);
+		content.add(BorderLayout.NORTH,typePanel);
 
-        JPanel actionPanel = new JPanel(new BorderLayout(6,6));
+		JPanel actionPanel = new JPanel(new BorderLayout(6,6));
 
-        ActionSet[] actionsList = jEdit.getActionSets();
-        TreeSet<ActionSet> actionSets = new TreeSet<ActionSet>();
-        for(int i = 0; i < actionsList.length; i++)
-        {
-            ActionSet actionSet = actionsList[i];
-            if(actionSet.getActionCount() != 0)
-            	actionSets.add(actionSet);
-        }
-        combo = new JComboBox(actionSets.toArray());
-        combo.setSelectedIndex(jEdit.getIntegerProperty("contextAddDialog.lastSelection",1));
-        combo.addActionListener(actionHandler);
-        actionPanel.add(BorderLayout.NORTH,combo);
+		ActionSet[] actionsList = jEdit.getActionSets();
+		TreeSet<ActionSet> actionSets = new TreeSet<ActionSet>();
+		for(int i = 0; i < actionsList.length; i++)
+		{
+			ActionSet actionSet = actionsList[i];
+			if(actionSet.getActionCount() != 0)
+				actionSets.add(actionSet);
+		}
+		combo = new JComboBox(actionSets.toArray());
+		combo.setSelectedIndex(jEdit.getIntegerProperty("contextAddDialog.lastSelection",1));
+		combo.addActionListener(actionHandler);
+		actionPanel.add(BorderLayout.NORTH,combo);
 
-        list = new JList();
-        list.setVisibleRowCount(8);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        actionPanel.add(BorderLayout.CENTER,new JScrollPane(list));
+		list = new JList();
+		list.setVisibleRowCount(8);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		actionPanel.add(BorderLayout.CENTER,new JScrollPane(list));
 
-        content.add(BorderLayout.CENTER,actionPanel);
+		content.add(BorderLayout.CENTER,actionPanel);
 
-        JPanel southPanel = new JPanel();
-        southPanel.setLayout(new BoxLayout(southPanel,BoxLayout.X_AXIS));
-        southPanel.setBorder(new EmptyBorder(12,0,0,0));
-        southPanel.add(Box.createGlue());
-        ok = new JButton(jEdit.getProperty("common.ok"));
-        ok.addActionListener(actionHandler);
-        getRootPane().setDefaultButton(ok);
-        southPanel.add(ok);
-        southPanel.add(Box.createHorizontalStrut(6));
-        cancel = new JButton(jEdit.getProperty("common.cancel"));
-        cancel.addActionListener(actionHandler);
-        southPanel.add(cancel);
-        southPanel.add(Box.createGlue());
+		JPanel southPanel = new JPanel();
+		southPanel.setLayout(new BoxLayout(southPanel,BoxLayout.X_AXIS));
+		southPanel.setBorder(new EmptyBorder(12,0,0,0));
+		southPanel.add(Box.createGlue());
+		ok = new JButton(jEdit.getProperty("common.ok"));
+		ok.addActionListener(actionHandler);
+		getRootPane().setDefaultButton(ok);
+		southPanel.add(ok);
+		southPanel.add(Box.createHorizontalStrut(6));
+		cancel = new JButton(jEdit.getProperty("common.cancel"));
+		cancel.addActionListener(actionHandler);
+		southPanel.add(cancel);
+		southPanel.add(Box.createGlue());
 
-        content.add(BorderLayout.SOUTH,southPanel);
+		content.add(BorderLayout.SOUTH,southPanel);
 
-        updateList();
+		updateList();
 
-        pack();
-        setLocationRelativeTo(GUIUtilities.getParentDialog(comp));
-        setVisible(true);
-    }
+		pack();
+		setLocationRelativeTo(GUIUtilities.getParentDialog(comp));
+		setVisible(true);
+	} //}}}
 
-    public void ok()
-    {
-        isOK = true;
-        dispose();
-    }
+	//{{{ ok() method
+	public void ok()
+	{
+		isOK = true;
+		dispose();
+	} //}}}
 
-    public void cancel()
-    {
-        dispose();
-    }
+	//{{{ cancel() method
+	public void cancel()
+	{
+		dispose();
+	} //}}}
 
-    public String getSelection()
-    {
-        if(!isOK)
-            return null;
+	//{{{ getSelection() method
+	public String getSelection()
+	{
+		if(!isOK)
+			return null;
 
-        if(separator.isSelected())
-            return "-";
-        else if(action.isSelected())
-        {
-            return ((AbstractContextOptionPane.MenuItem)list.getSelectedValue())
-                .actionName;
-        }
-        else
-            throw new InternalError();
-    }
+		if(separator.isSelected())
+			return "-";
+		else if(action.isSelected())
+		{
+			return ((AbstractContextOptionPane.MenuItem)list.getSelectedValue())
+			.actionName;
+		}
+		else
+			throw new InternalError();
+	} //}}}
 
-    // private members
-    private boolean isOK;
-    private JRadioButton separator, action;
-    private JComboBox combo;
-    private JList list;
-    private JButton ok, cancel;
 
-    private void updateList()
-    {
-        ActionSet actionSet = (ActionSet)combo.getSelectedItem();
-        jEdit.setIntegerProperty("contextAddDialog.lastSelection", combo.getSelectedIndex());
-                
-        EditAction[] actions = actionSet.getActions();
-        Vector listModel = new Vector(actions.length);
+	//{{{ private members
+	private boolean isOK;
+	private JRadioButton separator, action;
+	private JComboBox combo;
+	private JList list;
+	private JButton ok, cancel;
 
-        for(int i = 0; i < actions.length; i++)
-        {
-            EditAction action = actions[i];
-            String label = action.getLabel();
-            if(label == null)
-                continue;
+	//{{{ updateList() method
+	private void updateList()
+	{
+		ActionSet actionSet = (ActionSet)combo.getSelectedItem();
+		jEdit.setIntegerProperty("contextAddDialog.lastSelection", combo.getSelectedIndex());
 
-            listModel.addElement(new AbstractContextOptionPane.MenuItem(
-                action.getName(),label));
-        }
+		EditAction[] actions = actionSet.getActions();
+		Vector listModel = new Vector(actions.length);
 
-        Collections.sort(listModel,new AbstractContextOptionPane.MenuItemCompare());
+		for(int i = 0; i < actions.length; i++)
+		{
+			EditAction action = actions[i];
+			String label = action.getLabel();
+			if(label == null)
+				continue;
 
-        list.setListData(listModel);
-    }
+			listModel.addElement(new AbstractContextOptionPane.MenuItem(
+										    action.getName(),label));
+		}
 
-    class ActionHandler implements ActionListener
-    {
-        public void actionPerformed(ActionEvent evt)
-        {
-            Object source = evt.getSource();
-            if(source instanceof JRadioButton)
-            {
-                combo.setEnabled(action.isSelected());
-                list.setEnabled(action.isSelected());
-            }
-            if(source == ok)
-                ok();
-            else if(source == cancel)
-                cancel();
-            else if(source == combo)
-                updateList();
-        }
-    }
+		Collections.sort(listModel,new AbstractContextOptionPane.MenuItemCompare());
+
+		list.setListData(listModel);
+	} //}}}
+	//}}}
+
+
+	//{{{ ActionHandler class
+	class ActionHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent evt)
+		{
+			Object source = evt.getSource();
+			if(source instanceof JRadioButton)
+			{
+				combo.setEnabled(action.isSelected());
+				list.setEnabled(action.isSelected());
+			}
+			if(source == ok)
+				ok();
+			else if(source == cancel)
+				cancel();
+			else if(source == combo)
+				updateList();
+		}
+	} //}}}
+
 }
 
