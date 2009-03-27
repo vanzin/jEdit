a4a81447ea49a3eb63b82f7c3d142fa650ac3c47
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-03-27 12:51:18 +0000

    Fixed the handling of "single interval" selection mode in the filtered list model. In this mode, any call to "setSelectionInterval" removes all previous selections, keeping only the current one. Using "setSelectionInterval" for each item thus leaves only the last item selected.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@14857 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/FilteredListModel.java
+++ org/gjt/sp/jedit/gui/FilteredListModel.java
@@ -179,12 +179,34 @@ public abstract class FilteredListModel<E extends ListModel> extends AbstractLis
 		if (selectedIndices == null || getSize() == 0)
 			return;
 
-		for (Integer selectedIndex : selectedIndices)
+		// To correctly handle "single interval" selection mode,
+		// each interval has to be selected using a single call to
+		// setSelectionInterval; calling setSelectionInterval on
+		// each item cancels the previous selection.
+		// Sort the list of selected indices to simplify interval
+		// identification.
+		Vector<Integer> sel = new Vector<Integer>(selectedIndices);
+		Collections.sort(sel);
+		int from = -1;
+		int to = -1;
+		for (Integer selectedIndex : sel)
 		{
 			int i = getInternal2ExternalRow(selectedIndex.intValue());
 			if (i != -1)
-				list.setSelectionInterval(i, i);
+			{
+				if (from == -1)
+					from = to = i;
+				else if (i == to + 1)
+					to = i;
+				else
+				{
+					list.setSelectionInterval(from, to);
+					from = to = i;
+				}
+			}
 		}
+		if (from != -1)
+			list.setSelectionInterval(from, to);
 	}  //}}}
 
 	//{{{ getTrueRow() method
