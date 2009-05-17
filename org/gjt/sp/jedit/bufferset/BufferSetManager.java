7b95bc95227456be1c2f69ecc4063724b001dad5
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-05-17 14:18:40 +0000

    Added a new method jEdit.moveBuffer() to explicitly move a buffer in a BufferSet.
    This can be used to replace a hacky call of BufferSet#addBufferAt()
    which is used to move the buffer.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@15298 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/bufferset/BufferSetManager.java
+++ org/gjt/sp/jedit/bufferset/BufferSetManager.java
@@ -205,6 +205,17 @@ public class BufferSetManager implements EBComponent
 		}
 	} //}}}
 
+	//{{{ moveBuffer() method
+	/**
+	 * Moves a buffer from a old position to a new position in the
+	 * BufferSet used in an EditPane.
+	 */
+	public void moveBuffer(EditPane editPane,
+		int oldPosition, int newPosition)
+	{
+		editPane.getBufferSet().moveBuffer(oldPosition, newPosition);
+	} //}}}
+
 	//{{{ removeBuffer() methods
 	/**
 	 * Remove a buffer from the EditPane's bufferSet.
--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -2181,6 +2181,20 @@ public class jEdit
 		return buffersLast;
 	} //}}}
 
+	//{{{ moveBuffer() method
+	/**
+	 * Moves a buffer from a old position to a new position in the
+	 * BufferSet used in an EditPane.
+	 * @param editPane The EditPane in which a buffer is moved
+	 * @param oldPosition The position before the move
+	 * @param newPosition The position after the move
+	 */
+	public static void moveBuffer(EditPane editPane,
+		int oldPosition, int newPosition)
+	{
+		bufferSetManager.moveBuffer(editPane, oldPosition, newPosition);
+	} //}}}
+
 	//{{{ getGlobalBufferSet() method
 	/**
 	 * Returns the global buffer set, which can be shared by several
