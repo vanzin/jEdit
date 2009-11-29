0957a8fe8b5113a56c7dab188272673fc997e3ff
kerik-sf <kerik-sf@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-11-29 20:01:32 +0000

    set the name of EnhancedButton to the last part of the action, to simplify unit-testing.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@16561 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/EnhancedButton.java
+++ org/gjt/sp/jedit/gui/EnhancedButton.java
@@ -41,6 +41,20 @@ public class EnhancedButton extends RolloverButton
 
 		if(action != null)
 		{
+			// set the name of this button :
+			// for instance, if the action is 'vfs.browser.previous'
+			// the name will be 'previous'
+			// this helps greatly in testing the UI with Fest-Swing
+			int iSuffix = action.lastIndexOf('.');
+			if(iSuffix<0 || iSuffix == action.length()-1)
+			{
+				setName(action);
+			}
+			else
+			{
+				setName(action.substring(iSuffix+1));
+			}
+			
 			setEnabled(true);
 			addActionListener(new EditAction.Wrapper(context,action));
 			addMouseListener(new MouseHandler());
