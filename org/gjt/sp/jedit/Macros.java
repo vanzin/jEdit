3ed93f860bba630a3dff46e2929ee132598c0450
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-12-25 20:51:02 +0000

    Added an API for unregistering macro handlers. Previously, there was no way to unregister macro handlers, and therefore plugins with registered handlers could not be cleanly unloaded.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@16733 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/Macros.java
+++ org/gjt/sp/jedit/Macros.java
@@ -296,6 +296,25 @@ file_loop:			for(int i = 0; i < paths.length; i++)
 		macroHandlers.add(handler);
 	} //}}}
 
+	//{{{ unregisterHandler() method
+	/**
+	 * Removes a macro handler from the handlers list
+	 * @since jEdit 4.4.1
+	 */
+	public static void unregisterHandler(Handler handler)
+	{
+		if (macroHandlers.remove(handler))
+		{
+			Log.log(Log.DEBUG, Macros.class, "Unregistered " + handler.getName()
+				+ " macro handler");
+		}
+		else
+		{
+			Log.log(Log.ERROR, Macros.class, "Cannot unregister " + handler.getName()
+				+ " macro handler - it is not registered.");
+		}
+	} //}}}
+
 	//{{{ getHandlers() method
 	/**
 	 * Returns an array containing the list of registered macro handlers
