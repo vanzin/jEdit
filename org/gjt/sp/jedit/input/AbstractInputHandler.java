vanzin/jEdit

commit 62b6b940139e8784c5f06362ff175bb777a1b530

big changes in InputHandling to allow the standalone TextArea to use beanshell actions. The most incompatibility that can happens is when calling jEdit.getActionSets() that was returning ActionSet[] and now returns JEditActionSet[]
git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@11402 6b1eeb88-9816-0410-afa2-b43733a0f04e

org/gjt/sp/jedit/input/AbstractInputHandler.java
@@ -27,6 +27,10 @@ import org.gjt.sp.util.Log;
 
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+import org.gjt.sp.jedit.JEditAbstractEditAction;
+import org.gjt.sp.jedit.gui.ShortcutPrefixActiveEvent;
 
 /**
  * The abstract input handler manage the keyboard handling.
@@ -36,13 +40,15 @@ import java.awt.event.KeyEvent;
  * @author Matthieu Casanova
  * @version $Id: FoldHandler.java 5568 2006-07-10 20:52:23Z kpouer $
  */
-public abstract class AbstractInputHandler
+public abstract class AbstractInputHandler<E extends JEditAbstractEditAction>
 {
 	protected int lastActionCount;
 	/** This listener will receive keyboard events if it is not null. */
 	protected KeyListener keyEventInterceptor;
 	protected String readNextChar;
 	protected int repeatCount;
+	protected E lastAction;
+
 
 	protected static final int REPEAT_COUNT_THRESHOLD = 20;
 
@@ -50,6 +56,177 @@ public abstract class AbstractInputHandler
 	public AbstractInputHandler()
 	{
 		repeatCount = 1;
+	} //}}}
+	
+		//{{{ addKeyBinding() method
+	/**
+	 * Adds a key binding to this input handler. The key binding is
+	 * a list of white space separated key strokes of the form
+	 * <i>[modifiers+]key</i> where modifier is C for Control, A for Alt,
+	 * or S for Shift, and key is either a character (a-z) or a field
+	 * name in the KeyEvent class prefixed with VK_ (e.g., BACK_SPACE)
+	 * @param keyBinding The key binding
+	 * @param action The action
+	 * @since jEdit 4.2pre1
+	 */
+	public void addKeyBinding(String keyBinding, String action)
+	{
+		addKeyBinding(keyBinding,(Object)action);
+	} //}}}
+
+	//{{{ addKeyBinding() method
+	/**
+	 * Adds a key binding to this input handler. The key binding is
+	 * a list of white space separated key strokes of the form
+	 * <i>[modifiers+]key</i> where modifier is C for Control, A for Alt,
+	 * or S for Shift, and key is either a character (a-z) or a field
+	 * name in the KeyEvent class prefixed with VK_ (e.g., BACK_SPACE)
+	 * @param keyBinding The key binding
+	 * @param action The action
+	 */
+	public void addKeyBinding(String keyBinding, E action)
+	{
+		addKeyBinding(keyBinding,(Object)action);
+	} //}}}
+
+	//{{{ addKeyBinding() method
+	/**
+	 * Adds a key binding to this input handler. The key binding is
+	 * a list of white space separated key strokes of the form
+	 * <i>[modifiers+]key</i> where modifier is C for Control, A for Alt,
+	 * or S for Shift, and key is either a character (a-z) or a field
+	 * name in the KeyEvent class prefixed with VK_ (e.g., BACK_SPACE)
+	 * @param keyBinding The key binding
+	 * @param action The action
+	 * @since jEdit 4.3pre1
+	 */
+	public void addKeyBinding(String keyBinding, Object action)
+	{
+		Hashtable current = bindings;
+
+		String prefixStr = null;
+
+		StringTokenizer st = new StringTokenizer(keyBinding);
+		while(st.hasMoreTokens())
+		{
+			String keyCodeStr = st.nextToken();
+			if(prefixStr == null)
+				prefixStr = keyCodeStr;
+			else
+				prefixStr = prefixStr + " " + keyCodeStr;
+
+			KeyEventTranslator.Key keyStroke = KeyEventTranslator.parseKey(keyCodeStr);
+			if(keyStroke == null)
+				return;
+
+			if(st.hasMoreTokens())
+			{
+				Object o = current.get(keyStroke);
+				if(o instanceof Hashtable)
+					current = (Hashtable)o;
+				else
+				{
+					Hashtable hash = new Hashtable();
+					hash.put(PREFIX_STR,prefixStr);
+					o = hash;
+					current.put(keyStroke,o);
+					current = (Hashtable)o;
+				}
+			}
+			else
+				current.put(keyStroke,action);
+		}
+	} //}}}
+
+	//{{{ removeKeyBinding() method
+	/**
+	 * Removes a key binding from this input handler. This is not yet
+	 * implemented.
+	 * @param keyBinding The key binding
+	 */
+	public void removeKeyBinding(String keyBinding)
+	{
+		Hashtable current = bindings;
+
+		StringTokenizer st = new StringTokenizer(keyBinding);
+		while(st.hasMoreTokens())
+		{
+			String keyCodeStr = st.nextToken();
+			KeyEventTranslator.Key keyStroke = KeyEventTranslator.parseKey(keyCodeStr);
+			if(keyStroke == null)
+				return;
+
+			if(st.hasMoreTokens())
+			{
+				Object o = current.get(keyStroke);
+				if(o instanceof Hashtable)
+					current = ((Hashtable)o);
+				else if(o != null)
+				{
+					// we have binding foo
+					// but user asks to remove foo bar?
+					current.remove(keyStroke);
+					return;
+				}
+				else
+				{
+					// user asks to remove non-existent
+					return;
+				}
+			}
+			else
+				current.remove(keyStroke);
+		}
+	} //}}}
+
+	//{{{ removeAllKeyBindings() method
+	/**
+	 * Removes all key bindings from this input handler.
+	 */
+	public void removeAllKeyBindings()
+	{
+		bindings.clear();
+	} //}}}
+
+	//{{{ getKeyBinding() method
+	/**
+	 * Returns either an edit action, or a hashtable if the specified key
+	 * is a prefix.
+	 * @param keyBinding The key binding
+	 * @since jEdit 3.2pre5
+	 */
+	public Object getKeyBinding(String keyBinding)
+	{
+		Hashtable current = bindings;
+		StringTokenizer st = new StringTokenizer(keyBinding);
+
+		while(st.hasMoreTokens())
+		{
+			KeyEventTranslator.Key keyStroke = KeyEventTranslator.parseKey(
+				st.nextToken());
+			if(keyStroke == null)
+				return null;
+
+			if(st.hasMoreTokens())
+			{
+				Object o = current.get(keyStroke);
+				if(o instanceof Hashtable)
+				{
+					if(!st.hasMoreTokens())
+						return o;
+					else
+						current = (Hashtable)o;
+				}
+				else
+					return o;
+			}
+			else
+			{
+				return current.get(keyStroke);
+			}
+		}
+
+		return null;
 	} //}}}
 
 	//{{{ getLastActionCount() method
@@ -103,6 +280,22 @@ public abstract class AbstractInputHandler
 	{
 		return readNextChar != null;
 	} //}}}
+	
+	//{{{ setBindings() method
+	/**
+	 * Replace the set of key bindings.
+	 * @since jEdit 4.3pre1
+	 */
+	public void setBindings(Hashtable bindings)
+	{
+		this.bindings = this.currentBindings = bindings;
+	} //}}}
+	
+	//{{{ setCurrentBindings() method
+	public void setCurrentBindings(Hashtable bindings)
+	{
+		currentBindings = bindings;
+	} //}}}
 
 	//{{{ handleKey() method
 	/**
@@ -131,6 +324,23 @@ public abstract class AbstractInputHandler
 
 	//{{{ processKeyEventKeyStrokeHandling() method
 
+	//{{{ handleKey() methodprotected void sendShortcutPrefixOff()
+	/**
+	 *  If 
+	 */
+	protected void sendShortcutPrefixOff()
+	{
+		if(shortcutOn)
+		{
+			ShortcutPrefixActiveEvent.firePrefixStateChange(null, false);
+			shortcutOn = false;
+		}
+	} //}}}
+	
+	public abstract void invokeAction(String action);
+	
+	public abstract void invokeAction(E action);
+	
 	/**
 	 *
 	 * @param evt the keyboard event
@@ -163,4 +373,15 @@ public abstract class AbstractInputHandler
 			}
 		}
 	} //}}}
+	
+	//{{{ Private members
+
+	// Stores prefix name in bindings hashtable
+	public static Object PREFIX_STR = "PREFIX_STR";
+	protected boolean shortcutOn = false;
+	
+
+	protected Hashtable bindings;
+	protected Hashtable currentBindings;
+	//}}}
 }

org/gjt/sp/jedit/ActionListHandler.java
@@ -35,19 +35,24 @@ import org.gjt.sp.util.Log;
 import org.gjt.sp.util.XMLUtilities;
 //}}}
 
+/**
+ * This class loads the actions.xml files into a {@link JEditActionSet}. * @author Slava Pestov
+ * @author Mike Dillon
+ */
 class ActionListHandler extends DefaultHandler
 {
 	//{{{ ActionListHandler constructor
-	ActionListHandler(String path, ActionSet actionSet)
+	ActionListHandler(String path, JEditActionSet actionSet)
 	{
 		this.path = path;
 		this.actionSet = actionSet;
-		stateStack = new Stack();
-		code = new StringBuffer();
-		isSelected = new StringBuffer();
+		stateStack = new Stack<String>();
+		code = new StringBuilder();
+		isSelected = new StringBuilder();
 	} //}}}
 
 	//{{{ resolveEntity() method
+	@Override
 	public InputSource resolveEntity(String publicId, String systemId)
 	{
 		return XMLUtilities.findEntity(systemId, "actions.dtd", getClass());
@@ -70,6 +75,7 @@ class ActionListHandler extends DefaultHandler
 	} //}}}
 
 	//{{{ characters() method
+	@Override
 	public void characters(char[] c, int off, int len)
 	{
 		String tag = peekElement();
@@ -84,6 +90,7 @@ class ActionListHandler extends DefaultHandler
 	} //}}}
 
 	//{{{ startElement() method
+	@Override
 	public void startElement(String uri, String localName,
 				 String qName, Attributes attrs)
 	{
@@ -101,6 +108,7 @@ class ActionListHandler extends DefaultHandler
 	} //}}}
 
 	//{{{ endElement() method
+	@Override
 	public void endElement(String uri, String localName, String qName)
 	{
 		String tag = peekElement();
@@ -111,9 +119,14 @@ class ActionListHandler extends DefaultHandler
 			{
 				String selected = (isSelected.length() > 0) ?
 					isSelected.toString() : null;
-				actionSet.addAction(new BeanShellAction(actionName,
-					code.toString(),selected,
-					noRepeat,noRecord,noRememberLast));
+				JEditAbstractEditAction action = 
+					actionSet.createBeanShellAction(actionName,
+									code.toString(),
+									selected,
+									noRepeat,
+									noRecord,
+									noRememberLast);
+				actionSet.addAction(action);
 				noRepeat = noRecord = noRememberLast = false;
 				code.setLength(0);
 				isSelected.setLength(0);
@@ -129,6 +142,7 @@ class ActionListHandler extends DefaultHandler
 	} //}}}
 
 	//{{{ startDocument() method
+	@Override
 	public void startDocument()
 	{
 		try
@@ -137,7 +151,7 @@ class ActionListHandler extends DefaultHandler
 		}
 		catch (Exception e)
 		{
-			e.printStackTrace();
+			Log.log(Log.ERROR,this, e);
 		}
 	} //}}}
 
@@ -145,17 +159,17 @@ class ActionListHandler extends DefaultHandler
 
 	//{{{ Instance variables
 	private String path;
-	private ActionSet actionSet;
+	private JEditActionSet actionSet;
 
 	private String actionName;
-	private StringBuffer code;
-	private StringBuffer isSelected;
+	private final StringBuilder code;
+	private final StringBuilder isSelected;
 
 	private boolean noRepeat;
 	private boolean noRecord;
 	private boolean noRememberLast;
 
-	private Stack stateStack;
+	private final Stack<String> stateStack;
 	//}}}
 
 	//{{{ pushElement() method
@@ -171,15 +185,14 @@ class ActionListHandler extends DefaultHandler
 	//{{{ peekElement() method
 	private String peekElement()
 	{
-		return (String) stateStack.peek();
+		return stateStack.peek();
 	} //}}}
 
 	//{{{ popElement() method
 	private String popElement()
 	{
-		return (String) stateStack.pop();
+		return stateStack.pop();
 	} //}}}
 
 	//}}}
-
 }

org/gjt/sp/jedit/EditAction.java
@@ -23,11 +23,10 @@
 package org.gjt.sp.jedit;
 
 //{{{ Imports
+import org.gjt.sp.util.Log;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Component;
-
-import org.gjt.sp.util.Log;
 //}}}
 
 /**
@@ -40,15 +39,8 @@ import org.gjt.sp.util.Log;
  * @author Slava Pestov
  * @version $Id$
  */
-public abstract class EditAction
+public abstract class EditAction extends JEditAbstractEditAction<View>
 {
-	//{{{ Private members
-	private String name;
-	
-	protected Object[] args;
-
-	//}}}
-	
 	//{{{ EditAction constructors
 	/**
 	 * Creates a new edit action with the specified name.
@@ -56,33 +48,14 @@ public abstract class EditAction
 	 */
 	public EditAction(String name)
 	{
-		this.name = name;
+		super(name);
 	}
 	
-	public EditAction(String name, Object[] newArgs) {
-		this.name = name;
-		this.args = newArgs;
-	} //}}}
-		
-	//{{{ getName() method
-	/**
-	 * Returns the internal name of this action.
-	 */
-	public String getName()
+	public EditAction(String name, Object[] newArgs) 
 	{
-		return name;
+		super(name, newArgs);
 	} //}}}
-	
-	// {{{ setName() method
-	/**
-	 * Changes the name of an action
-	 * @param newName
-	 * @since jEdit 4.3pre4
-	 */
-	public void setName(String newName) {
-		name = newName;
-	}// }}}
-
+			
 	//{{{ getLabel() method
 	/**
 	 * Returns the action's label. This returns the
@@ -119,17 +92,6 @@ public abstract class EditAction
 	 * abstract since jEdit 4.3pre7
 	 */
 	abstract public void invoke(View view);
-
-	/**
-	 * 
-	 * @param view
-	 * @param newArgs new argument list
-	 * @since jEdit 4.3pre7
-	 */
-	final public void invoke(View view, Object[] newArgs) {
-		args = newArgs;
-		invoke(view);
-	} //}}}
 	
 	//{{{ getView() method
 	/**
@@ -216,13 +178,7 @@ public abstract class EditAction
 		return "jEdit.getAction(" + name + ").invoke(view); ";
 	}
 	//}}}
-
-	//{{{ toString() method
-	public String toString()
-	{
-		return name;
-	} //}}}
-
+	
 	//{{{ Wrapper class
 	/**
 	 * 'Wrap' EditActions in this class to turn them into AWT
@@ -231,8 +187,8 @@ public abstract class EditAction
 	public static class Wrapper implements ActionListener
 	{
 
-		private ActionContext context;
-		private String actionName;
+		private final ActionContext context;
+		private final String actionName;
 		
 		/**
 		 * Creates a new action listener wrapper.
@@ -263,7 +219,5 @@ public abstract class EditAction
 			else
 				context.invokeAction(evt,action);
 		}
-
-
 	} //}}}
 }

org/gjt/sp/jedit/IPropertyManager.java
@@ -1,9 +1,9 @@
 /*
- * ActionContext.java - For code sharing between jEdit and VFSBrowser
+ * IPropertyManager.java - An interface for class that returns properties
  * :tabSize=8:indentSize=8:noTabs=false:
  * :folding=explicit:collapseFolds=1:
  *
- * Copyright (C) 1998, 2003 Slava Pestov
+ * Copyright (C) 2007 Matthieu Casanova
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,21 +22,20 @@
 
 package org.gjt.sp.jedit;
 
+import java.io.*;
 import java.util.*;
 
 /**
- * Manages a collection of action sets. There are two instances of this class
- * in jEdit:
- * <ul>
- * <li>{@link org.gjt.sp.jedit.jEdit#getActionContext()} - editor actions
- * <li>{@link org.gjt.sp.jedit.browser.VFSBrowser#getActionContext()} - browser
- * actions
- * </ul>
- *
- * @since jEdit 4.2pre1
- * @author Slava Pestov
- * @version $Id$
+ * Manage properties.
+ * @author Matthieu Casanova
+ * @since 4.3pre13
  */
-public abstract class ActionContext extends JEditActionContext<EditAction, ActionSet>
+public interface IPropertyManager
 {
+	/**
+	 * Returns a String property
+	 * @param name the name of the property
+	 * @return the string property or null if it doesn't exists
+	 */
+	String getProperty(String name);
 }

org/gjt/sp/jedit/JEditAbstractEditAction.java
@@ -1,0 +1,107 @@
+/*
+ * EditAction.java - jEdit action listener
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 1998, 2003 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.gjt.sp.jedit;
+
+/**
+ * An action that can be bound to a menu item, tool bar button or keystroke.
+ *
+ * @see jEdit#getAction(String)
+ * @see jEdit#getActionNames()
+ * @see ActionSet
+ *
+ * @author Slava Pestov
+ * @version $Id: EditAction.java 11177 2007-12-01 09:50:50Z k_satoda $
+ * @since 4.3pre13
+ */
+public abstract class JEditAbstractEditAction<E>
+{
+	//{{{ Private members
+	protected String name;
+	
+	protected Object[] args;
+
+	//}}}
+	
+	//{{{ EditAction constructors
+	/**
+	 * Creates a new edit action with the specified name.
+	 * @param name The action name
+	 */
+	public JEditAbstractEditAction(String name)
+	{
+		this.name = name;
+	}
+	
+	public JEditAbstractEditAction(String name, Object[] newArgs) 
+	{
+		this.name = name;
+		this.args = newArgs;
+	} //}}}
+		
+	//{{{ getName() method
+	/**
+	 * Returns the internal name of this action.
+	 */
+	public String getName()
+	{
+		return name;
+	} //}}}
+	
+	// {{{ setName() method
+	/**
+	 * Changes the name of an action
+	 * @param newName
+	 * @since jEdit 4.3pre4
+	 */
+	public void setName(String newName) 
+	{
+		name = newName;
+	}// }}}
+
+	//{{{ invoke() method
+	/**
+	 * Invokes the action. This is an implementation of the Command pattern,
+	 * and concrete actions should override this.
+	 * 
+	 * @param arg the argument
+	 */
+	abstract public void invoke(E arg);
+
+	/**
+	 * 
+	 * @param arg
+	 * @param newArgs new argument list
+	 */
+	public final void invoke(E arg, Object[] newArgs) 
+	{
+		args = newArgs;
+		invoke(arg);
+	} //}}}
+	
+	//{{{ toString() method
+	@Override
+	public String toString()
+	{
+		return name;
+	} //}}}
+}

org/gjt/sp/jedit/JEditActionContext.java
@@ -1,0 +1,170 @@
+/*
+ * JEditActionContext.java - For code sharing between jEdit and VFSBrowser
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 1998, 2003 Slava Pestov
+ * Portions copyright (C) 2007 Matthieu Casanova
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.gjt.sp.jedit;
+
+import java.util.*;
+
+/**
+ * Manages a collection of action sets. There are two instances of this class
+ * in jEdit:
+ * <ul>
+ * <li>{@link org.gjt.sp.jedit.jEdit#getActionContext()} - editor actions
+ * <li>{@link org.gjt.sp.jedit.browser.VFSBrowser#getActionContext()} - browser
+ * actions
+ * </ul>
+ *
+ * @since jEdit 4.3pre13
+ * @author Slava Pestov
+ * @version $Id: ActionContext.java 6884 2006-09-06 02:38:55Z ezust $
+ */
+public abstract class JEditActionContext<F extends JEditAbstractEditAction, E extends JEditActionSet<F>>
+{
+	//{{{ invokeAction() method
+	/**
+	 * Invokes the given action in response to a user-generated event.
+	 * @param evt The event
+	 * @param action The action
+	 * @since jEdit 4.3pre13
+	 */
+	public abstract void invokeAction(EventObject evt, F action);
+	//}}}
+
+	//{{{ addActionSet() method
+	/**
+	 * Adds a new action set to the context.
+	 * @since jEdit 4.3pre13
+	 */
+	public void addActionSet(E actionSet)
+	{
+		actionNames = null;
+		actionSets.addElement(actionSet);
+		actionSet.context = this;
+		String[] actions = actionSet.getActionNames();
+		for(int i = 0; i < actions.length; i++)
+		{
+			/* Is it already there? */
+			if (actionHash.containsKey(actions[i])) 
+			{
+				/* Save it for plugin unloading time */
+				E oldAction = actionHash.get(actions[i]);
+				overriddenActions.put(actions[i], oldAction);
+			}
+			actionHash.put(actions[i],actionSet);
+		}
+	} //}}}
+
+	//{{{ removeActionSet() method
+	/**
+	 * Removes an action set from the context.
+	 * @since jEdit 4.23pre13
+	 */
+	public void removeActionSet(E actionSet)
+	{
+		actionNames = null;
+		actionSets.removeElement(actionSet);
+		actionSet.context = null;
+		String[] actions = actionSet.getActionNames();
+		for(int i = 0; i < actions.length; i++)
+		{
+			actionHash.remove(actions[i]);
+			if (overriddenActions.containsKey(actions[i])) 
+			{
+				E oldAction = overriddenActions.remove(actions[i]);
+				actionHash.put(actions[i], oldAction);
+			}
+		}
+	} //}}}
+
+	//{{{ getActionSets() method
+	/**
+	 * Returns all registered action sets.
+	 * @since jEdit 4.23pre13
+	 */
+	public JEditActionSet[] getActionSets()
+	{
+		// todo : test this method
+		JEditActionSet[] retVal = new JEditActionSet[actionSets.size()];
+		actionSets.copyInto(retVal);
+		return retVal;
+	} //}}}
+
+	//{{{ getAction() method
+	/**
+	 * Returns the specified action.
+	 * @param name The action name
+	 * @since jEdit 4.2pre1
+	 */
+	public F getAction(String name)
+	{
+		E set = actionHash.get(name);
+		if(set == null)
+			return null;
+		else
+			return set.getAction(name);
+	} //}}}
+
+	//{{{ getActionSetForAction() method
+	/**
+	 * Returns the action set that contains the specified action.
+	 *
+	 * @param action The action
+	 * @since jEdit 4.2pre1
+	 */
+	public E getActionSetForAction(String action)
+	{
+		return actionHash.get(action);
+	} //}}}
+
+	//{{{ getActionNames() method
+	/**
+	 * Returns all registered action names.
+	 */
+	public String[] getActionNames()
+	{
+		if(actionNames == null)
+		{
+			List<String> vec = new LinkedList<String>();
+			for(int i = 0; i < actionSets.size(); i++)
+				(actionSets.elementAt(i)).getActionNames(vec);
+
+			actionNames = vec.toArray(new String[vec.size()]);
+			Arrays.sort(actionNames,
+				new MiscUtilities.StringICaseCompare());
+		}
+
+		return actionNames;
+	} //}}}
+
+	//{{{ Package-private members
+	String[] actionNames;
+	Hashtable<String, E> actionHash = new Hashtable<String, E>();
+	
+	/** A map of built-in actions that were overridden by plugins. */
+	Hashtable<String, E> overriddenActions = new Hashtable<String, E>(); 
+	//}}}
+
+	//{{{ Private members
+	private final Vector<E> actionSets = new Vector<E>();
+	//}}}
+}

commit 09095ccac99d44f50cd434488ea53853c212e1d0

Fixed some broken parts in comments, which were introduced in r11402.
git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@16740 6b1eeb88-9816-0410-afa2-b43733a0f04e

org/gjt/sp/jedit/input/AbstractInputHandler.java
@@ -58,7 +58,7 @@ public abstract class AbstractInputHandler<E extends JEditAbstractEditAction>
 		repeatCount = 1;
 	} //}}}
 	
-		//{{{ addKeyBinding() method
+	//{{{ addKeyBinding() method
 	/**
 	 * Adds a key binding to this input handler. The key binding is
 	 * a list of white space separated key strokes of the form
@@ -322,12 +322,7 @@ public abstract class AbstractInputHandler<E extends JEditAbstractEditAction>
 	public abstract void processKeyEvent(KeyEvent evt, int from, boolean global); 
 	//}}}
 
-	//{{{ processKeyEventKeyStrokeHandling() method
-
-	//{{{ handleKey() methodprotected void sendShortcutPrefixOff()
-	/**
-	 *  If 
-	 */
+	//{{{ sendShortcutPrefixOff() method
 	protected void sendShortcutPrefixOff()
 	{
 		if(shortcutOn)
@@ -382,6 +377,7 @@ public abstract class AbstractInputHandler<E extends JEditAbstractEditAction>
 		return b.toString();
 	} //}}}
 
+	//{{{ processKeyEventKeyStrokeHandling() method
 	/**
 	 *
 	 * @param evt the keyboard event