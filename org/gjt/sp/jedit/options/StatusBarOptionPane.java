7a80e94504a14d9fb5d2597b740674678aab753f
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-03-19 21:30:21 +0000

    In status bar option pane, when adding a widget only widgets that are not   already in the bar will be proposed (feature #2937286)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@17501 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/options/StatusBarOptionPane.java
+++ org/gjt/sp/jedit/options/StatusBarOptionPane.java
@@ -3,7 +3,7 @@
  * :tabSize=8:indentSize=8:noTabs=false:
  * :folding=explicit:collapseFolds=1:
  *
- * Copyright (C) 2008 Matthieu Casanova
+ * Copyright (C) 2008-2010 Matthieu Casanova
  * Portions Copyright (C) 2000-2002 Slava Pestov
  *
  * This program is free software; you can redistribute it and/or
@@ -357,7 +357,7 @@ public class StatusBarOptionPane extends AbstractOptionPane
 	//}}}
 
 	//{{{ WidgetSelectionDialog class
-	static private class WidgetSelectionDialog extends EnhancedDialog
+	private class WidgetSelectionDialog extends EnhancedDialog
 	{
 		private JButton ok;
 		private JButton cancel;
@@ -385,8 +385,20 @@ public class StatusBarOptionPane extends AbstractOptionPane
 			labelField = new JTextField();
 
 			widgetLabel = new JLabel(jEdit.getProperty("options.status.edit.widgetLabel"));
-			widgetCombo = new JComboBox(ServiceManager.getServiceNames("org.gjt.sp.jedit.gui.statusbar.StatusWidget"));
 
+			String[] allWidgets = ServiceManager.getServiceNames("org.gjt.sp.jedit.gui.statusbar.StatusWidget");
+			Vector<String> widgets = new Vector<String>(allWidgets.length);
+			Set<String> usedWidget = new HashSet<String>(listModel.getSize());
+			for (int i = 0; i < listModel.getSize(); i++)
+			{
+				usedWidget.add((String) listModel.get(i));
+			}
+			for (String widget : allWidgets)
+			{
+				if (!usedWidget.contains(widget))
+					widgets.add(widget);
+			}
+			widgetCombo = new JComboBox(widgets);
 			ActionHandler actionHandler = new ActionHandler();
 			labelRadio.addActionListener(actionHandler);
 			widgetRadio.addActionListener(actionHandler);
