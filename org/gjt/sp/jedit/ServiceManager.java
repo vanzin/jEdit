a8bbc9bf6dfdddad284b09d2be5e2acff3b7a819
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-04-09 18:54:57 +0000

    added javadoc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@17613 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/ServiceManager.java
+++ org/gjt/sp/jedit/ServiceManager.java
@@ -212,7 +212,7 @@ public class ServiceManager
 		return getServiceNames(clazz.getName());
 	} //}}}
 
-	//{{{ getService() method
+	//{{{ getService() methods
 	/**
 	 * Returns an instance of the given service. The first time this is
 	 * called for a given service, the BeanShell code is evaluated. The
@@ -246,10 +246,21 @@ public class ServiceManager
 		}
 	} //}}}
 
+    /**
+     * Returns an instance of the given service. The first time this is
+	 * called for a given service, the BeanShell code is evaluated. The
+	 * result is cached for future invocations, so in effect services are
+	 * singletons.
+     *
+     * @param clazz The service class
+	 * @param name The service name
+     * @return the service instance
+     * @since jEdit 4.4pre1
+     */
 	public static <E> E getService(Class<E> clazz, String name)
 	{
 		return (E) getService(clazz.getName(), name);
-	}
+	} //}}}
 
 	//{{{ Package-private members
 
