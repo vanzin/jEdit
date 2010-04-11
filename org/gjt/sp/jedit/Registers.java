891b335324227b7b050f98cee6a917b030900b3d
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-04-11 02:03:16 +0000

    fixed javadoc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@17618 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/Registers.java
+++ org/gjt/sp/jedit/Registers.java
@@ -213,7 +213,9 @@ public class Registers
 	 * Insets the contents of the specified register into the text area.
 	 * @param textArea The text area
 	 * @param register The register
-	 * @since jEdit 2.7pre2
+     * @param preferredDataFlavor the preferred dataflavor. If not available
+	 * <tt>DataFlavor.stringFlavor</tt> will be used
+	 * @since jEdit 4.4pre1
 	 */
 	public static void paste(TextArea textArea, char register, DataFlavor preferredDataFlavor)
 	{
