cd10c2d00d7218ce11cc1c123a9d6bce589373d3
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-04-16 10:47:56 +0000

    javadoc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@17651 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/buffer/ContentManager.java
+++ org/gjt/sp/jedit/buffer/ContentManager.java
@@ -103,7 +103,12 @@ public class ContentManager
 	//{{{ getSegment() method
 	/**
 	 * Returns a read-only segment of the buffer.
+	 * It doesn't copy the text
 	 *
+	 * @param start The start offset
+	 * @param len The number of characters to get
+	 *
+	 * @return a CharSequence that contains the text wanted text
 	 * @since jEdit 4.3pre15
 	 */
 	public CharSequence getSegment(int start, int len)
--- org/gjt/sp/jedit/buffer/JEditBuffer.java
+++ org/gjt/sp/jedit/buffer/JEditBuffer.java
@@ -597,10 +597,12 @@ public class JEditBuffer
 	//{{{ getSegment() method
 	/**
 	 * Returns the specified text range. This method is thread-safe.
+	 * It doesn't copy the text
 	 *
 	 * @param start The start offset
 	 * @param length The number of characters to get
 	 *
+	 * @return a CharSequence that contains the text wanted text
 	 * @since jEdit 4.3pre15
 	 */
 	public CharSequence getSegment(int start, int length)
