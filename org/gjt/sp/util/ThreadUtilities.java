5e5eacb8f3f67301857cb0f64474a9e15c4887cf
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-05-06 15:49:22 +0000

    beginning work on a new ThreadPool
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@17751 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/util/ThreadUtilities.java
+++ org/gjt/sp/util/ThreadUtilities.java
@@ -4,6 +4,7 @@
  * :folding=explicit:collapseFolds=1:
  *
  * Copyright (C) 2010 Matthieu Casanova
+ * Portions Copyright (C) 2010 Marcelo Vanzin
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,14 +22,21 @@
  */
 package org.gjt.sp.util;
 
+//{{{ Imports
 import java.awt.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+//}}}
 
 /**
  * @author Matthieu Casanova
+ * @author Marcelo Vanzin
  * @since jEdit 4.4pre1
  */
 public class ThreadUtilities
 {
+	//{{{ runInDispatchThread() method
+
 	/**
 	 * Run the runnable in EventDispatch Thread.
 	 * If the current thread is EventDispatch, it will run
@@ -41,13 +49,98 @@ public class ThreadUtilities
 	 */
 	public static void runInDispatchThread(Runnable runnable)
 	{
-		if(EventQueue.isDispatchThread())
+		if (EventQueue.isDispatchThread())
 			runnable.run();
 		else
 			EventQueue.invokeLater(runnable);
+	} //}}}
+
+	public static void executeAndWait(Runnable runnable)
+	{
+		MyRunnable run = new MyRunnable(runnable);
+		execute(run);
+		while (!run.done)
+		{
+			synchronized (run)
+			{
+				try
+				{
+					run.wait(1000L);
+				}
+				catch (InterruptedException e)
+				{
+					Log.log(Log.ERROR, ThreadUtilities.class, e);
+				}
+			}
+		}
 	}
+
+	//{{{ execute() method
+
+	/**
+	 * Run the runnable in the threadpool.
+	 *
+	 * @param runnable the runnable to run
+	 * @since jEdit 4.4pre1
+	 */
+	public static void execute(Runnable runnable)
+	{
+		threadPool.execute(runnable);
+	} //}}}
 
 	private ThreadUtilities()
 	{
+	}
+
+	private static class JEditThreadFactory implements ThreadFactory
+	{
+
+		private JEditThreadFactory()
+		{
+			threadIDs = new AtomicInteger(0);
+			threadGroup = new ThreadGroup("jEdit Workers");
+		}
+
+		public Thread newThread(Runnable r)
+		{
+			Thread t = new Thread(threadGroup, r);
+			t.setName("jEdit Worker #" +
+				threadIDs.getAndIncrement());
+			return t;
+		}
+
+		private final AtomicInteger threadIDs;
+		private final ThreadGroup threadGroup;
+	}
+
+	private static final ExecutorService threadPool;
+
+	static
+	{
+		threadPool = Executors.newCachedThreadPool(new JEditThreadFactory());
+		((ThreadPoolExecutor) threadPool).setCorePoolSize(2);
+		((ThreadPoolExecutor) threadPool).setMaximumPoolSize(10);
+	}
+
+	private static class MyRunnable implements Runnable
+	{
+		private final Runnable runnable;
+
+		private volatile boolean done;
+
+		private MyRunnable(Runnable runnable)
+		{
+			this.runnable = runnable;
+		}
+
+		public void run()
+		{
+			runnable.run();
+			done = true;
+			synchronized (runnable)
+			{
+				notifyAll();
+			}
+		}
 	}
 }
