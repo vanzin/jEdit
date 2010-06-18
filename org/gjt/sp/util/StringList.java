62b125a4334d66dcfe5f5210d21b2b5aed8a926b
voituk <voituk@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-06-18 21:07:48 +0000

    Minor code refactoring
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@18112 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/util/StringList.java
+++ org/gjt/sp/util/StringList.java
@@ -97,13 +97,11 @@ public class StringList extends ArrayList<String>
 	 * The reverse of split - given a collection, takes each element
 	 * and places it in a string, joined by a delimiter.
 	 */
-	public static String join(Collection c, String delim)
+	public static String join(Collection<String> c, String delim)
 	{
 		StringList sl = new StringList();
-		for (Object o: c) {
-			String s = o.toString();
+		for (String s: c)
 			sl.add(s);
-		}
 		return sl.join(delim);
 	}
 
@@ -116,9 +114,7 @@ public class StringList extends ArrayList<String>
 	 */
 	public static String join(Object[] arr, String delim) 
 	{
-		StringList sl = new StringList();
-		sl.addAll(arr);
-		return sl.join(delim);
+		return new StringList(arr).join(delim);
 	}
 
 
