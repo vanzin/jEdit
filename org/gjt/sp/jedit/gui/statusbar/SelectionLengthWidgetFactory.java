888e166cf56d5ae16f051bf87432c93105898afb
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-03-12 10:42:06 +0000

    The selectionLength status bar widget is now updated when jEdit starts, and says 0 instead of nothing if there is no selection
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@19442 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/statusbar/SelectionLengthWidgetFactory.java
+++ org/gjt/sp/jedit/gui/statusbar/SelectionLengthWidgetFactory.java
@@ -73,6 +73,32 @@ public class SelectionLengthWidgetFactory implements StatusWidgetFactory
 
 		public void update()
 		{
+			Selection selection = textArea.getSelectionAtOffset(textArea.getCaretPosition());
+			if (selection == null)
+			{
+				selectionLength.setText("0");
+			}
+			else
+			{
+				int selectionEnd = selection.getEnd();
+				int selectionStart = selection.getStart();
+				int len;
+				if (selection instanceof Selection.Rect)
+				{
+					int startLine = selection.getStartLine();
+					int endLine = selection.getEndLine();
+					JEditTextArea textArea = view.getTextArea();
+					int startLineOffset = textArea.getLineStartOffset(startLine);
+					int endLineOffset = textArea.getLineStartOffset(endLine);
+					int lines = endLine - startLine + 1;
+					int columns = (selectionEnd - endLineOffset) -
+						(selectionStart - startLineOffset);
+					len = lines * columns;
+				}
+				else
+					len = selectionEnd - selectionStart;
+				selectionLength.setText(Integer.toString(len));
+			}
 		}
 
 		public void propertiesChanged()
@@ -118,32 +144,7 @@ public class SelectionLengthWidgetFactory implements StatusWidgetFactory
 
 			public void caretUpdate(CaretEvent e)
 			{
-				Selection selection = textArea.getSelectionAtOffset(textArea.getCaretPosition());
-				if (selection == null)
-				{
-					setText("");
-				}
-				else
-				{
-					int selectionEnd = selection.getEnd();
-					int selectionStart = selection.getStart();
-					int len;
-					if (selection instanceof Selection.Rect)
-					{
-						int startLine = selection.getStartLine();
-						int endLine = selection.getEndLine();
-						JEditTextArea textArea = view.getTextArea();
-						int startLineOffset = textArea.getLineStartOffset(startLine);
-						int endLineOffset = textArea.getLineStartOffset(endLine);
-						int lines = endLine - startLine + 1;
-						int columns = (selectionEnd - endLineOffset) -
-							(selectionStart - startLineOffset);
-						len = lines * columns;
-					}
-					else
-						len = selectionEnd - selectionStart;
-					setText(Integer.toString(len));
-				}
+				SelectionLengthWidget.this.update();
 			}
 		}
 	} //}}}
