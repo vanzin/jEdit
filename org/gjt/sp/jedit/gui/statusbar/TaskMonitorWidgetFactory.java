5b8aa23b99af99d1f0af9d2e723ac1a2b94dfc0a
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-06-15 15:47:36 +0000

    Added a tooltip to the taskmonitor widget. Added taskmonitor to list of default status widgets.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@19585 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/statusbar/TaskMonitorWidgetFactory.java
+++ org/gjt/sp/jedit/gui/statusbar/TaskMonitorWidgetFactory.java
@@ -46,6 +46,7 @@ public class TaskMonitorWidgetFactory implements StatusWidgetFactory
     public Widget getWidget(View view)
 	{
 		Widget widget = new TaskMonitorWidget(view);
+		widget.getComponent().setToolTipText(jEdit.getProperty("statusbar.task-monitor.tooltip"));
 		return widget;
 	} //}}}
 
