602e07b0285d390c5c0181d8751a48867a62f75f
kerik-sf <kerik-sf@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-10-01 16:28:09 +0000

    fix bug #3052815 "Update silently fails with wrong proxy password", adding detailed error messages for common cases.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@20035 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/pluginmgr/PluginList.java
+++ org/gjt/sp/jedit/pluginmgr/PluginList.java
@@ -25,6 +25,7 @@ package org.gjt.sp.jedit.pluginmgr;
 //{{{ Imports
 import java.io.*;
 import java.net.URL;
+import java.net.HttpURLConnection;
 import java.util.*;
 import java.util.zip.GZIPInputStream;
 
@@ -62,7 +63,7 @@ class PluginList
 	private String cachedURL;
 	private final Task task;
 	String gzipURL;
-	//{{{ PluginList constructor
+
 	PluginList(Task task)
 	{
 		id = jEdit.getProperty("plugin-manager.mirror.id");
@@ -170,19 +171,52 @@ class PluginList
 	{
 		BufferedInputStream is = null;
 		BufferedOutputStream out = null;
+		/* download the plugin list, while trying to show informative error messages.
+		 * Currently when :
+		 * - the proxy requires authentication
+		 * - another HTTP error happens (may be good to know that the site is broken)
+		 * - the host can't be reached (reported as internet access error)
+		 * Otherwise, only an error message is logged in the activity log.
+		 **/
 		try
 		{
 			
 			task.setStatus(jEdit.getProperty("plugin-manager.list-download"));
-			InputStream inputStream = new URL(gzipURL).openStream();
-			String fileName = cachedURL.replaceFirst("file:///", "");
-			out = new BufferedOutputStream(new FileOutputStream(fileName));
-			long start = System.currentTimeMillis();
-			is = new BufferedInputStream(inputStream);
-			IOUtilities.copyStream(4096, null, is, out, false);
-			jEdit.setProperty("plugin-manager.mirror.cached-id", id);
-			Log.log(Log.MESSAGE, this, "Updated cached pluginlist " + (System.currentTimeMillis() - start));
+			URL downloadURL = new URL(gzipURL);
+			HttpURLConnection c = (HttpURLConnection)downloadURL.openConnection();
+			if(c.getResponseCode() == HttpURLConnection.HTTP_PROXY_AUTH)
+			{
+				GUIUtilities.error(jEdit.getActiveView()
+					, "plugin-manager.list-download.need-password"
+					, new Object[]{});
+				Log.log (Log.ERROR, this, "CacheRemotePluginList: proxy requires authentication");
+			}
+			else if(c.getResponseCode() == HttpURLConnection.HTTP_OK)
+			{
+				InputStream inputStream = c.getInputStream();
+				String fileName = cachedURL.replaceFirst("file:///", "");
+				out = new BufferedOutputStream(new FileOutputStream(fileName));
+				long start = System.currentTimeMillis();
+				is = new BufferedInputStream(inputStream);
+				IOUtilities.copyStream(4096, null, is, out, false);
+				jEdit.setProperty("plugin-manager.mirror.cached-id", id);
+				Log.log(Log.MESSAGE, this, "Updated cached pluginlist " + (System.currentTimeMillis() - start));
+			}
+			else
+			{
+				GUIUtilities.error(jEdit.getActiveView()
+					, "plugin-manager.list-download.generic-error"
+					, new Object[]{c.getResponseCode(), c.getResponseMessage()});
+				Log.log (Log.ERROR, this, "CacheRemotePluginList: HTTP error: "+c.getResponseCode()+ c.getResponseMessage());
+			}
 		}
+		catch(java.net.UnknownHostException e)
+		{
+				GUIUtilities.error(jEdit.getActiveView()
+					, "plugin-manager.list-download.disconnected"
+					, new Object[]{e.getMessage()});
+			Log.log (Log.ERROR, this, "CacheRemotePluginList: error", e);
+		}	
 		catch (Exception e)
 		{
 			Log.log (Log.ERROR, this, "CacheRemotePluginList: error", e);
