d1bac42fbaabe538c072159e03d49d79d1e895fb
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-10-02 01:11:56 +0000

    Merge req# 3389428: Update and install panel not refreshed
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/branches/4.4.x@20039 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/pluginmgr/PluginManager.java
+++ org/gjt/sp/jedit/pluginmgr/PluginManager.java
@@ -137,6 +137,18 @@ public class PluginManager extends JFrame
 
 	public static final String PROPERTY_PLUGINSET = "plugin-manager.pluginset.path";
 
+	//{{{ pluginRemoved() method
+	/**
+	 * A callback called by the @link ManagePanel} when a plugin is removed.
+	 * In that case, if the plugin had an update, the updated has to remove it,
+	 * and the installer panel has to show the plugin again.
+	 */
+	void pluginRemoved()
+	{
+		updater.updateModel();
+		installer.updateModel();
+	} //}}}
+
 	//{{{ PluginManager constructor
 	private PluginManager(Frame parent)
 	{
