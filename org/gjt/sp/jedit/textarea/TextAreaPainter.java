d46f706bb80ec696504fc9616a9beca5b87f18b2
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-10-17 18:15:58 +0000

    New method TextAreaPainter.getRenderingHints() expose the rendering hints (patch #2973861)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@20101 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/TextAreaPainter.java
+++ org/gjt/sp/jedit/textarea/TextAreaPainter.java
@@ -830,6 +830,18 @@ public class TextAreaPainter extends JComponent implements TabExpander
 		}
 	} //}}}
 
+	//{{{ getRenderingHints() method
+	/**
+	 * Returns the rendering hints used by the Graphics2D class; in this
+         * case, for anti-aliasing of text.
+	 *
+	 * @since jEdit 4.5pre1
+	 */
+        public RenderingHints getRenderingHints()
+	{
+            return renderingHints;
+        }
+
 	//{{{ update() method
 	/**
 	 * Repaints the text.
