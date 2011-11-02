cab3785d9dc481c266e23106ae2ee24c7bdbc45f
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-11-02 17:17:43 +0000

    removed unnecessary public and added licence
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@20188 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/Node.java
+++ org/gjt/sp/jedit/textarea/Node.java
@@ -1,10 +1,31 @@
+/*
+ * ColumnBlock.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2010 Anshal Shukla
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package org.gjt.sp.jedit.textarea;
 
 import java.util.Vector;
 
 public interface Node 
 {
-	public void addChild(Node node);
-	public Vector  getChildren();
-	public Node getParent();
+	void addChild(Node node);
+	Vector  getChildren();
+	Node getParent();
 }
