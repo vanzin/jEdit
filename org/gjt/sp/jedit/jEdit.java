b07d584d03d0f1a1ecc95abe923aa06902a49f99
Vampire0 <Vampire0@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-11-20 01:02:34 +0000

    Update the version number to 4.5pre2
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/branches/4.5.x@20381 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -90,7 +90,7 @@ public class jEdit
 	public static String getBuild()
 	{
 		// (major).(minor).(<99 = preX, 99 = "final").(bug fix)
-		return "04.05.01.00";
+		return "04.05.02.00";
 	} //}}}
 
 	//{{{ main() method
