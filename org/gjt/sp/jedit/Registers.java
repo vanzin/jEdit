c503a6bafcc6986af7ab5f7fe64a4eba30a0bd2c
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-12-09 17:41:23 +0000

    factored some duplicate code
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@20593 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/Registers.java
+++ org/gjt/sp/jedit/Registers.java
@@ -273,54 +273,7 @@ public class Registers
 		}
 		JEditBuffer buffer = textArea.getBuffer();
 		applyMode(mode, buffer);
-		try
-		{
-			buffer.beginCompoundEdit();
-
-			/* vertical paste */
-			if(vertical && textArea.getSelectionCount() == 0)
-			{
-				int caret = textArea.getCaretPosition();
-				int caretLine = textArea.getCaretLine();
-				Selection.Rect rect = new Selection.Rect(
-					caretLine,caret,caretLine,caret);
-				textArea.setSelectedText(rect,selection);
-				caretLine = textArea.getCaretLine();
-
-				if(caretLine != textArea.getLineCount() - 1)
-				{
-
-					int startColumn = rect.getStartColumn(
-						buffer);
-					int offset = buffer
-						.getOffsetOfVirtualColumn(
-						caretLine + 1,startColumn,null);
-					if(offset == -1)
-					{
-						buffer.insertAtColumn(caretLine + 1,startColumn,"");
-						textArea.setCaretPosition(
-							buffer.getLineEndOffset(
-							caretLine + 1) - 1);
-					}
-					else
-					{
-						textArea.setCaretPosition(
-							buffer.getLineStartOffset(
-							caretLine + 1) + offset);
-					}
-				}
-			}
-			else /* Regular paste */
-			{
-				textArea.replaceSelection(selection);
-			}
-		}
-		finally
-		{
-			buffer.endCompoundEdit();
-		}
-
-		HistoryModel.getModel("clipboard").addItem(selection);
+		_paste(textArea, vertical, selection, buffer);
 	}
 
 	/**
@@ -387,6 +340,11 @@ public class Registers
 				applyMode(_mode, buffer);
 			}
 		}     */
+		_paste(textArea, vertical, selection, buffer);
+	}
+
+	private static void _paste(TextArea textArea, boolean vertical, String selection, JEditBuffer buffer)
+	{
 		try
 		{
 			buffer.beginCompoundEdit();
