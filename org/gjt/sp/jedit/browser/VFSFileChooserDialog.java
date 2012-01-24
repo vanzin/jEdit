07080d82a883f333021a56adebdeca127ae4e5c5
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-01-24 20:24:26 +0000

    Dialog titles in file system browser (SPennington - #2968930, #2807301)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@20945 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/browser/VFSFileChooserDialog.java
+++ org/gjt/sp/jedit/browser/VFSFileChooserDialog.java
@@ -68,7 +68,7 @@ public class VFSFileChooserDialog extends EnhancedDialog
 	public VFSFileChooserDialog(View view, String path,
 		int mode, boolean multipleSelection, boolean autoshow)
 	{
-		super(view,getDefaultTitle(),true);
+		super(view,getTitle(mode),true);
 		setFocusTraversalPolicy(new LayoutFocusTraversalPolicy());
 		_init(view,path,mode,multipleSelection,autoshow);
 	} //}}}
@@ -83,7 +83,7 @@ public class VFSFileChooserDialog extends EnhancedDialog
 	public VFSFileChooserDialog(Dialog parent, View view, String path,
 		int mode, boolean multipleSelection, boolean autoshow)
 	{
-		super(parent,getDefaultTitle(),true);
+		super(parent,getTitle(mode),true);
 		setFocusTraversalPolicy(new LayoutFocusTraversalPolicy());		
 		_init(view,path,mode,multipleSelection,autoshow);
 	} //}}}
@@ -98,7 +98,7 @@ public class VFSFileChooserDialog extends EnhancedDialog
 	public VFSFileChooserDialog(Frame parent, View view, String path,
 		int mode, boolean multipleSelection, boolean autoshow)
 	{
-		super(parent, getDefaultTitle(),true);
+		super(parent,getTitle(mode),true);
 		setFocusTraversalPolicy(new LayoutFocusTraversalPolicy());		
 		_init(view,path,mode,multipleSelection,autoshow);
 	} //}}}
@@ -264,6 +264,26 @@ public class VFSFileChooserDialog extends EnhancedDialog
 	private static String getDefaultTitle()
 	{
 		return jEdit.getProperty("vfs.browser.title");
+	}// }}}
+
+	//{{{ getTitle() method
+	private static String getTitle(int mode)
+	{
+		switch(mode)
+		{
+		case VFSBrowser.OPEN_DIALOG:
+			return jEdit.getProperty("vfs.browser.title.open");
+		case VFSBrowser.SAVE_DIALOG:
+			return jEdit.getProperty("vfs.browser.title.save");
+		case VFSBrowser.BROWSER:
+			return jEdit.getProperty("vfs.browser.title");
+		case VFSBrowser.CHOOSE_DIRECTORY_DIALOG:
+			return jEdit.getProperty("vfs.browser.title");
+		case VFSBrowser.BROWSER_DIALOG:
+			return jEdit.getProperty("vfs.browser.title.dialog");
+		default:
+			return jEdit.getProperty("vfs.browser.title");
+		}
 	}// }}}
 
 	//{{{ _init method
