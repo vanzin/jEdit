22da16c985f79561d4bbaf9923b00732a9a82eb6
jarekczek <jarekczek@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-05-19 15:38:08 +0000

    Optimized LineManager.getLineOfOffset for the case when it is called   sequentially many times for the same line. (patch #3528212 Thomas Meyer)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@21685 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/buffer/LineManager.java
+++ org/gjt/sp/jedit/buffer/LineManager.java
@@ -61,6 +61,20 @@ public class LineManager
 	//{{{ getLineOfOffset() method
 	public int getLineOfOffset(int offset)
 	{
+		/*
+		 * Performance optimization: assumption is that this method
+		 * is called many times for the same line.
+		 * Not optimizing the first line is intentional,
+		 * profiling proves this is the right approach (#3528212).
+		 */
+		if(getLineOfOffsetLine > 0 && getLineOfOffsetLine < lineCount)
+		{
+			int s = getLineEndOffset(getLineOfOffsetLine - 1);
+			int e = getLineEndOffset(getLineOfOffsetLine);
+			if(offset >= s && offset < e)
+				return getLineOfOffsetLine;
+		}
+
 		int start = 0;
 		int end = lineCount - 1;
 
@@ -70,24 +84,29 @@ public class LineManager
 			{
 			case 0:
 				if(getLineEndOffset(start) <= offset)
-					return start + 1;
+					getLineOfOffsetLine = start + 1;
 				else
-					return start;
+					getLineOfOffsetLine = start;
+				return getLineOfOffsetLine;
 			case 1:
 				if(getLineEndOffset(start) <= offset)
 				{
 					if(getLineEndOffset(end) <= offset)
-						return end + 1;
+						getLineOfOffsetLine = end + 1;
 					else
-						return end;
+						getLineOfOffsetLine = end;
 				}
 				else
-					return start;
+					getLineOfOffsetLine = start;
+				return getLineOfOffsetLine;
 			default:
 				int pivot = (end + start) / 2;
 				int value = getLineEndOffset(pivot);
 				if(value == offset)
-					return pivot + 1;
+				{
+					getLineOfOffsetLine = pivot + 1; 
+					return getLineOfOffsetLine;
+				}
 				else if(value < offset)
 					start = pivot + 1;
 				else
@@ -303,6 +322,11 @@ public class LineManager
 	 * have an invalid fold level.
 	 */
 	private int firstInvalidFoldLevel;
+
+	/**
+	 * Performance helper for {@link #getLineOfOffset}. 
+	 */
+	private int getLineOfOffsetLine = -1;
 	//}}}
 
 	//{{{ setLineEndOffset() method
