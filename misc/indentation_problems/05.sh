9f5a552a96dee589017aee7f81128f67647db433
jarekczek <jarekczek@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-05-21 08:10:44 +0000

    Added a new case (05) to indentation problems examples
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@21693 6b1eeb88-9816-0410-afa2-b43733a0f04e

---
+++ misc/indentation_problems/05.sh
@@ -0,0 +1,14 @@
+if a; then
+  doit
+else
+  els
+  
+# In line 4 one is to type "elsen" which is a custom defined function.
+# "else" is a keyword defined in unindentThisLine property and
+# "e" is electric key.
+# It should be indented as it is initially:
+
+if a; then
+  doit;
+else
+  elsen
