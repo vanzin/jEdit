20b1c9c2ca8cb357fb4c3872494409669b19dbb1
jarekczek <jarekczek@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-05-23 05:18:42 +0000

    Made 05.sh example more realistic and shorter.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@21704 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- misc/indentation_problems/05.sh
+++ misc/indentation_problems/05.sh
@@ -1,14 +1,10 @@
 if a; then
-  doit
-else
-  els
-  
-# In line 4 one is to type "elsen" which is a custom defined function.
-# "else" is a keyword defined in unindentThisLine property and
-# "e" is electric key.
+  f
+
+# In line 2 one is to type "find".
+# "fi" is a keyword defined in unindentThisLine property and
+# "i" is an electric key.
 # It should be indented as it is initially:
 
 if a; then
-  doit;
-else
-  elsen
+  find
