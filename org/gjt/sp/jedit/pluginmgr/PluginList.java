cf543d67984e57f97376e45e35a5490f3ed6a06f
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-05-31 17:42:05 +0000

    Implemented "Plugin Central default" as auto selecting mirror, and made it default. (SF.net bug #2913393, #3501210)
    The "switch" workaround had been made since r10595 for bug #1757229.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@21741 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/pluginmgr/PluginList.java
+++ org/gjt/sp/jedit/pluginmgr/PluginList.java
@@ -73,9 +73,11 @@ class PluginList
 	
 	void readPluginList(boolean allowRetry)
 	{
-		gzipURL = jEdit.getProperty("plugin-manager.export-url");	
-		if (!id.equals(MirrorList.Mirror.NONE))
-			gzipURL += "?mirror="+id;		
+		String mirror = buildMirror(id);
+		if (mirror == null)
+			return;
+		gzipURL = jEdit.getProperty("plugin-manager.export-url");
+		gzipURL += "?mirror=" + mirror;
 		String path = null;
 		if (jEdit.getSettingsDirectory() == null)
 		{
@@ -598,4 +600,57 @@ class PluginList
 				+ ",to=" + to + ",plugin=" + plugin + ']';
 		}
 	} //}}}
+
+	//{{{ Private members
+
+	private static String buildMirror(String id)
+	{
+		if (id != null && !id.equals(MirrorList.Mirror.NONE))
+		{
+			return id;
+		}
+		try
+		{
+			return getAutoSelectedMirror();
+		}
+		catch (Exception e)
+		{
+			GUIUtilities.error(jEdit.getActiveView()
+				, "plugin-manager.list-download.mirror-autoselect-error"
+				, new Object[]{e});
+			Log.log(Log.DEBUG, PluginList.class, "Getting auto-selected mirror: error", e);
+		}
+		return null;
+	}
+
+	private static String getAutoSelectedMirror()
+		throws java.io.IOException
+	{
+		final String samplerUrl = "http://sourceforge.net/projects/jedit/files/latest/download";
+		final HttpURLConnection connection = (HttpURLConnection)((new URL(samplerUrl)).openConnection());
+		connection.setInstanceFollowRedirects(false);
+		final int response = connection.getResponseCode();
+		if (response != HttpURLConnection.HTTP_MOVED_TEMP)
+		{
+			throw new RuntimeException("Unexpected response: " + response + ": from " + samplerUrl);
+		}
+		final String redirected = connection.getHeaderField("Location");
+		if (redirected == null)
+		{
+			throw new RuntimeException("Missing Location header: " + samplerUrl);
+		}
+		final String prefix = "use_mirror=";
+		final int found = redirected.lastIndexOf(prefix);
+		if (found == -1)
+		{
+			throw new RuntimeException("Mirror prefix \"use_mirror\" was not found in redirected URL: " + redirected);
+		}
+		final int start = found + prefix.length();
+		final int end = redirected.indexOf('&', start);
+		return end != -1 ?
+			redirected.substring(start, end) :
+			redirected.substring(start);
+	}
+
+	//}}}
 }
