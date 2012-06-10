8ed76b58fba7f7b0a867091e2f892a27b905aae1
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-06-10 14:37:41 +0000

    javadoc again.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@21787 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -2707,11 +2707,11 @@ public class jEdit
 	 * are stored. This will be <code>null</code> if jEdit was
 	 * started with the <code>-nosettings</code> command-line switch; do not
 	 * blindly use this method without checking for a <code>null</code>
-	 * return value first.
-	 *
-	 * NOTE: plugins should <b>not</b> use this directory as a base to 
-	 * store their files. Instead they should use EditPlugin.getPluginHome().
-	 * @see EditPlugin.getPluginHome()
+	 * return value first. <p>
+	 * 
+	 * <b>NOTE</b>: plugins should <b>not</b> use this directory as a base to 
+	 * store their files. Instead, they should use EditPlugin.getPluginHome().
+	 * @see EditPlugin#getPluginHome()
 	 */
 	public static String getSettingsDirectory()
 	{
