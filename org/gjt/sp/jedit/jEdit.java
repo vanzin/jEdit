94d51740a519a722effbd77fd8601d3167ad51a9
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-06-10 02:43:01 +0000

    Bumped the trunk version to 5.1 since the release branch for 5.0.x had been created.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@21782 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -93,7 +93,7 @@ public class jEdit
 	public static String getBuild()
 	{
 		// (major).(minor).(<99 = preX, 99 = "final").(bug fix)
-		return "05.00.01.00";
+		return "05.01.01.00";
 	} //}}}
 
 	//{{{ main() method
