4c1a43f63ff05aafc55549489ad8e070e3171a2f
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-07-03 15:35:12 +0000

    reverted changes done in 21918 as it introduce another bug Two bounds must be saved, one for full screen the other one for normal state
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@21920 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/GUIUtilities.java
+++ org/gjt/sp/jedit/GUIUtilities.java
@@ -2202,7 +2202,6 @@ public class GUIUtilities
 					jEdit.setIntegerProperty(name + ".height",bounds.height);
 					break;
 
-				case Frame.MAXIMIZED_BOTH:
 				case Frame.NORMAL:
 					saveGeometry(frame,parent,name );
 					break;
