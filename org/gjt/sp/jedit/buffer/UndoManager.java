21614ae2b2565f496c05147915c404723e11c2b3
k_satoda <k_satoda@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-07-18 14:09:45 +0000

    In a CompoundEdit compact a sequence of Remove&Insert Edits into a Replace Edit; further compact consecutive Replace Edits into a CompressedReplace Edit object. (Patch #3531515 by Thomas Meyer)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@21939 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/buffer/UndoManager.java
+++ org/gjt/sp/jedit/buffer/UndoManager.java
@@ -23,6 +23,7 @@
 package org.gjt.sp.jedit.buffer;
 
 //{{{ Imports
+import org.gjt.sp.util.IntegerArray;
 import org.gjt.sp.util.Log;
 //}}}
 
@@ -191,7 +192,7 @@ public class UndoManager
 		}
 
 		if(compoundEdit != null)
-			compoundEdit.add(ins);
+			compoundEdit.add(this, ins);
 		else
 		{
 			reviseUndoId();
@@ -236,7 +237,7 @@ public class UndoManager
 		}
 
 		if(compoundEdit != null)
-			compoundEdit.add(rem);
+			compoundEdit.add(this, rem);
 		else
 		{
 			reviseUndoId();
@@ -339,6 +340,73 @@ public class UndoManager
 		undoId = new Object();
 	} //}}}
 
+	//{{{ getReplaceFromRemoveInsert() method
+	// a Replace Edit is a Remove Edit and then an Insert Edit
+	private Replace getReplaceFromRemoveInsert(Edit lastElement, Edit newElement)
+	{
+		if(lastElement instanceof Remove && newElement instanceof Insert)
+		{
+			// don't fold a undoClearDirty Remove Edit, because
+			// it's the identity is significant.
+			if(lastElement == undoClearDirty)
+				return null;
+
+			/* newElement is guaranteed to be an Compound-Insert Edit, redoClearDirty will be an Normal-Insert, Normal-Remove,
+			 * Compound-Remove-Insert-Edit or Compound-Replace-Edit (all possible edit operations)
+			 * redoClearDirty cannot become equal to newElement because:
+			 * - redoClearDirty will be set after the file has been saved and the first new change is made, which
+			 *   could be an Normal-Insert, Normal-Remove, Compound-Replace-Edit, Compound-Remove-Insert-Edit,
+			 *   or null, if this is the first change in the file at all.
+			 *   For Compound-Edit case it will be the last element of the Compound edit.
+			 * - As the first Remove&Insert sequence of a Compound-Edit is never compacted by above if statement,
+			 *   redoClearDirty can never be any of the following Remove&Insert elements, as the user as no option to save the
+			 *   file after the first Remove&Insert sequence, because the GUI is blocked by the search&replace all operation.
+			 */  
+			assert newElement  != redoClearDirty;
+			assert lastElement != redoClearDirty;
+			/* search for nothing (via regexp) will results just in Insert-Edits.
+			 * So we won't get here as lastElement will be an Insert, too
+			 */
+			assert newElement  != undoClearDirty;
+
+			Remove rem = (Remove) lastElement;
+			Insert ins = (Insert) newElement;
+		
+			if(rem.offset == ins.offset)
+			{
+				return new Replace(rem.offset, rem.str, ins.str);
+			}
+		}
+		return null;
+	} //}}}
+
+	//{{{ getCompressedReplaceFromReplaceReplace() method
+	// a CompressedReplace Edit is one to many Replace Edit compressed via offsets
+	private CompressedReplace getCompressedReplaceFromReplaceReplace(Edit lastElement, Edit newElement)
+	{
+
+		if(newElement instanceof Replace)
+		{
+			CompressedReplace rep = null;
+			// try to pack the next Replace into the CompressedReplace
+			if(lastElement instanceof CompressedReplace)
+			{
+				rep = (CompressedReplace) lastElement;
+				rep.add((Replace) newElement);
+				return rep;
+			}
+	
+			// try to create a compressed Replace
+			if(lastElement instanceof Replace)
+			{
+				rep = new CompressedReplace((Replace)lastElement);
+				rep.add((Replace) newElement);
+				return rep;
+			}
+		}
+		return null;
+	} //}}}
+
 	//{{{ Inner classes
 
 	//{{{ Edit class
@@ -419,6 +487,89 @@ public class UndoManager
 		String str;
 	} //}}}
 
+	//{{{ Replace class
+	private static class Replace extends Edit
+	{
+		//{{{ Replace constructor
+		Replace(int offset, String strRemove, String strInsert)
+		{
+			this.offset = offset;
+			this.strRemove = strRemove;
+			this.strInsert = strInsert;
+		} //}}}
+
+		//{{{ undo() method
+		int undo(UndoManager mgr)
+		{
+			mgr.buffer.remove(offset,strInsert.length());
+			mgr.buffer.insert(offset,strRemove);
+			assert mgr.undoClearDirty != this;
+			return offset + strRemove.length();
+		} //}}}
+
+		//{{{ redo() method
+		int redo(UndoManager mgr)
+		{
+			mgr.buffer.remove(offset,strRemove.length());
+			mgr.buffer.insert(offset,strInsert);
+			if(mgr.redoClearDirty == this)
+				mgr.buffer.setDirty(false);
+			return offset + strInsert.length();
+		} //}}}
+
+		int offset;
+		String strRemove, strInsert;
+	} //}}}
+
+	//{{{ CompressedReplace class
+	private static class CompressedReplace extends Replace
+	{
+		//{{{ CompressedReplace constructor
+		CompressedReplace(Replace r1)
+		{
+			super(r1.offset, r1.strRemove, r1.strInsert);
+			offsets = new IntegerArray(4);
+			offsets.add(r1.offset);
+		} //}}}
+
+		//{{{ add() method
+		CompressedReplace add(Replace rep)
+		{
+			if(this.strInsert.equals(rep.strInsert) && this.strRemove.equals(rep.strRemove))
+			{
+				offsets.add(rep.offset);
+				return this;
+			}
+			return null;
+		} //}}}
+
+		//{{{ undo() method
+		int undo(UndoManager mgr)
+		{
+			int caret = -1;
+			for(int i = offsets.getSize() - 1; i >= 0; i--)
+			{
+				offset = offsets.get(i);
+				caret = super.undo(mgr);
+			}
+			return caret;
+		} //}}}
+
+		//{{{ redo() method
+		int redo(UndoManager mgr)
+		{
+			int caret = -1;
+			for(int i = 0; i < offsets.getSize(); i++)
+			{
+				offset = offsets.get(i);
+				caret = super.redo(mgr);
+			}
+			return caret;
+		} //}}}
+
+		IntegerArray offsets;
+	} //}}}
+
 	//{{{ CompoundEdit class
 	private static class CompoundEdit extends Edit
 	{
@@ -448,8 +599,8 @@ public class UndoManager
 			return retVal;
 		} //}}}
 
-		//{{{ add() method
-		public void add(Edit edit)
+		//{{{ _add() method
+		private void _add(Edit edit)
 		{
 			if(first == null)
 				first = last = edit;
@@ -457,6 +608,60 @@ public class UndoManager
 			{
 				edit.prev = last;
 				last.next = edit;
+				last = edit;
+			}
+		} //}}}
+
+		//{{{ add() method
+		public void add(UndoManager mgr, Edit edit)
+		{
+			_add(edit);
+
+			// try to compact a sequence of Remove and Insert into a Replace
+			// Edit to save memory for large search&replace operations
+			if(last.prev != null)
+			{
+				Edit rep = mgr.getReplaceFromRemoveInsert(last.prev, last);
+				if(rep != null)
+					exchangeLastElement(rep);
+			}
+
+			// try to compress a sequence of Replace and Replace into a "CompressedReplace"
+			if(last.prev != null)
+			{
+				Edit rep = mgr.getCompressedReplaceFromReplaceReplace(last.prev, last);
+				if(rep != null)
+					exchangeLastElement(rep);
+			}
+			
+			// try to compress a sequence of CompressedReplace and Replace into a "CompressedReplace"
+			if(last.prev != null)
+			{
+				Edit rep = mgr.getCompressedReplaceFromReplaceReplace(last.prev, last);
+				if(rep != null)
+					exchangeLastElement(rep);
+			}
+		} //}}}
+
+		//{{{ exchangeLastElement() method
+		private void exchangeLastElement(Edit edit)
+		{
+			// remove last
+			if(first == last)
+				first = last = null;
+			else
+			{
+				last.prev.next = null;
+				last = last.prev;
+			}
+
+			// exchange current last
+			if(first == null || first == last)
+				first = last = edit;
+			else
+			{
+				edit.prev = last.prev;
+				last.prev.next = edit;
 				last = edit;
 			}
 		} //}}}
