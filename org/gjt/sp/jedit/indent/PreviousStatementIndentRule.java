927da30be476e73c89f23070ff1cd1430fc80823
Marcelo Vanzin <vanza@users.sourceforge.net>
2012-08-12 21:04:22 -0700

    Expand the statement-based indent rule to also indent blocks.
    Use the same "base indent" discovery mechanism when calculating
    the indent inside blocks, and after blocks are closed.

--- org/gjt/sp/jedit/indent/PreviousStatementIndentRule.java
+++ org/gjt/sp/jedit/indent/PreviousStatementIndentRule.java
@@ -23,6 +23,7 @@ package org.gjt.sp.jedit.indent;
 import java.util.List;
 import javax.swing.text.Segment;
 
+import org.gjt.sp.jedit.TextUtilities;
 import org.gjt.sp.jedit.buffer.JEditBuffer;
 import org.gjt.sp.jedit.syntax.Token;
 import org.gjt.sp.jedit.syntax.TokenHandler;
@@ -50,6 +51,25 @@ import org.gjt.sp.util.StandardUtilities;
  * starts, this rule implements some basic parsing of the buffer to
  * try to figure that out.
  * <p>
+ * The rule also checks for blocks of code that should be indented,
+ * and follows the same rules for defining the base indent within
+ * the block, and after the block is closed:
+ *
+ *
+ * <blockquote><tt>
+ *   1.   String statementBeforeBlock;
+ *   2.
+ *   3.   if (blockIsOpened) {
+ *   4.       statementWithinBlock;
+ *   5.   }
+ *   6.
+ *   7.   statementAfterBlock;
+ * </tt></blockquote>
+ *
+ * In the example above, both the indent of "statementWithinBlock" and
+ * "statementAfterBlock" are calculated based on the indent of line 3,
+ * which is the first statement that preceeds the start of the block.
+ * <p>
  * It works like the following: if the previous line ends with a
  * statement terminator (e.g., line 4), the code will parse previous
  * lines in the buffer until it finds the end of a previous statement
@@ -74,11 +94,8 @@ public class PreviousStatementIndentRule implements IndentRule
 									   String closeBrackets)
 	{
 		this.statementEnd = statementEnd;
-
-		if (openBrackets != null && closeBrackets != null)
-			this.brackets = openBrackets + closeBrackets;
-		else
-			this.brackets = null;
+		this.openBrackets = openBrackets;
+		this.closeBrackets = closeBrackets;
 	}
 
 	public void apply(JEditBuffer buffer, int thisLineIndex,
@@ -88,14 +105,47 @@ public class PreviousStatementIndentRule implements IndentRule
 		if (prevLineIndex < 0)
 			return;
 
-		Scanner s = new Scanner(false);
-		buffer.markTokens(prevLineIndex, s);
+		Scanner s = null;
+		boolean isBlockStart = false;
+
+		// Check to see if we're closing a block. If so, start the
+		// scan from the point where the block was opened.
+		if (closeBrackets != null) {
+			s = new Scanner(closeBrackets, false);
+			buffer.markTokens(thisLineIndex, s);
+			if (s.isFound())
+			{
+				int offset = TextUtilities.findMatchingBracket(
+					buffer, thisLineIndex, s.getIndex());
+				prevLineIndex = buffer.getLineOfOffset(offset);
+				if (prevLineIndex < 0)
+					return;
+			}
+		}
+
+		// Check if it's a statement end.
+		if (s == null || !s.isFound()) {
+			s = new Scanner(statementEnd, false);
+			buffer.markTokens(prevLineIndex, s);
+		}
+
+		if (!s.isFound() && openBrackets != null)
+		{
+			// If it's not a statement end, check to see if we're
+			// starting a new block.
+			s = new Scanner(openBrackets, false);
+			buffer.markTokens(prevLineIndex, s);
+			if (s.isFound())
+				isBlockStart = true;
+		}
+
 		if (!s.isFound())
 			return;
 
-		// We found the end of a statement. Start backtracking to find
-		// the previous statement.
-		s = new Scanner(true);
+		// We found something for which we should calculate the
+		// new indent. Start backtracking to find the previous
+		// statement.
+		s = new Scanner(statementEnd, true);
 		int baseLine = prevLineIndex;
 		for (int lineIndex = buffer.getPriorNonEmptyLine(prevLineIndex);
 			 lineIndex >= 0;
@@ -103,25 +153,32 @@ public class PreviousStatementIndentRule implements IndentRule
 		{
 			buffer.markTokens(lineIndex, s);
 			if (s.isFound())
-			{
 				break;
-			}
 			baseLine = lineIndex;
 		}
 
+		indentActions.clear();
 		indentActions.add(new IndentAction.SetBaseIndent(baseLine));
+		if (isBlockStart)
+			indentActions.add(new IndentAction.Increase());
 	}
 
-	private final String brackets;
+	private final String closeBrackets;
+	private final String openBrackets;
 	private final String statementEnd;
 
 	private class Scanner implements TokenHandler
 	{
 
-		Scanner(boolean searchBrackets)
+		Scanner(String needle, boolean searchBrackets)
 		{
 			this.index = -1;
-			this.searchBrackets = searchBrackets;
+			this.needle = needle;
+			if (searchBrackets && openBrackets != null &&
+				closeBrackets != null)
+				this.brackets = openBrackets + closeBrackets;
+			else
+				this.brackets = null;
 		}
 
 		public void handleToken(Segment seg,
@@ -155,7 +212,10 @@ public class PreviousStatementIndentRule implements IndentRule
 			if (isEmpty)
 				return;
 
-			index = StandardUtilities.indexOf(seq, statementEnd);
+			for (int i = 0; i < needle.length(); i++) {
+				index = Math.max(index,
+					StandardUtilities.lastIndexOf(seq, needle.charAt(i)));
+			}
 			if (index >= 0)
 			{
 				// The statement end should be the last thing on the line.
@@ -169,7 +229,7 @@ public class PreviousStatementIndentRule implements IndentRule
 				}
 			}
 
-			if (index == -1 && searchBrackets && brackets != null)
+			if (index == -1 && brackets != null)
 			{
 				int idx2 = -1;
 				for (int i = 0; idx2 == -1 && i < brackets.length(); i++)
@@ -180,6 +240,9 @@ public class PreviousStatementIndentRule implements IndentRule
 
 				index = idx2;
 			}
+
+			if (index >= 0)
+				this.offset = offset;
 		}
 
 		public void setLineContext(TokenMarker.LineContext lineContext)
@@ -191,8 +254,15 @@ public class PreviousStatementIndentRule implements IndentRule
 			return index >= 0;
 		}
 
-		private final boolean searchBrackets;
+		int getIndex()
+		{
+			return offset + index;
+		}
+
+		private final String needle;
+		private final String brackets;
 		private int index;
+		private int offset;
 
 	}
 }
