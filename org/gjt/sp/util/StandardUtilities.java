f349ecb83ffdfedfadd58f9ce9d1a1b4dcc8531e
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-08-23 08:34:06 +0000

    fixed coding style and reuse of the same byte array in the loop
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@22044 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/util/StandardUtilities.java
+++ org/gjt/sp/util/StandardUtilities.java
@@ -738,20 +738,25 @@ loop:		for(int i = 0; i < str.length(); i++)
 	 * @param charSequence Given string
 	 * @return md5 sum of given string
 	 */
-	public static byte[] md5(CharSequence charSequence) {
-		final byte[] dummy = new byte[1];
-		try {
+	public static byte[] md5(CharSequence charSequence)
+	{
+		try
+		{
 			MessageDigest digest = MessageDigest.getInstance("MD5");
+			byte[] ba = new byte[2];
 			for(int i = 0, n = charSequence.length(); i < n; i++)
 			{
 				char cp = charSequence.charAt(i);
-				byte ba[] = { (byte)(cp & 0xff), (byte)(cp >> 8 & 0xff) };
+				ba[0] = (byte)(cp & 0xff);
+				ba[1] = (byte)(cp >> 8 & 0xff);
 				digest.update(ba);
 			}
 			return digest.digest();
-		} catch (NoSuchAlgorithmException e) {
+		}
+		catch (NoSuchAlgorithmException e)
+		{
 			Log.log(Log.ERROR, StandardUtilities.class, "Can't Calculate MD5 hash!", e);
-			return dummy;
+			return new byte[1];
 		}
 	}
 	// }}}
