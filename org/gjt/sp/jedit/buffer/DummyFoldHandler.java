6b581b53987ba57df8313456ee1b979167cf0b8f
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2013-04-30 13:55:18 +0000

    added missing @Override
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@22962 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/buffer/DummyFoldHandler.java
+++ org/gjt/sp/jedit/buffer/DummyFoldHandler.java
@@ -49,6 +49,7 @@ public class DummyFoldHandler extends FoldHandler
 	 * @return The fold level of the specified line
 	 * @since jEdit 4.0pre1
 	 */
+	@Override
 	public int getFoldLevel(JEditBuffer buffer, int lineIndex, Segment seg)
 	{
 		return 0;
