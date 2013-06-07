e85d6045048cc690eb4bc0bf187b0247b79f460f
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2013-06-07 15:42:17 +0000

    The column title are missing in shortcuts option pane (#3614404)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@23039 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/options/ShortcutsOptionPane.java
+++ org/gjt/sp/jedit/options/ShortcutsOptionPane.java
@@ -579,9 +579,9 @@ public class ShortcutsOptionPane extends AbstractOptionPane
 			case 0:
 				return jEdit.getProperty("options.shortcuts.name");
 			case 1:
-				return selectedKeymap.getShortcut("options.shortcuts.shortcut1");
+				return jEdit.getProperty("options.shortcuts.shortcut1");
 			case 2:
-				return selectedKeymap.getShortcut("options.shortcuts.shortcut2");
+				return jEdit.getProperty("options.shortcuts.shortcut2");
 			case 3:
 				return jEdit.getProperty("options.shortcuts.actionset");
 			default:
