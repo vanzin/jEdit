2284068c320b37d49b19f025787e2e9635faef36
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2013-07-26 21:40:38 +0000

    Thick caret when in overwrite mode too.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@23077 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/TextAreaPainter.java
+++ org/gjt/sp/jedit/textarea/TextAreaPainter.java
@@ -1477,12 +1477,15 @@ public class TextAreaPainter extends JComponent implements TabExpander
 
 			if(textArea.isOverwriteEnabled())
 			{
-				gfx.drawLine(caretX, y + lineHeight - 1,
-					     caretX + textArea.charWidth,
-					     y + lineHeight - 1);
+				if (thickCaret) 
+					gfx.fillRect(caretX, y + lineHeight - 4, textArea.charWidth, 3);
+				
+				else  gfx.drawLine(caretX, y + lineHeight - 1, 
+					caretX + textArea.charWidth, y + lineHeight - 1);
 			}
 			else if(blockCaret)
-				gfx.drawRect(caretX, y + charOffset, textArea.charWidth - 1, charHeight - 1);
+				gfx.drawRect(caretX, y + charOffset, 
+					textArea.charWidth - 1, charHeight - 1);
 			else
 			{
 				if (thickCaret) 
