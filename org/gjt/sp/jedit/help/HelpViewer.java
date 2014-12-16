4199c865291d0647204467dbefbd7355f7c1153e
kerik-sf <kerik-sf@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2014-12-16 07:06:27 +0000

    fix [bugs:3671] autodetect text file encoding in HelpViewer ([patches:#540])
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@23777 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/help/HelpViewer.java
+++ org/gjt/sp/jedit/help/HelpViewer.java
@@ -43,11 +43,14 @@ import java.awt.datatransfer.StringSelection;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -79,6 +82,8 @@ import org.gjt.sp.jedit.jEdit;
 import org.gjt.sp.jedit.MiscUtilities;
 
 import org.gjt.sp.jedit.EditBus.EBHandler;
+import org.gjt.sp.jedit.io.AutoDetection;
+import org.gjt.sp.jedit.io.RegexEncodingDetector;
 import org.gjt.sp.jedit.msg.PluginUpdate;
 
 import org.gjt.sp.util.Log;
@@ -95,6 +100,7 @@ import static org.gjt.sp.jedit.help.HelpHistoryModel.HistoryEntry;
 public class HelpViewer extends JFrame implements HelpViewerInterface, HelpHistoryModelListener
 {
 	private static final long serialVersionUID = 1L;
+	private static final RegexEncodingDetector ENCODING_DETECTOR = new RegexEncodingDetector(":encoding=([^:]+):", "$1");
 
 	//{{{ HelpViewer constructor
 	/**
@@ -298,6 +304,26 @@ public class HelpViewer extends JFrame implements HelpViewerInterface, HelpHisto
 				{
 					try
 					{
+						// reset encoding
+						viewer.putClientProperty("charset", null);
+						// guess encoding
+						if(_url.getPath().matches(".+\\.([tT][xX][tT])"))
+						{
+							URLConnection connection = _url.openConnection();
+							if(connection.getContentEncoding() == null)
+							{
+								InputStream is = connection.getInputStream();
+								BufferedInputStream in = AutoDetection.getMarkedStream(is);
+								String encoding = ENCODING_DETECTOR.detectEncoding(in);
+								if(encoding != null)
+								{
+									// JEditorPane uses charset to create the reader passed to the
+									// EditorKit in JEditorPane.read().
+									viewer.putClientProperty("charset", encoding);
+								}
+								in.close();
+							}
+						}
 						viewer.setPage(_url);
 						success = true;
 					}
