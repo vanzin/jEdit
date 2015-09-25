feffc97e63735255a95df3425be44a47d140e1d5
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2015-09-25 14:59:14 +0000

    Possible fix for #3916, array index out of bounds exception in ChunkCache.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24094 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/ChunkCache.java
+++ org/gjt/sp/jedit/textarea/ChunkCache.java
@@ -255,12 +255,15 @@ class ChunkCache
 
 	//{{{ getLineInfo() method
 	/**
-	 * Returns the line informations for a given screen line
+	 * Returns the line informations for a given screen line or a non-null
+	 * new LineInfo if the requested <code>screenLine</code> is out of range.
 	 * @param screenLine the screen line
 	 * @return the LineInfo for the screenLine
 	 */
 	LineInfo getLineInfo(int screenLine)
 	{
+		if (screenLine >= lineInfo.length)
+			return new LineInfo();
 		updateChunksUpTo(screenLine);
 		return lineInfo[screenLine];
 	} //}}}
