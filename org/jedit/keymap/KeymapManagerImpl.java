e1a4724bd96b7fa5556fa462d67fa3f24e631560
makarius <makarius@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2015-10-27 13:46:56 +0000

    More space in messages.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24116 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/jedit/keymap/KeymapManagerImpl.java
+++ org/jedit/keymap/KeymapManagerImpl.java
@@ -67,9 +67,9 @@ public class KeymapManagerImpl implements KeymapManager
 		this.systemKeymapFolder = systemKeymapFolder;
 		if (!systemKeymapFolder.isDirectory())
 		{
-			Log.log(Log.ERROR, this, "System keymap folder do not exist, your installation is broken."
+			Log.log(Log.ERROR, this, "System keymap folder do not exist, your installation is broken. "
 						 + "Install keymaps in " + systemKeymapFolder.getAbsolutePath());
-			JOptionPane.showMessageDialog(null, "System keymap folder do not exist, your installation is broken."
+			JOptionPane.showMessageDialog(null, "System keymap folder do not exist, your installation is broken. "
 							    + "Install keymaps in " + systemKeymapFolder.getAbsolutePath(), "Fatal Error",
 						      JOptionPane.ERROR_MESSAGE);
 			System.exit(10);
