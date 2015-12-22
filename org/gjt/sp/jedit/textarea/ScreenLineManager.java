3eef6a0d199ce9873ef3236d1999db8e7225d1c5
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2015-12-22 17:56:09 +0000

    Avoid occasional NPE.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24248 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/ScreenLineManager.java
+++ org/gjt/sp/jedit/textarea/ScreenLineManager.java
@@ -48,6 +48,8 @@ class ScreenLineManager
 	//{{{ isScreenLineCountValid() method
 	boolean isScreenLineCountValid(int line)
 	{
+		if (screenLines == null || line < 0 || line >= screenLines.length)
+			return false;
 		return screenLines[line] > 0;
 	} //}}}
 
