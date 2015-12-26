e9fc06d6275737e01b26438e5e059de152a8169b
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2015-12-26 15:35:15 +0000

    Check for nulls.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24271 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/GUIUtilities.java
+++ org/gjt/sp/jedit/GUIUtilities.java
@@ -1912,16 +1912,22 @@ public class GUIUtilities
  	 */
 	public static void makeSameSize(Component... components) 
 	{
+		if (components == null)
+			return;
 		int width = 0;
 		int height = 0;
 		for (Component component : components) 
 		{
+			if (component == null)
+				continue;
 			width = Math.max(width, component.getPreferredSize().width);
 			height = Math.max(height, component.getPreferredSize().height);
 		}
 		Dimension d = new Dimension(width, height);
 		for (Component component : components) 
 		{
+			if (component == null)
+				continue;
 			component.setPreferredSize(d);	
 		}
 	} //}}}
