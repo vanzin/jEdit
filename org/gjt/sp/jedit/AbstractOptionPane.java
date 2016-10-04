faa22d97402b5fdaeb7be1bc089d919f3b655c62
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2016-10-04 20:18:52 +0000

    API Docs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24561 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/AbstractOptionPane.java
+++ org/gjt/sp/jedit/AbstractOptionPane.java
@@ -65,7 +65,12 @@ public class AbstractOptionPane extends JPanel implements OptionPane
 	//{{{ AbstractOptionPane constructor
 	/**
 	 * Creates a new option pane.
-	 * @param internalName The internal name. The option pane's label is set to the
+	 * @param internalName The internal name.
+	 * This should be the same name as that provided
+	 * in the plugin's <code>plugin.<i>className</i>.option-group</code>
+	 * or <code>.option-pane</code> property.
+	 *
+	 * The option pane's label is set to the
 	 * value of the property named <code>options.<i>name</i>.label</code>.
 	 */
 	public AbstractOptionPane(String internalName)
