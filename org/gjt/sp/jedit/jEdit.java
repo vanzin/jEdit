a38e064e86a7bac73290210f8928862ee22d9a14
vampire0 <vampire0@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2018-04-08 23:54:10 +0000

    increase version to 5.5.0
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24853 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -106,7 +106,7 @@ public class jEdit
 	public static String getBuild()
 	{
 		// (major).(minor).(<99 = preX, 99 = "final").(bug fix)
-		return "05.05.01.00";
+		return "05.05.99.00";
 	} //}}}
 
 	//{{{ main() method
