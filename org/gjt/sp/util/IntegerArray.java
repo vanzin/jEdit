651664f6a244a75d2d4350a8065588ec372dafc2
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2018-06-18 09:35:25 +0000

    New method IntegerArray.isEmpty()
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24864 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/util/IntegerArray.java
+++ org/gjt/sp/util/IntegerArray.java
@@ -58,6 +58,17 @@ public class IntegerArray
 		return array[index];
 	} //}}}
 
+	//{{{ isEmpty() method
+	/**
+	 * Returns {@code true} if this collection contains no elements.
+	 *
+	 * @return {@code true} if this collection contains no elements
+	 * @since jEdit 5.6pre1
+	 */
+	public boolean isEmpty() {
+		return len == 0;
+	} //}}}
+
 	//{{{ getSize() method
 	public final int getSize()
 	{
