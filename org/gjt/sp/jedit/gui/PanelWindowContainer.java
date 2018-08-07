e38326abc0ce6685e02bd5046ee330be0c4fd125
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2018-08-07 15:23:15 +0000

    Added property to turn on use of the RolloverToggleButton, set use.rolloverToggleButtons to true to use them instead of regular JToggleButtons.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24880 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/PanelWindowContainer.java
+++ org/gjt/sp/jedit/gui/PanelWindowContainer.java
@@ -148,8 +148,16 @@ public class PanelWindowContainer implements DockableWindowContainer, DockingAre
 		else
 			throw new InternalError("Invalid position: " + position);
 
-		JToggleButton button = new RolloverToggleButton();
-		//button.setMargin(new Insets(1,1,1,1));
+		JToggleButton button;
+		if (jEdit.getBooleanProperty("use.rolloverToggleButtons"))
+		{
+			button = new RolloverToggleButton();
+		}
+		else 
+		{
+			button = new JToggleButton();	
+			button.setMargin(new Insets(1,1,1,1));
+		}
 		GenericGUIUtilities.setButtonContentMargin(button, new Insets(6,6,6,6));
 		button.setRequestFocusEnabled(false);
 		button.setIcon(new RotatedTextIcon(rotation,button.getFont(),
