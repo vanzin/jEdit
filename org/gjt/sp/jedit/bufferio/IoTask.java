63d05bdc368999336f494ab189e5c86b10955ba8
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2020-03-21 14:56:51 +0000

    add a missing GPL2 licence
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@24995 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/bufferio/IoTask.java
+++ org/gjt/sp/jedit/bufferio/IoTask.java
@@ -1,3 +1,24 @@
+/*
+ * BufferIORequest.java - I/O request
+ * :tabSize=4:indentSize=4:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2013 jEdit contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package org.gjt.sp.jedit.bufferio;
 
 import org.gjt.sp.util.Task;
