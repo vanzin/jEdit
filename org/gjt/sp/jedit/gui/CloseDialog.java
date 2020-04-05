c895932723391d3235d15668cf6fc3bc44b16563
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2020-04-05 14:14:33 +0000

    some cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@25128 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/gui/CloseDialog.java
+++ org/gjt/sp/jedit/gui/CloseDialog.java
@@ -28,10 +28,8 @@ import java.util.Arrays;
 import javax.swing.border.*;
 import javax.swing.event.*;
 import javax.swing.*;
-import java.awt.event.*;
 import java.awt.*;
 import org.gjt.sp.jedit.bufferio.BufferIORequest;
-import org.gjt.sp.jedit.io.*;
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.util.GenericGUIUtilities;
 import org.gjt.sp.util.Log;
@@ -87,24 +85,24 @@ public class CloseDialog extends EnhancedDialog
 
 		content.add(BorderLayout.CENTER,centerPanel);
 
-		ActionHandler actionListener = new ActionHandler();
-
 		Box buttons = new Box(BoxLayout.X_AXIS);
 		buttons.add(Box.createGlue());
+		JButton selectAll;
 		buttons.add(selectAll = new JButton(jEdit.getProperty("close.selectAll")));
 		selectAll.setMnemonic(jEdit.getProperty("close.selectAll.mnemonic").charAt(0));
-		selectAll.addActionListener(actionListener);
+		selectAll.addActionListener((e) -> selectAll());
 		buttons.add(Box.createHorizontalStrut(6));
 		buttons.add(save = new JButton(jEdit.getProperty("close.save")));
 		save.setMnemonic(jEdit.getProperty("close.save.mnemonic").charAt(0));
-		save.addActionListener(actionListener);
+		save.addActionListener(e -> save());
 		buttons.add(Box.createHorizontalStrut(6));
 		buttons.add(discard = new JButton(jEdit.getProperty("close.discard")));
 		discard.setMnemonic(jEdit.getProperty("close.discard.mnemonic").charAt(0));
-		discard.addActionListener(actionListener);
+		discard.addActionListener(e -> discard());
 		buttons.add(Box.createHorizontalStrut(6));
+		JButton cancel;
 		buttons.add(cancel = new JButton(jEdit.getProperty("common.cancel")));
-		cancel.addActionListener(actionListener);
+		cancel.addActionListener(e -> cancel());
 		buttons.add(Box.createGlue());
 		bufferList.setSelectedIndex(0);
 		content.add(BorderLayout.SOUTH,buttons);
@@ -139,10 +137,8 @@ public class CloseDialog extends EnhancedDialog
 	private final View view;
 	private final JList<String> bufferList;
 	private final DefaultListModel<String> bufferModel;
-	private final JButton selectAll;
 	private final JButton save;
 	private final JButton discard;
-	private final JButton cancel;
 
 	private boolean ok; // only set if all buffers saved/closed
 
@@ -155,84 +151,77 @@ public class CloseDialog extends EnhancedDialog
 		discard.getModel().setEnabled(index != -1);
 	} //}}}
 
-	//{{{ ActionHandler class
-	private class ActionHandler implements ActionListener
+	private void discard()
 	{
-		public void actionPerformed(ActionEvent evt)
+		java.util.List<String> paths = bufferList.getSelectedValuesList();
+
+		for (String path : paths)
 		{
-			Object source = evt.getSource();
-			if(source == selectAll)
-			{
-				// I'm too tired to think of a better way
-				// to handle this right now.
-				try
-				{
-					selectAllFlag = true;
-
-					bufferList.setSelectionInterval(0,
-						bufferModel.getSize() - 1);
-				}
-				finally
-				{
-					selectAllFlag = false;
-				}
-				bufferList.requestFocus();
-			}
-			else if(source == save)
-			{
-				java.util.List<String> paths = bufferList.getSelectedValuesList();
-
-				for (String path : paths)
-				{
-					Buffer buffer = jEdit.getBuffer(path);
-					if (!buffer.save(view, null, true, true)) return;
-					TaskManager.instance.waitForIoTasks();
-					if (buffer.getBooleanProperty(BufferIORequest.ERROR_OCCURRED)) return;
-					jEdit._closeBuffer(view, buffer);
-					bufferModel.removeElement(path);
-				}
-
-				if(bufferModel.getSize() == 0)
-				{
-					ok = true;
-					dispose();
-				}
-				else
-				{
-					bufferList.setSelectedIndex(0);
-					bufferList.requestFocus();
-				}
-			}
-			else if(source == discard)
-			{
-				java.util.List<String> paths = bufferList.getSelectedValuesList();
-
-				for (String path : paths)
-				{
-					Buffer buffer = jEdit.getBuffer(path);
-					jEdit._closeBuffer(view, buffer);
-					bufferModel.removeElement(path);
-				}
-
-				if(bufferModel.getSize() == 0)
-				{
-					ok = true;
-					dispose();
-				}
-				else
-				{
-					bufferList.setSelectedIndex(0);
-					bufferList.requestFocus();
-				}
-			}
-			else if(source == cancel)
-				cancel();
+			Buffer buffer = jEdit.getBuffer(path);
+			jEdit._closeBuffer(view, buffer);
+			bufferModel.removeElement(path);
 		}
-	} //}}}
+
+		if(bufferModel.getSize() == 0)
+		{
+			ok = true;
+			dispose();
+		}
+		else
+		{
+			bufferList.setSelectedIndex(0);
+			bufferList.requestFocus();
+		}
+	}
+
+	private void save()
+	{
+		java.util.List<String> paths = bufferList.getSelectedValuesList();
+
+		for (String path : paths)
+		{
+			Buffer buffer = jEdit.getBuffer(path);
+			if (!buffer.save(view, null, true, true)) return;
+			TaskManager.instance.waitForIoTasks();
+			if (buffer.getBooleanProperty(BufferIORequest.ERROR_OCCURRED)) return;
+			jEdit._closeBuffer(view, buffer);
+			bufferModel.removeElement(path);
+		}
+
+		if(bufferModel.getSize() == 0)
+		{
+			ok = true;
+			dispose();
+		}
+		else
+		{
+			bufferList.setSelectedIndex(0);
+			bufferList.requestFocus();
+		}
+	}
+
+	private void selectAll()
+	{
+		// I'm too tired to think of a better way
+		// to handle this right now.
+		try
+		{
+			selectAllFlag = true;
+
+			bufferList.setSelectionInterval(0,
+				bufferModel.getSize() - 1);
+		}
+		finally
+		{
+			selectAllFlag = false;
+		}
+		bufferList.requestFocus();
+	}
 
 	//{{{ ListHandler class
 	private class ListHandler implements ListSelectionListener
 	{
+		@Override
 		public void valueChanged(ListSelectionEvent evt)
 		{
 			if(selectAllFlag)
