936427d0d5c65f30eaf551d9e39df53c4bba9a2a
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2020-04-18 10:30:48 +0000

    remove final modifier that prevents from doing unit tests mocking and is useless now
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@25264 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/TextArea.java
+++ org/gjt/sp/jedit/textarea/TextArea.java
@@ -632,7 +632,7 @@ public abstract class TextArea extends JPanel
 	 * Returns the number of lines visible in this text area.
 	 * @return the number of visible lines in the textarea
 	 */
-	public final int getVisibleLines()
+	public int getVisibleLines()
 	{
 		return visibleLines;
 	} //}}}
