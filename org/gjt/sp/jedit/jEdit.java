3fe52a075917e0dd865f13fff6ec0508d2f6c0ed
vampire0 <vampire0@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2020-09-03 09:40:01 +0000

    Update the version number to 5.7pre1
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@25361 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -104,7 +104,7 @@ public class jEdit
 	public static String getBuild()
 	{
 		// (major).(minor).(<99 = preX, 99 = "final").(bug fix)
-		return "05.06.99.00";
+		return "05.07.01.00";
 	} //}}}
 
 	//{{{ main() method
