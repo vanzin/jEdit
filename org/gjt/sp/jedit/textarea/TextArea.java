bb1fe8fe6d19ef4fe8a7af78e8934df8cbdee04b
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2021-03-25 22:00:35 +0000

    remove deprecated method
    git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@25440 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- org/gjt/sp/jedit/textarea/TextArea.java
+++ org/gjt/sp/jedit/textarea/TextArea.java
@@ -6284,32 +6284,6 @@ loop:		for(int i = lineNo - 1; i >= 0; i--)
 		}
 	} //}}}
 
-	//{{{ showPopupMenu() method - copied from GUIUtilities
-	/**
-	 * Shows the specified popup menu, ensuring it is displayed within
-	 * the bounds of the screen.
-	 * @param popup The popup menu
-	 * @param comp The component to show it for
-	 * @param x The x co-ordinate
-	 * @param y The y co-ordinate
-	 * @param point If true, then the popup originates from a single point;
-	 * otherwise it will originate from the component itself. This affects
-	 * positioning in the case where the popup does not fit onscreen.
-	 *
-	 * FIXME: move parts of GUIUtilities compatible with standalone TextArea in a separate
-	 * class, to prevent such copies
-	 *
-	 * @since jEdit 4.1pre1
-	 * @deprecated use {@link GenericGUIUtilities#showPopupMenu(JPopupMenu, Component, int, int, boolean)}
-	 */
-	@Deprecated
-	public static void showPopupMenu(JPopupMenu popup, Component comp,
-		int x, int y, boolean point)
-	{
-		GenericGUIUtilities.showPopupMenu(popup, comp, x, y, point);
-
-	} //}}}
-
 	//{{{ Character boundary staffs
 	//{{{ LineCharacterBreaker class
 	// Shared context among some operations which are aware of
